7159328fd2dcc7eb6e43fc4ca4c4f696b742ef21
Frederick-S <mao_xiaodan@hotmail.com>
2014-11-19 08:23:18 +0000

    Update code

--- 001_two_sum.py
+++ 001_two_sum.py
@@ -4,14 +4,10 @@
 class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
-        index1 = 0
-        index2 = 0
         mapping = {}
         
         for index, value in enumerate(num):
             if value in mapping:
-                index1 = mapping[value] + 1
-                index2 = index + 1
-                return (index1, index2)
+                return (mapping[value] + 1, index + 1)
             else:
                 mapping[target - value] = index
