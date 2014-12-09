3eeb8f507706b7fb7c386513ff4151c8404d4874
Frederick-S <mao_xiaodan@hotmail.com>
2014-12-09 08:51:21 +0800

    Update 007_reverse_integer.py

--- 007_reverse_integer.py
+++ 007_reverse_integer.py
@@ -4,15 +4,13 @@
 class Solution:
     # @return an integer
     def reverse(self, x):
-        digit = 0
         reversed_x = 0
         abs_x = abs(x)
         isNegative = x < 0
         max_abs_x = 2147483648 if x < 0 else 2147483647
         
         while abs_x:
-            digit = abs_x % 10
-            reversed_x = reversed_x * 10 + digit
+            reversed_x = reversed_x * 10 + abs_x % 10
             abs_x /= 10
         
         if reversed_x > max_abs_x:
