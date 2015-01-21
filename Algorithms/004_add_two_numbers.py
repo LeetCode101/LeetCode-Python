ce5ab82ba4aab38eada4bce18b9dfc0c37f94692
Frederick-S <mao_xiaodan@hotmail.com>
2015-01-21 10:17:13 +0800

    Update 004_add_two_numbers.py

--- Algorithms/004_add_two_numbers.py
+++ Algorithms/004_add_two_numbers.py
@@ -13,27 +13,27 @@ class Solution:
         node, head = None, None
         remain = ListNode(0)
         
-        while l1 or l2:
+        while l1 is not None or l2 is not None:
             total = 0
             
-            if l1:
+            if l1 is not None:
                 total += l1.val
                 l1 = l1.next
                 
-            if l2:
+            if l2 is not None:
                 total += l2.val
                 l2 = l2.next
                 
             total += remain.val
             remain.val = total / 10
             
-            if node:
+            if node is not None:
                 node.next = ListNode(total % 10)
                 node = node.next
             else:
                 node = ListNode(total % 10)
                 
-            if not head:
+            if head is None:
                 head = node
             
         if remain.val == 1:
