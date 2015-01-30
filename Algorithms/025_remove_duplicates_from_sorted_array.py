3f9411f97a94a7bc1eef93d3052bba871ebb86a7
Frederick-S <mao_xiaodan@hotmail.com>
2015-01-30 03:11:52 +0000

    Update problem 25

--- Algorithms/025_remove_duplicates_from_sorted_array.py
+++ Algorithms/025_remove_duplicates_from_sorted_array.py
@@ -6,24 +6,16 @@ class Solution:
     # @return an integer
     def removeDuplicates(self, A):
         length = len(A)
+        index = 0
         
-        if length < 2:
-            return length
+        if length == 0:
+            return index
+        
+        for i in range(length):
+            if A[index] == A[i]:
+                continue
             
-        current, max_so_far = 0, A[0]
-        new_length, available_index = 1, []
+            index += 1
+            A[index] = A[i]
         
-        for i in range(1, length):
-            current = A[i]
-            
-            if current > max_so_far:
-                max_so_far = current
-                new_length += 1
-                
-                if len(available_index) > 0:
-                    A[available_index.pop(0)] = current
-                    available_index.append(i)
-            else:
-                available_index.append(i)
-        
-        return new_length
+        return index + 1
