24ff4cfbb7f814ee14d05b060d7416502754c592
Frederick-S <mao_xiaodan@hotmail.com>
2015-05-13 15:02:05 +0800

    Create 056_merge_intervals.py

---
+++ Algorithms/056_merge_intervals.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    # @param {Interval[]} intervals
+    # @return {Interval[]}
+    def merge(self, intervals):
+        result = []
+        length = len(intervals)
+        
+        if length == 0:
+            return result
+        
+        intervals.sort(self.compare)
+        
+        prev = intervals[0]
+        
+        for i in range(1, length):
+            current = intervals[i]
+            
+            if prev.end < current.start:
+                result.append(prev)
+                prev = current
+            else:
+                prev = Interval(prev.start, max(prev.end, current.end))
+        
+        result.append(prev)
+        
+        return result
+    
+    def compare(self, a, b):
+        return a.start - b.start
