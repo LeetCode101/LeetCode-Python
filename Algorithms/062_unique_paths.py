199b2bf6d8b01a3bea2f83efc0da737a9ed5e5ea
Frederick-S <mao_xiaodan@hotmail.com>
2015-05-20 15:03:45 +0800

    Create 062_unique_paths.py

---
+++ Algorithms/062_unique_paths.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {integer} m
+    # @param {integer} n
+    # @return {integer}
+    def uniquePaths(self, m, n):
+        paths = [[0] * n for i in range(m)]
+        paths[0] = [1] * n
+        
+        for i in range(m):
+            paths[i][0] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if paths[i][j] == 0:
+                    paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
+        
+        return paths[m - 1][n - 1]
