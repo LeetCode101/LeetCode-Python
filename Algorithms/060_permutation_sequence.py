d40488d9fedb9a574b45a358604eda9ebafa9899
Frederick-S <mao_xiaodan@hotmail.com>
2015-05-20 13:33:28 +0800

    Create 060_permutation_sequence.py

---
+++ Algorithms/060_permutation_sequence.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {integer} n
+    # @param {integer} k
+    # @return {string}
+    def getPermutation(self, n, k):
+        permutation = ''
+        available_numbers = [i for i in range(1, n + 1)]
+        
+        for i in range(n):
+            group_count = self.factorial(n - 1 - i)
+            group_number = int(math.ceil(k * 1.0 / group_count))
+            permutation += str(available_numbers[group_number - 1])
+            del available_numbers[group_number - 1]
+            k -= group_count * (group_number - 1)
+        
+        return permutation
+    
+    def factorial(self, n):
+        result = 1
+        
+        for i in range(2, n + 1):
+            result *= i
+        
+        return result
