617116b5e8bff21ac85e93b847e39eaa910cc2ad
Frederick-S <mao_xiaodan@hotmail.com>
2015-06-03 10:56:05 +0800

    Update 071_simplify_path.py

--- Algorithms/071_simplify_path.py
+++ Algorithms/071_simplify_path.py
@@ -8,30 +8,15 @@ class Solution:
         i = 0
         folder = ''
         folders = []
-        path_length = len(path)
+        paths = path.split('/')
         
-        while i < path_length:
-            current = path[i]
-            
-            if current == '/' and folder != '':
-                self.append_folder(folders, folder)
-                        
-                folder = ''
-            elif current != '/':
-                folder += current
-            
-            i += 1
-
-        if folder != '':
-            self.append_folder(folders, folder)
+        for p in paths:
+            if p == '.' or p == '':
+                pass
+            elif p == '..':
+                if len(folders) > 0:
+                    folders.pop()
+            else:
+                folders.append(p)
         
         return '/' + '/'.join(folders)
-
-    def append_folder(self, folders, folder):
-        if folder == '.':
-            pass
-        elif folder == '..':
-            if len(folders) > 0:
-                folders.pop()
-        else:
-            folders.append(folder)
