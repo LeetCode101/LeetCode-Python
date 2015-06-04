822c9167a5363a756027a86bb32206f48dd67495
Frederick-S <mao_xiaodan@hotmail.com>
2015-06-04 10:56:02 +0800

    Update 073_set_matrix_zeroes.py

--- Algorithms/073_set_matrix_zeroes.py
+++ Algorithms/073_set_matrix_zeroes.py
@@ -7,20 +7,34 @@ class Solution:
     def setZeroes(self, matrix):
         row = len(matrix)
         column = len(matrix[0])
-        is_original_zero = [[False] * column for i in range(row)]
+        is_first_row_zero = False
+        is_first_column_zero = False
+        
+        for i in range(column):
+            if matrix[0][i] == 0:
+                is_first_row_zero = True
+                break
         
         for i in range(row):
-            for j in range(column):
-                is_original_zero[i][j] = matrix[i][j] == 0
-
-        i = 0
-        j = 0
+            if matrix[i][0] == 0:
+                is_first_column_zero = True
+                break
         
-        for i in range(row):
-            for j in range(column):
-                if matrix[i][j] == 0 and is_original_zero[i][j]:
-                    for m in range(column):
-                        matrix[i][m] = 0
-                    
-                    for n in range(row):
-                        matrix[n][j] = 0
+        for i in range(1, row):
+            for j in range(1, column):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1, row):
+            for j in range(1, column):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] =0
+        
+        if is_first_row_zero:
+            for i in range(column):
+                matrix[0][i] = 0
+        
+        if is_first_column_zero:
+            for i in range(row):
+                matrix[i][0] = 0
