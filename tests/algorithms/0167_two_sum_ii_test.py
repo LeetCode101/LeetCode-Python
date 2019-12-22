2478214266be40e3e40a6edf9782d7d09a2c3664
Frederick-S <mao_xiaodan@hotmail.com>
2019-12-22 22:39:20 +0800

    Rename file

--- tests/algorithms/0167_two_sum_ii_test.py
+++ tests/algorithms/0167_two_sum_ii_test.py
@@ -1,10 +1,10 @@
 import unittest
-from leetcode.algorithms.q0167_two_sum_ii import Solution
+from leetcode.algorithms.q0167_two_sum_ii_1 import Solution as Solution1
 
 
 class TestTwoSum(unittest.TestCase):
-    def test_two_sum(self):
+    def test_two_sum1(self):
         nums = [2, 7, 11, 15]
         target = 9
 
-        self.assertListEqual([1, 2], Solution().twoSum(nums, target))
+        self.assertListEqual([1, 2], Solution1().twoSum(nums, target))
