7414550a944b60627f1ca275bff9513e40988cd0
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-01 14:41:23 +0800

    Add 653

---
+++ leetcode/algorithms/p0653_two_sum_iv.py
@@ -0,0 +1,25 @@
+from typing import Set
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        return self.find(root, k, set())
+
+    def find(self, root: TreeNode, k: int, visited: Set[int]) -> bool:
+        if root is None:
+            return False
+
+        if k - root.val in visited:
+            return True
+
+        visited.add(root.val)
+
+        return self.find(root.left, k, visited) or self.find(
+            root.right, k, visited)
---
+++ tests/algorithms/0653_two_sum_iv_test.py
@@ -0,0 +1,19 @@
+import unittest
+from leetcode.algorithms.p0653_two_sum_iv import Solution
+from leetcode.algorithms.p0653_two_sum_iv import TreeNode
+
+
+class TestTwoSum(unittest.TestCase):
+    def test_two_sum(self):
+        solution = Solution()
+        a = TreeNode(3)
+        a.left = TreeNode(2)
+        a.right = TreeNode(4)
+        b = TreeNode(6)
+        b.right = TreeNode(7)
+        root = TreeNode(5)
+        root.left = a
+        root.right = b
+
+        self.assertTrue(solution.findTarget(root, 9))
+        self.assertFalse(solution.findTarget(root, 28))
