bd7aaeacf9a9653e5d87f8aa3127e4ba9d18d3f6
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-04 17:03:41 +0800

    Add 523

---
+++ leetcode/algorithms/p0523_continuous_subarray_sum.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        sum = 0
+        mapping = {0: -1}
+
+        for i, n in enumerate(nums):
+            sum += n
+            key = sum if k == 0 else sum % k
+
+            if key in mapping:
+                if i - mapping[key] > 1:
+                    return True
+            else:
+                mapping[key] = i
+
+        return False
---
+++ tests/algorithms/p0523_continuous_subarray_sum_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0523_continuous_subarray_sum import Solution
+
+
+class TestContinuousSubarraySum(unittest.TestCase):
+    def test_continuous_subarray_sum(self):
+        solution = Solution()
+
+        self.assertTrue(solution.checkSubarraySum([23, 2, 4, 6, 7], 6))
+        self.assertTrue(solution.checkSubarraySum([23, 2, 6, 4, 7], 6))
