903dbf55b5b50d102883aabf74229e1329a88085
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-06 23:02:46 +0800

    Add 206

---
+++ leetcode/algorithms/p0206_reverse_linked_list_1.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, current = None, head
+
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev, current = current, next
+
+        return prev
