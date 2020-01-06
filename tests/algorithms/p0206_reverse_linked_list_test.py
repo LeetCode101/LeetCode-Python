c0512c27af10fb9a4772bf93146dc942aba93344
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-06 23:03:00 +0800

    Add 206 empty test

---
+++ tests/algorithms/p0206_reverse_linked_list_test.py
@@ -0,0 +1,8 @@
+import unittest
+from leetcode.algorithms.p0206_reverse_linked_list_1 \
+    import Solution as Solution1, ListNode
+
+
+class TestReverseLinkedList(unittest.TestCase):
+    def test_reverse_linked_list(self):
+        pass
