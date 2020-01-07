b8fac1ba2bbbb779cbe38b3b8fce255dce2973dd
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-07 22:24:38 +0800

    Update test

--- tests/algorithms/p0206_reverse_linked_list_test.py
+++ tests/algorithms/p0206_reverse_linked_list_test.py
@@ -4,5 +4,28 @@ from leetcode.algorithms.p0206_reverse_linked_list_1 \
 
 
 class TestReverseLinkedList(unittest.TestCase):
-    def test_reverse_linked_list(self):
-        pass
+    def test_reverse_linked_list1(self):
+        solution = Solution1()
+        a = ListNode(1)
+        b = ListNode(2)
+        c = ListNode(3)
+        d = ListNode(4)
+        e = ListNode(5)
+        d.next = e
+        c.next = d
+        b.next = c
+        a.next = b
+
+        self.assertListEqual(
+            [5, 4, 3, 2, 1],
+            self.convert_linked_list_to_list(solution.reverseList(a)))
+
+    def convert_linked_list_to_list(self, head):
+        list, current = [], head
+
+        while current is not None:
+            list.append(current.val)
+
+            current = current.next
+
+        return list
