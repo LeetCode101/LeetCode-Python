14f523ca8303e44798742cd0cc79d42fff3952a8
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-12 20:16:15 +0800

    Add 92

---
+++ leetcode/algorithms/p0092_reverse_linked_list_ii_2.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        dummy = ListNode(-1)
+        dummy.next = head
+        tail_of_left = dummy
+
+        for i in range(m - 1):
+            tail_of_left = tail_of_left.next
+
+        current = tail_of_left.next
+
+        for i in range(m, n):
+            next = current.next
+            current.next = next.next
+            next.next, tail_of_left.next = tail_of_left.next, next
+
+        return dummy.next
---
+++ tests/algorithms/p0092_reverse_linked_list_ii_2_test.py
@@ -0,0 +1,30 @@
+import unittest
+from leetcode.algorithms.p0092_reverse_linked_list_ii_2 \
+    import Solution, ListNode
+from .list_helper import convert_linked_list_to_list
+
+
+class TestReverseLinkedList(unittest.TestCase):
+    def test_reverse_linked_list(self):
+        solution = Solution()
+        a = ListNode(1)
+        b = ListNode(2)
+        c = ListNode(3)
+        d = ListNode(4)
+        e = ListNode(5)
+        d.next = e
+        c.next = d
+        b.next = c
+        a.next = b
+
+        self.assertListEqual(
+            [1, 4, 3, 2, 5],
+            convert_linked_list_to_list(solution.reverseBetween(a, 2, 4)))
+
+        a1 = ListNode(3)
+        b1 = ListNode(5)
+        a1.next = b1
+
+        self.assertListEqual([5, 3],
+                             convert_linked_list_to_list(
+                                 solution.reverseBetween(a1, 1, 2)))
