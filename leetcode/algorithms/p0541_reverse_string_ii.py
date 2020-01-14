1fbc9a8f151157f3a75ed73cac1b4753839a9f97
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-14 21:55:55 +0800

    Remove ambiguous naming

--- leetcode/algorithms/p0541_reverse_string_ii.py
+++ leetcode/algorithms/p0541_reverse_string_ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        l = list(s)
+        chars = list(s)
 
-        for i in range(0, len(l), 2 * k):
-            low, high = i, min(i + k - 1, len(l) - 1)
+        for i in range(0, len(chars), 2 * k):
+            low, high = i, min(i + k - 1, len(chars) - 1)
 
             while low < high:
-                l[low], l[high] = l[high], l[low]
+                chars[low], chars[high] = chars[high], chars[low]
 
                 low += 1
                 high -= 1
 
-        return ''.join(l)
+        return ''.join(chars)
