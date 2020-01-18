2638c5ff6258bd7578a3598d955f3f4784c6f188
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-18 14:35:29 +0800

    Update 445

---
+++ leetcode/algorithms/p0445_add_two_numbers_ii_2.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        stack1, stack2 = self.convert_list_to_stack(l1), \
+                         self.convert_list_to_stack(l2)
+        prev = None
+        carry = 0
+
+        while stack1 or stack2:
+            sum = carry
+
+            if stack1:
+                sum += stack1.pop()
+
+            if stack2:
+                sum += stack2.pop()
+
+            carry = sum // 10
+            new = ListNode(sum % 10)
+            new.next, prev = prev, new
+
+        if carry != 0:
+            new = ListNode(carry)
+            new.next, prev = prev, new
+
+        return prev
+
+    def convert_list_to_stack(self, head: ListNode) -> List[int]:
+        stack, current = [], head
+
+        while current:
+            stack.append(current.val)
+
+            current = current.next
+
+        return stack
---
+++ tests/algorithms/p0445_add_two_numbers_ii_2_test.py
@@ -0,0 +1,30 @@
+import unittest
+from leetcode.algorithms.p0445_add_two_numbers_ii_2 \
+    import Solution, ListNode
+from .list_helper import convert_linked_list_to_list
+
+
+class TestAddTwoNumbers(unittest.TestCase):
+    def test_add_two_numbers(self):
+        solution = Solution()
+        a = ListNode(7)
+        b = ListNode(2)
+        c = ListNode(4)
+        d = ListNode(3)
+        a.next = b
+        b.next = c
+        c.next = d
+
+        e = ListNode(5)
+        f = ListNode(6)
+        g = ListNode(4)
+        e.next = f
+        f.next = g
+
+        self.assertListEqual(
+            [7, 8, 0, 7],
+            convert_linked_list_to_list(solution.addTwoNumbers(a, e)))
+        self.assertListEqual(
+            [1, 0],
+            convert_linked_list_to_list(
+                solution.addTwoNumbers(ListNode(5), ListNode(5))))
