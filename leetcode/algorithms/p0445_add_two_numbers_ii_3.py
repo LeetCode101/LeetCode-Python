b4627c33978a77f11ab8c38fbfe18e770086459e
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-18 14:47:34 +0800

    Update 445

---
+++ leetcode/algorithms/p0445_add_two_numbers_ii_3.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        sum = self.get_list_sum(l1) + self.get_list_sum(l2)
+        numbers = list(str(sum))
+        dummy = ListNode(-1)
+        prev = dummy
+
+        for n in numbers:
+            new = ListNode(int(n))
+            prev.next, prev = new, new
+
+        return dummy.next
+
+    def get_list_sum(self, head: ListNode) -> int:
+        sum, current = 0, head
+
+        while current:
+            sum = sum * 10 + current.val
+            current = current.next
+
+        return sum
