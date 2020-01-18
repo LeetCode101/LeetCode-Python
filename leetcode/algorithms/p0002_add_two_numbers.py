5f0a4ef2bbdc75fae8f802b6031173c138d93d15
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-18 10:50:55 +0800

    Add 2

---
+++ leetcode/algorithms/p0002_add_two_numbers.py
@@ -0,0 +1,36 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        current1, current2 = l1, l2
+        dummy = ListNode(-1)
+        prev = dummy
+        carry = 0
+
+        while current1 or current2:
+            sum = carry
+
+            if current1:
+                sum += current1.val
+
+            if current2:
+                sum += current2.val
+
+            carry = sum // 10
+            prev.next = ListNode(sum % 10)
+            prev = prev.next
+
+            if current1:
+                current1 = current1.next
+
+            if current2:
+                current2 = current2.next
+
+        if carry != 0:
+            prev.next = ListNode(carry)
+
+        return dummy.next
---
+++ tests/algorithms/p0002_add_two_numbers_test.py
@@ -0,0 +1,24 @@
+import unittest
+from leetcode.algorithms.p0002_add_two_numbers \
+    import Solution, ListNode
+from .list_helper import convert_linked_list_to_list
+
+
+class TestAddTwoNumbers(unittest.TestCase):
+    def test_add_two_numbers(self):
+        solution = Solution()
+        a = ListNode(2)
+        b = ListNode(4)
+        c = ListNode(3)
+        a.next = b
+        b.next = c
+
+        d = ListNode(5)
+        e = ListNode(6)
+        f = ListNode(4)
+        d.next = e
+        e.next = f
+
+        self.assertListEqual(
+            [7, 0, 8],
+            convert_linked_list_to_list(solution.addTwoNumbers(a, d)))
