6290622f04bde4b64dccc9144551c4e7712c8a22
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-19 08:39:29 +0800

    Update 23

--- leetcode/algorithms/p0023_merge_k_sorted_lists_2.py
+++ leetcode/algorithms/p0023_merge_k_sorted_lists_2.py
@@ -8,20 +8,28 @@ class ListNode:
         self.next = None
 
 
+class ListNodeWrap:
+    def __init__(self, node: ListNode):
+        self.node = node
+
+    def __lt__(self, other):
+        return self.node.val < other.node.val
+
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        queue = [(node.val, id(node), node) for node in lists if node]
+        queue = [(node.val, ListNodeWrap(node)) for node in lists if node]
         heapq.heapify(queue)
         dummy = ListNode(-1)
         prev = dummy
 
         while queue:
-            value, node_id, node = heapq.heappop(queue)
+            value, node_wrap = heapq.heappop(queue)
             prev.next = ListNode(value)
             prev = prev.next
-            node = node.next
+            node = node_wrap.node.next
 
             if node:
-                heapq.heappush(queue, (node.val, id(node), node))
+                heapq.heappush(queue, (node.val, ListNodeWrap(node)))
 
         return dummy.next
