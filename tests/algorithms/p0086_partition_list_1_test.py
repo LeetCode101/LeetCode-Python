9acfe475ac69440253a682431273c757a80d1696
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-21 10:07:36 +0800

    Rename test

--- tests/algorithms/p0086_partition_list_1_test.py
+++ tests/algorithms/p0086_partition_list_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0086_partition_list \
+from leetcode.algorithms.p0086_partition_list_1 \
     import Solution, ListNode
 from .list_helper import convert_linked_list_to_list
 
