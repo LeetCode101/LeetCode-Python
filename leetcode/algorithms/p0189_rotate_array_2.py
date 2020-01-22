b3df47bed524076580c7a01827caebad7501f090
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-22 20:34:09 +0800

    Add solution 2 to p 189

---
+++ leetcode/algorithms/p0189_rotate_array_2.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+# Time Limit Exceeded!
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        step = k % len(nums)
+        length = len(nums)
+        temp = [nums[i] for i in range(length - step)]
+
+        for i in range(step):
+            nums[i] = nums[length - step + i]
+
+        for i in range(step, length):
+            nums[i] = temp[i - step]
