c14581c69728c8090840efdfebf6065d7d2b7f44
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-22 22:57:52 +0800

    Add solution 3 of p189

---
+++ leetcode/algorithms/p0189_rotate_array_3.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        step = k % len(nums)
+        length = len(nums)
+
+        self.reverse(nums, 0, length - 1)
+        self.reverse(nums, 0, step - 1)
+        self.reverse(nums, step, length - 1)
+
+    def reverse(self, nums: List[int], low: int, high: int):
+        while low < high:
+            nums[low], nums[high] = nums[high], nums[low]
+            low += 1
+            high -= 1
