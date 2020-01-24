f3d9cc1c04ae5d5a323fc43310b7584b9b2cb2b7
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-24 11:42:16 +0800

    Add solution 2 to p15

---
+++ leetcode/algorithms/p0015_3sum_2.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        sums = []
+
+        for i in range(0, len(nums) - 2):
+            if i - 1 >= 0 and nums[i] == nums[i - 1]:
+                continue
+
+            target = -nums[i]
+            low, high = i + 1, len(nums) - 1
+
+            while low < high:
+                if nums[low] + nums[high] == target:
+                    sums.append([-target, nums[low], nums[high]])
+
+                    while low < high and nums[low] == nums[low + 1]:
+                        low += 1
+
+                    while low < high and nums[high] == nums[high - 1]:
+                        high -= 1
+
+                    low += 1
+                    high -= 1
+                elif nums[low] + nums[high] < target:
+                    low += 1
+                else:
+                    high -= 1
+
+        return sums
--- tests/algorithms/p0015_3sum_2_test.py
+++ tests/algorithms/p0015_3sum_2_test.py
@@ -1,12 +1,12 @@
 import unittest
-from leetcode.algorithms.p0015_3sum_1 import Solution
+from leetcode.algorithms.p0015_3sum_2 import Solution
 
 
 class Test3Sum(unittest.TestCase):
     def test_3_sum(self):
         solution = Solution()
-        actual_lists = solution.threeSum([-1, 0, 1, 2, -1, -4])
-        expected_lists = [[-1, 0, 1], [-1, -1, 2]]
+        actual_lists = solution.threeSum([3, 0, -2, -1, 1, 2])
+        expected_lists = [[-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
 
         for i, l in enumerate(actual_lists):
             self.assertListEqual(sorted(expected_lists[i]), sorted(l))
