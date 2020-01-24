c4a0ef10390fef0d04f1befcba63ae53f12f8c5b
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-24 11:53:34 +0800

    Update test

--- tests/algorithms/p0015_3sum_2_test.py
+++ tests/algorithms/p0015_3sum_2_test.py
@@ -5,7 +5,7 @@ from leetcode.algorithms.p0015_3sum_2 import Solution
 class Test3Sum(unittest.TestCase):
     def test_3_sum(self):
         solution = Solution()
-        actual_lists = solution.threeSum([-10, 3, 0, -2, -2, -1, 1, 2, 0])
+        actual_lists = solution.threeSum([-10, 3, 0, -2, -2, -1, 1, 3, 2, 0])
         expected_lists = [[-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
 
         self.assertEqual(len(expected_lists), len(actual_lists))
