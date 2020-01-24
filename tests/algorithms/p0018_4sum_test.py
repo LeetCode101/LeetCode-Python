e85e8f9013166c5afcc9d18cf44b052234dd8e3d
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-24 16:37:42 +0800

    Update test

--- tests/algorithms/p0018_4sum_test.py
+++ tests/algorithms/p0018_4sum_test.py
@@ -5,7 +5,7 @@ from leetcode.algorithms.p0018_4sum import Solution
 class Test4Sum(unittest.TestCase):
     def test_4sum(self):
         solution = Solution()
-        actual_lists = solution.fourSum([1, 0, -1, 0, -2, 2], 0)
+        actual_lists = solution.fourSum([0, 1, 0, -1, 0, -2, 2, 2], 0)
         expected_lists = [
             [-2, -1, 1, 2],
             [-2, 0, 0, 2],
