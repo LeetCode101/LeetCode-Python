10a0b47b89246b56229dd79e0fd8f96f9a476076
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-25 17:21:59 +0800

    Update test

--- tests/algorithms/p0414_third_maximum_number_test.py
+++ tests/algorithms/p0414_third_maximum_number_test.py
@@ -6,6 +6,6 @@ class TestThirdMaximumNumber(unittest.TestCase):
     def test_third_maximum_number(self):
         solution = Solution()
 
-        # self.assertEqual(1, solution.thirdMax([3, 2, 1]))
-        # self.assertEqual(2, solution.thirdMax([1, 2]))
+        self.assertEqual(1, solution.thirdMax([3, 2, 1]))
+        self.assertEqual(2, solution.thirdMax([1, 2]))
         self.assertEqual(1, solution.thirdMax([2, 2, 3, 1]))
