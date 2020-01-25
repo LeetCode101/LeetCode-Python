4e9ad0e0a3facaa006d6b288caf098fd8a3739a1
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-25 16:29:34 +0800

    Add solution 3 of p215

---
+++ leetcode/algorithms/p0215_kth_largest_element_in_an_array_3.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        low, high = 0, len(nums) - 1
+
+        while True:
+            p = self.partition(nums, low, high)
+
+            if p == k - 1:
+                return nums[p]
+            elif p > k - 1:
+                high = p - 1
+            else:
+                low = p + 1
+
+    def partition(self, nums: List[int], low: int, high: int):
+        pivot = (low + high) // 2
+        pivot_value = nums[pivot]
+        nums[pivot], nums[high] = nums[high], nums[pivot]
+        p = low
+
+        for i in range(low, high):
+            if nums[i] >= pivot_value:
+                nums[p], nums[i] = nums[i], nums[p]
+                p += 1
+
+        nums[high], nums[p] = nums[p], nums[high]
+
+        return p
