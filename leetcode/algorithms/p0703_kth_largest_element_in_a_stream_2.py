48a1d43e9f4762105010f292c7a8e826cd937abb
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-25 15:43:02 +0800

    Update 703

--- leetcode/algorithms/p0703_kth_largest_element_in_a_stream_2.py
+++ leetcode/algorithms/p0703_kth_largest_element_in_a_stream_2.py
@@ -11,6 +11,9 @@ class KthLargest:
             self.add_to_heap(n)
 
     def add(self, val: int) -> int:
+        if len(self.heap) == self.k and self.heap[0] > val:
+            return self.heap[0]
+
         self.add_to_heap(val)
 
         return self.heap[0]
