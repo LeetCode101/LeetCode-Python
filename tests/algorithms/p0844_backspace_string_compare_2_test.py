523ec86ba055603f8ef74ae8c415c33b468cd5da
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-25 09:56:11 +0800

    Update test

--- tests/algorithms/p0844_backspace_string_compare_2_test.py
+++ tests/algorithms/p0844_backspace_string_compare_2_test.py
@@ -6,6 +6,7 @@ class TestBackspaceStringCompare(unittest.TestCase):
     def test_backspace_string_compare(self):
         solution = Solution()
 
-        # self.assertTrue(solution.backspaceCompare('ab##', 'c#d#'))
+        self.assertTrue(solution.backspaceCompare('ab##', 'c#d#'))
         self.assertFalse(solution.backspaceCompare('a#c', 'bc'))
         self.assertFalse(solution.backspaceCompare('a#c', 'b'))
+        self.assertTrue(solution.backspaceCompare('a', 'a'))
