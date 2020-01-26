a9d1d1e3b96760817c64b07f3dd8f6fe969abd08
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-26 19:26:31 +0800

    Rename test

--- tests/algorithms/p0169_majority_element_test.py
+++ tests/algorithms/p0169_majority_element_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0169_majority_element_1 import Solution
+from leetcode.algorithms.p0169_majority_element import Solution
 
 
 class TestMajorityElement(unittest.TestCase):
