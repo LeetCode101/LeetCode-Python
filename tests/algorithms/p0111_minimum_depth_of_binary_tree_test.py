2d105e79c35c9b7483a4eb1c008d7022dbeba202
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-26 22:28:15 +0800

    Add 111

---
+++ tests/algorithms/p0111_minimum_depth_of_binary_tree_test.py
@@ -0,0 +1,17 @@
+import unittest
+from leetcode.algorithms.p0111_minimum_depth_of_binary_tree \
+    import Solution, TreeNode
+
+
+class TestMinimumDepthOfBinaryTree(unittest.TestCase):
+    def test_minimum_depth_of_binary_tree(self):
+        solution = Solution()
+        a = TreeNode(3)
+        b = TreeNode(9)
+        c = TreeNode(20)
+        a.left = b
+        a.right = c
+        c.left = TreeNode(15)
+        c.right = TreeNode(7)
+
+        self.assertEqual(2, solution.minDepth(a))
