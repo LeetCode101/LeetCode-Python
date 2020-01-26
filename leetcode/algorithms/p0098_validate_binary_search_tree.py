4f2a873f1c26f7eb446f564caa5487d272dff8b7
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-26 14:12:17 +0800

    Add 98

---
+++ leetcode/algorithms/p0098_validate_binary_search_tree.py
@@ -0,0 +1,27 @@
+import sys
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
+        return self.is_valid(root.left, -sys.maxsize, root.val) and \
+            self.is_valid(root.right, root.val, sys.maxsize)
+
+    def is_valid(self, root: TreeNode, low: int, high: int) -> bool:
+        if not root:
+            return True
+
+        if root.val <= low or root.val >= high:
+            return False
+
+        return self.is_valid(root.left, low, root.val) and \
+            self.is_valid(root.right, root.val, high)
