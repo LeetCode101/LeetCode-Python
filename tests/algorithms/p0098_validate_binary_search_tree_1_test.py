d7a4bb7305106accc60272e38a5c8e898b0cad55
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-26 14:17:42 +0800

    Update test

---
+++ tests/algorithms/p0098_validate_binary_search_tree_1_test.py
@@ -0,0 +1,24 @@
+import unittest
+from leetcode.algorithms.p0098_validate_binary_search_tree_1 \
+    import Solution, TreeNode
+
+
+class TestValidateBinarySearchTree(unittest.TestCase):
+    def test_validate_binary_search_tree(self):
+        solution = Solution()
+        a = TreeNode(2)
+        a.left = TreeNode(1)
+        a.right = TreeNode(3)
+
+        self.assertTrue(solution.isValidBST(None))
+        self.assertTrue(solution.isValidBST(a))
+
+        a = TreeNode(5)
+        b = TreeNode(1)
+        c = TreeNode(4)
+        c.left = TreeNode(3)
+        c.right = TreeNode(6)
+        a.left = b
+        a.right = c
+
+        self.assertFalse(solution.isValidBST(a))
