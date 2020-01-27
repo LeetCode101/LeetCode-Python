43774e7c69efe08958f91e01fbe0919c4290d979
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-27 10:51:58 +0800

    Update 22

--- leetcode/algorithms/p0022_generate_parentheses.py
+++ leetcode/algorithms/p0022_generate_parentheses.py
@@ -5,23 +5,22 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
 
-        self.generate(0, 0, n, '', result)
+        self.generate(n, n, '', result)
 
         return result
 
     def generate(
             self, left_parenthesis_count: int, right_parenthesis_count: int,
-            n: int, parenthesis: str, result: List[str]) -> None:
-        if left_parenthesis_count == n and right_parenthesis_count == n:
+            parenthesis: str, result: List[str]) -> None:
+        if left_parenthesis_count == 0 and right_parenthesis_count == 0:
             result.append(parenthesis)
 
             return
 
-        if left_parenthesis_count < n:
-            self.generate(left_parenthesis_count + 1, right_parenthesis_count,
-                          n, parenthesis + '(', result)
+        if left_parenthesis_count > 0:
+            self.generate(left_parenthesis_count - 1, right_parenthesis_count,
+                          parenthesis + '(', result)
 
-        if left_parenthesis_count > right_parenthesis_count \
-                and right_parenthesis_count < n:
-            self.generate(left_parenthesis_count, right_parenthesis_count + 1,
-                          n, parenthesis + ')', result)
+        if left_parenthesis_count < right_parenthesis_count:
+            self.generate(left_parenthesis_count, right_parenthesis_count - 1,
+                          parenthesis + ')', result)
