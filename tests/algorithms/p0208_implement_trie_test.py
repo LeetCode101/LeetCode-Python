72fb300374ef7b9ed5887d3d62b1ab14ab2e0f7d
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-27 20:00:48 +0800

    Add 208

---
+++ tests/algorithms/p0208_implement_trie_test.py
@@ -0,0 +1,16 @@
+import unittest
+from leetcode.algorithms.p0208_implement_trie import Trie
+
+
+class TestImplementTrie(unittest.TestCase):
+    def test_implement_trie(self):
+        trie = Trie()
+        trie.insert('apple')
+
+        self.assertTrue(trie.search('apple'))
+        self.assertFalse(trie.search('app'))
+        self.assertTrue(trie.startsWith('app'))
+
+        trie.insert('app')
+
+        self.assertTrue(trie.search('app'))
