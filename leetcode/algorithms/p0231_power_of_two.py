bedb9f2b14dea688e0bf3c90e1418857ab99b8ea
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-27 23:05:03 +0800

    Add 231

---
+++ leetcode/algorithms/p0231_power_of_two.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+
+        while n != 0:
+            if n == 1:
+                return True
+
+            if n & 1:
+                return False
+
+            n >>= 1
+
+        return True
