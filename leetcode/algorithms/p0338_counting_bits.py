d528f191fab5cd0a3b02671b9763cb230fd8ddf9
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-28 11:02:50 +0800

    Add 338

---
+++ leetcode/algorithms/p0338_counting_bits.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        return [self.hammingWeight(x) for x in range(num + 1)]
+
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+
+        while n > 0:
+            n = n & (n - 1)
+
+            count += 1
+
+        return count
