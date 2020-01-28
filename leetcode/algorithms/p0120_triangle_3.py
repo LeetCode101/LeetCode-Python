56035af964924410a80d81a3c7ace0341f1d52bb
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-28 17:38:29 +0800

    Add 120

---
+++ leetcode/algorithms/p0120_triangle_3.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        sums = [[0] * len(t) for t in triangle]
+
+        for i, value in enumerate(triangle[-1]):
+            sums[len(triangle) - 1][i] = value
+
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(i + 1):
+                sums[i][j] = triangle[i][j] + min(sums[i + 1][j], sums[i + 1][j + 1])
+
+        return sums[0][0]
