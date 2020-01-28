752da44ac8476fa5d2d10a63cc792eabf7228621
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-28 21:58:38 +0800

    Add 121

---
+++ leetcode/algorithms/p0121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0] if prices else 0
+        max_profit = 0
+
+        for i in range(1, len(prices)):
+            min_price = min(min_price, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price)
+
+        return max_profit
