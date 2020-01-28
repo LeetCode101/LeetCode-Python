d3fcfcb80d8f7f63b8169fcc681b14f3033077bd
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-28 15:33:04 +0800

    Format code

--- leetcode/algorithms/p0070_climbing_stairs.py
+++ leetcode/algorithms/p0070_climbing_stairs.py
@@ -8,6 +8,6 @@ class Solution:
         steps[1] = 2
 
         for i in range(2, n):
-            steps[i] =  steps[i - 1] + steps[i - 2]
+            steps[i] = steps[i - 1] + steps[i - 2]
 
         return steps[n - 1]
