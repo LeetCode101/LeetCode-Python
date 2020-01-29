678c08e3776fd072c25588583b260b3396baf33d
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-29 13:30:04 +0800

    Update test

--- tests/algorithms/p0188_best_time_to_buy_and_sell_stock_iv_test.py
+++ tests/algorithms/p0188_best_time_to_buy_and_sell_stock_iv_test.py
@@ -7,6 +7,7 @@ class TestBestTimeToBuyAndSellStock(unittest.TestCase):
     def test_best_time_to_buy_and_sell_stock(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.maxProfit(2, []))
         self.assertEqual(2, solution.maxProfit(2, [2, 4, 1]))
         self.assertEqual(7, solution.maxProfit(2, [3, 2, 6, 5, 0, 3]))
         self.assertEqual(7, solution.maxProfit(100, [3, 2, 6, 5, 0, 3]))
