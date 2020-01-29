27c3e81cb6f65f81344b9f1a3d59f27decb0fb7a
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-29 16:29:02 +0800

    Add 300

---
+++ leetcode/algorithms/p0300_longest_increasing_subsequence.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        dp = [0] * len(nums)
+
+        for i in range(len(nums)):
+            dp[i] = max([dp[j] for j in range(i) if nums[j] < nums[i]], default=0) + 1
+
+        return max(dp)
---
+++ tests/algorithms/p0300_longest_increasing_subsequence_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0300_longest_increasing_subsequence import Solution
+
+
+class TestLongestIncreasingSubsequence(unittest.TestCase):
+    def test_longest_increasing_subsequence(self):
+        solution = Solution()
+
+        self.assertEqual(0, solution.lengthOfLIS([]))
+        self.assertEqual(4, solution.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
