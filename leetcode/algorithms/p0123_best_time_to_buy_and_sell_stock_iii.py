19ecdd87bb375c2fb20cc3e4f27e291cf5edad6b
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-29 12:29:14 +0800

    Add 123

---
+++ leetcode/algorithms/p0123_best_time_to_buy_and_sell_stock_iii.py
@@ -0,0 +1,28 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        length = len(prices)
+        profits = [[[0 for _ in range(2)] for _ in range(3)]
+                   for _ in range(length)]
+        profits[0][0][0], profits[0][0][1] = 0, -prices[0]
+        profits[0][1][0], profits[0][1][1] = -sys.maxsize, -sys.maxsize
+        profits[0][2][0], profits[0][2][1] = -sys.maxsize, -sys.maxsize
+
+        for i in range(1, length):
+            profits[i][0][0] = profits[i - 1][0][0]
+            profits[i][0][1] = \
+                max(profits[i - 1][0][1], profits[i - 1][0][0] - prices[i])
+            profits[i][1][0] = \
+                max(profits[i - 1][1][0], profits[i - 1][0][1] + prices[i])
+            profits[i][1][1] = \
+                max(profits[i - 1][1][1], profits[i - 1][1][0] - prices[i])
+            profits[i][2][0] = \
+                max(profits[i - 1][2][0], profits[i - 1][1][1] + prices[i])
+
+        return max(profits[-1][0][0], profits[-1][1][0], profits[-1][2][0])
