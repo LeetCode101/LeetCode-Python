5776c2fd8ab226a14c1b47f64ca006a84f16ae2e
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-30 15:23:50 +0800

    Add 5

---
+++ leetcode/algorithms/p0005_longest_palindromic_substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        length = len(s)
+        start, end = 0, 0
+        dp = [[False for _ in range(length)] for _ in range(length)]
+
+        for i in range(length):
+            dp[i][i] = True
+
+            for j in range(0, i):
+                dp[j][i] = s[i] == s[j] and (i - j == 1 or dp[j + 1][i - 1])
+
+                if dp[j][i] and i - j > end - start:
+                    start, end = j, i
+
+        return s[start:end + 1]
