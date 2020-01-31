20215974e88afdb3165c8b178c30840c203e1095
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-31 15:54:37 +0800

    Add 42

---
+++ leetcode/algorithms/p0042_trapping_rain_water_3.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        count = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
+        while left <= right:
+            left_max = max(left_max, height[left])
+            right_max = max(right_max, height[right])
+
+            if left_max < height[right]:
+                count += left_max - height[left]
+                left += 1
+            else:
+                count += right_max - height[right]
+                right -= 1
+
+        return count
