9a1d02ee9d1a1f603838fa9ebd83b8eb96c65f49
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-31 14:59:18 +0800

    Update 42

--- leetcode/algorithms/p0042_trapping_rain_water_1.py
+++ leetcode/algorithms/p0042_trapping_rain_water_1.py
@@ -8,9 +8,9 @@ class Solution:
         for i in range(1, len(height)):
             left_max = max(height[0:i], default=0)
             right_max = max(height[i+1:], default=0)
-            left_right = min(left_max, right_max)
+            left_right_min = min(left_max, right_max)
 
-            if left_right > height[i]:
-                count += left_right - height[i]
+            if left_right_min > height[i]:
+                count += left_right_min - height[i]
 
         return count
