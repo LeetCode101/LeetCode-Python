adc78ec8f8bc7577f593f71745924626bfc280d1
Frederick-S <mao_xiaodan@hotmail.com>
2020-01-31 15:32:25 +0800

    Update test

--- tests/algorithms/p0042_trapping_rain_water_2_test.py
+++ tests/algorithms/p0042_trapping_rain_water_2_test.py
@@ -6,5 +6,6 @@ class TestTrappingRainWater(unittest.TestCase):
     def test_trapping_rain_water(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.trap([]))
         self.assertEqual(
             6, solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
