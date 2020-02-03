aa920214cc33733ca4ce0d0fb9764b702ab5f6cc
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-03 17:25:26 +0800

    Add 10

---
+++ tests/algorithms/p0010_regular_expression_matching_test.py
@@ -0,0 +1,13 @@
+import unittest
+from leetcode.algorithms.p0010_regular_expression_matching import Solution
+
+
+class TestRegularExpressionMatching(unittest.TestCase):
+    def test_regular_expression_matching(self):
+        solution = Solution()
+
+        self.assertFalse(solution.isMatch('aa', 'a'))
+        self.assertTrue(solution.isMatch('aa', '*'))
+        self.assertTrue(solution.isMatch('ab', '.*'))
+        self.assertTrue(solution.isMatch('aab', 'c*a*b'))
+        self.assertFalse(solution.isMatch('mississippi', 'mis*is*p*.'))
