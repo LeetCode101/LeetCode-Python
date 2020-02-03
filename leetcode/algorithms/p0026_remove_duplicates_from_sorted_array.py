c364047f5af2102cc60ebecf5e8d722f1bfb42ba
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-03 18:47:20 +0800

    Add 26

---
+++ leetcode/algorithms/p0026_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i, length = 0, len(nums)
+        size = 0
+
+        while i < length:
+            j = i
+
+            while j < length and nums[j] == nums[i]:
+                j += 1
+
+            nums[size] = nums[i]
+            size += 1
+            i = j
+
+        return size
---
+++ tests/algorithms/p0026_remove_duplicates_from_sorted_array_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode\
+    .algorithms.p0026_remove_duplicates_from_sorted_array import Solution
+
+
+class TestRemoveDuplicatesFromSortedArray(unittest.TestCase):
+    def test_remove_duplicates_from_sorted_array(self):
+        solution = Solution()
+
+        self.assertEqual(2, solution.removeDuplicates([1, 1, 2]))
+        self.assertEqual(
+            5, solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
