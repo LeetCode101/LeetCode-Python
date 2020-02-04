ee3f564bde1094b38d110829dc1cf0bc5339524c
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-04 12:50:36 +0800

    Add 33

---
+++ leetcode/algorithms/p0033_search_in_rotated_sorted_array.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            middle = low + (high - low) // 2
+
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                if nums[middle] < nums[high] or nums[high] < target:
+                    high = middle - 1
+                else:
+                    low = middle + 1
+            else:
+                if nums[middle] > nums[high] or nums[high] >= target:
+                    low = middle + 1
+                else:
+                    high = middle - 1
+
+        return -1
---
+++ tests/algorithms/p0033_search_in_rotated_sorted_array_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0033_search_in_rotated_sorted_array import Solution
+
+
+class TestSearchInRotatedSortedArray(unittest.TestCase):
+    def test_search_in_rotated_sorted_array(self):
+        solution = Solution()
+
+        self.assertEqual(4, solution.search([4, 5, 6, 7, 0, 1, 2], 0))
+        self.assertEqual(-1, solution.search([4, 5, 6, 7, 0, 1, 2], 3))
