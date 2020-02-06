0d281055f4cd10250b00625dbd5831ef0c866684
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-06 22:51:21 +0800

    Add 1008

---
+++ leetcode/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        return self.build(preorder, 0, len(preorder) - 1)
+
+    def build(self, preorder: List[int], start: int, end: int) -> TreeNode:
+        if start > end:
+            return None
+
+        root_value = preorder[start]
+        right_tree_start = next(
+            (i for i in range(start, end + 1) if preorder[i] > root_value),
+            end + 1)
+
+        root = TreeNode(root_value)
+        root.left = self.build(preorder, start + 1, right_tree_start - 1)
+        root.right = self.build(preorder, right_tree_start, end)
+
+        return root
---
+++ tests/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms\
+    .p1008_construct_binary_search_tree_from_preorder_traversal import Solution
+
+
+class TestConstructBinarySearchTreeFromPreorderTraversal(unittest.TestCase):
+    def test_construct_binary_search_tree_from_preorder_traversal(self):
+        solution = Solution()
+
+        self.assertEqual(4, solution.bstFromPreorder([4, 2]).val)
+        self.assertEqual(8, solution.bstFromPreorder([8, 5, 1, 7, 10, 12]).val)
