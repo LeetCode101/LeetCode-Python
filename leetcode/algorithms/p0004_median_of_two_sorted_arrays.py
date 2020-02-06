6fa8c3d7ef7a53fe91510ed653d4addcac028c85
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-06 16:01:10 +0800

    Add 4

---
+++ leetcode/algorithms/p0004_median_of_two_sorted_arrays.py
@@ -0,0 +1,33 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) \
+            -> float:
+        m, n = len(nums1), len(nums2)
+        left, right = (m + n + 1) // 2, (m + n + 2) // 2
+
+        return (self.find_kth(nums1, 0, nums2, 0, left) +
+                self.find_kth(nums1, 0, nums2, 0, right)) / 2
+
+    def find_kth(self, nums1: List[int], i: int,
+                 nums2: List[int], j: int, k: int) -> int:
+        if i >= len(nums1):
+            return nums2[j + k - 1]
+
+        if j >= len(nums2):
+            return nums1[i + k - 1]
+
+        if k == 1:
+            return min(nums1[i], nums2[j])
+
+        middle_value1 = nums1[i + k // 2 - 1] if i + k // 2 - 1 < len(nums1) \
+            else sys.maxsize
+        middle_value2 = nums2[j + k // 2 - 1] if j + k // 2 - 1 < len(nums2) \
+            else sys.maxsize
+
+        if middle_value1 < middle_value2:
+            return self.find_kth(nums1, i + k // 2, nums2, j, k - k // 2)
+        else:
+            return self.find_kth(nums1, i, nums2, j + k // 2, k - k // 2)
---
+++ tests/algorithms/p0004_median_of_two_sorted_arrays_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0004_median_of_two_sorted_arrays import Solution
+
+
+class TestMedianOfTwoSortedArrays(unittest.TestCase):
+    def test_median_of_two_sorted_arrays(self):
+        solution = Solution()
+
+        self.assertEqual(2.0, solution.findMedianSortedArrays([1, 3], [2]))
+        self.assertEqual(2.5, solution.findMedianSortedArrays([1, 2], [3, 4]))
