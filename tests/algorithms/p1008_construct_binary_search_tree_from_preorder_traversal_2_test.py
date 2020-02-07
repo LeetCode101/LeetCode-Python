4da3304fb3c788631f404aa053a61e22b2a2075c
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-07 12:07:51 +0800

    Update test

--- tests/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal_2_test.py
+++ tests/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal_2_test.py
@@ -8,5 +8,6 @@ class TestConstructBinarySearchTreeFromPreorderTraversal(unittest.TestCase):
     def test_construct_binary_search_tree_from_preorder_traversal(self):
         solution = Solution()
 
+        self.assertIsNone(solution.bstFromPreorder([]))
         self.assertEqual(4, solution.bstFromPreorder([4, 2]).val)
         self.assertEqual(8, solution.bstFromPreorder([8, 5, 1, 7, 10, 12]).val)
