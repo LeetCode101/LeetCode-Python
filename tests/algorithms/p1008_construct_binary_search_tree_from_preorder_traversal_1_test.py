a3e5bcc29e83088a18a3b37c6498497a8535a8d8
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-07 11:12:12 +0800

    Rename test

--- tests/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal_1_test.py
+++ tests/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal_1_test.py
@@ -1,6 +1,6 @@
 import unittest
 from leetcode.algorithms\
-    .p1008_construct_binary_search_tree_from_preorder_traversal import Solution
+    .p1008_construct_binary_search_tree_from_preorder_traversal_1 import Solution
 
 
 class TestConstructBinarySearchTreeFromPreorderTraversal(unittest.TestCase):
