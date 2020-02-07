dd895865e45ab88c5260f8e84c892133aa8bf0b3
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-07 22:22:04 +0800

    Add 273

---
+++ leetcode/algorithms/p0273_integer_to_english_words.py
@@ -0,0 +1,98 @@
+import math
+
+
+class Solution:
+    def __init__(self):
+        self.less_than_10 = {
+            '0': 'Zero',
+            '1': 'One',
+            '2': 'Two',
+            '3': 'Three',
+            '4': 'Four',
+            '5': 'Five',
+            '6': 'Six',
+            '7': 'Seven',
+            '8': 'Eight',
+            '9': 'Nine'
+        }
+        self.less_than_20 = {
+            '10': 'Ten',
+            '11': 'Eleven',
+            '12': 'Twelve',
+            '13': 'Thirteen',
+            '14': 'Fourteen',
+            '15': 'Fifteen',
+            '16': 'Sixteen',
+            '17': 'Seventeen',
+            '18': 'Eighteen',
+            '19': 'Nineteen'
+        }
+        self.larger_than_20 = {
+            '2': 'Twenty',
+            '3': 'Thirty',
+            '4': 'Forty',
+            '5': 'Fifty',
+            '6': 'Sixty',
+            '7': 'Seventy',
+            '8': 'Eighty',
+            '9': 'Ninety'
+        }
+
+    def numberToWords(self, num: int) -> str:
+        number_string = str(num)
+        length = len(number_string)
+        i = 0
+        words = []
+        groups = {
+            1: '',
+            2: ' Thousand',
+            3: ' Million',
+            4: ' Billion'
+        }
+
+        while i < length:
+            high_digits_length = (length - i) % 3
+            group = math.ceil((length - i) / 3)
+            high_digits = number_string[i:i + high_digits_length] \
+                if high_digits_length != 0 else number_string[i:i + 3]
+            digits = self.trim_zero(high_digits)
+
+            if digits:
+                words.append(self.parse_three_digits(digits) +
+                             groups.get(group, ''))
+
+            i += len(high_digits)
+
+        return ' '.join(words)
+
+    def parse_three_digits(self, num: str) -> str:
+        if len(num) == 3:
+            left = int(num[1:])
+
+            return self.parse_one_digit(num[0]) + ' Hundred' + \
+                (' ' + self.parse_two_digits(str(left)) if left != 0 else '')
+        else:
+            return self.parse_two_digits(num)
+
+    def parse_two_digits(self, num: str) -> str:
+        if len(num) == 1:
+            return self.parse_one_digit(num)
+        elif int(num) < 20:
+            return self.less_than_20[num]
+        else:
+            return self.larger_than_20[num[0]] + \
+                (' ' + self.less_than_10[num[1]] if num[1] != '0' else '')
+
+    def parse_one_digit(self, num: str) -> str:
+        return self.less_than_10[num]
+
+    def trim_zero(self, num: str) -> str:
+        if len(num) == 1:
+            return num
+
+        i = 0
+
+        while i < len(num) and num[i] == '0':
+            i += 1
+
+        return num[i:]
---
+++ tests/algorithms/p0273_integer_to_english_words_test.py
@@ -0,0 +1,16 @@
+import unittest
+from leetcode.algorithms.p0273_integer_to_english_words import Solution
+
+
+class TestIntegerToEnglishWords(unittest.TestCase):
+    def test_integer_to_english_words(self):
+        solution = Solution()
+        words = 'One Billion Two Hundred Thirty Four Million Five ' \
+                'Hundred Sixty Seven Thousand Eight Hundred Ninety One'
+
+        self.assertEqual('One Thousand', solution.numberToWords(1000))
+        self.assertEqual('One Thousand One', solution.numberToWords(1001))
+        self.assertEqual('Ten', solution.numberToWords(10))
+        self.assertEqual('One Hundred', solution.numberToWords(100))
+        self.assertEqual('Twenty', solution.numberToWords(20))
+        self.assertEqual(words, solution.numberToWords(1234567891))
