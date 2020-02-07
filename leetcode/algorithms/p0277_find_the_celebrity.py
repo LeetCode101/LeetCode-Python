d95a2321be8a89ca493b15e44ff964e48da34c91
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-07 19:54:45 +0800

    Update test

---
+++ leetcode/algorithms/p0277_find_the_celebrity.py
@@ -0,0 +1,43 @@
+def knows(a: int, b: int) -> bool:
+    graph = [
+        [1, 1, 0, 1],
+        [0, 1, 0, 1],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]
+
+    return graph[a][b] == 1
+
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        possible_celebrityies = []
+
+        for i in range(n):
+            know_someone = False
+
+            for j in range(n):
+                if i == j:
+                    continue
+
+                if knows(i, j):
+                    know_someone = True
+
+                    break
+
+            if not know_someone:
+                possible_celebrityies.append(i)
+
+        for i in possible_celebrityies:
+            is_celebrity = True
+
+            for j in range(n):
+                if not knows(j, i):
+                    is_celebrity = False
+
+                    break
+
+            if is_celebrity:
+                return i
+
+        return -1
