5b7e73239bb64bad0042cab789b9b1bd104c2999
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-07 11:59:30 +0800

    Add 1008

---
+++ leetcode/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal_2.py
@@ -0,0 +1,34 @@
+import sys
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+
+        n = len(preorder)
+        root = TreeNode(preorder[0])
+        stack = [root]
+
+        for i in range(1, n):
+            node, child = stack[-1], TreeNode(preorder[i])
+
+            while stack and stack[-1].val < child.val:
+                node = stack.pop()
+
+            if node.val > child.val:
+                node.left = child
+            else:
+                node.right = child
+
+            stack.append(child)
+
+        return root
---
+++ tests/algorithms/p1008_construct_binary_search_tree_from_preorder_traversal_2_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms\
+    .p1008_construct_binary_search_tree_from_preorder_traversal_2 \
+    import Solution
+
+
+class TestConstructBinarySearchTreeFromPreorderTraversal(unittest.TestCase):
+    def test_construct_binary_search_tree_from_preorder_traversal(self):
+        solution = Solution()
+
+        self.assertEqual(4, solution.bstFromPreorder([4, 2]).val)
+        self.assertEqual(8, solution.bstFromPreorder([8, 5, 1, 7, 10, 12]).val)
