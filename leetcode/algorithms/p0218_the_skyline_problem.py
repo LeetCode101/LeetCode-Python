4c93815a55ac0b4c52889d84440bd5276d971248
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-08 15:58:25 +0800

    Add 218

--- leetcode/algorithms/p0218_the_skyline_problem.py
+++ leetcode/algorithms/p0218_the_skyline_problem.py
@@ -3,4 +3,46 @@ from typing import List
 
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        pass
+        if not buildings:
+            return []
+
+        if len(buildings) == 1:
+            return [[buildings[0][0], buildings[0][2]], [buildings[0][1], 0]]
+
+        middle = len(buildings) // 2
+        left_skyline = self.getSkyline(buildings[:middle])
+        right_skyline = self.getSkyline(buildings[middle:])
+
+        return self.merge(left_skyline, right_skyline)
+
+    def merge(self, left_skyline, right_skyline):
+        left_height, right_height = 0, 0
+        left, right = 0, 0
+        result = []
+
+        while left < len(left_skyline) and right < len(right_skyline):
+            if left_skyline[left][0] < right_skyline[right][0]:
+                current_point = [left_skyline[left][0],
+                                 max(left_skyline[left][1], right_height)]
+                left_height = left_skyline[left][1]
+                left += 1
+            elif left_skyline[left][0] > right_skyline[right][0]:
+                current_point = [right_skyline[right][0],
+                                 max(right_skyline[right][1], left_height)]
+                right_height = right_skyline[right][1]
+                right += 1
+            else:
+                current_point = [
+                    left_skyline[left][0],
+                    max(left_skyline[left][1], right_skyline[right][1])]
+                left_height = left_skyline[left][1]
+                right_height = right_skyline[right][1]
+                left += 1
+                right += 1
+
+            if len(result) == 0 or result[-1][1] != current_point[1]:
+                result.append(current_point)
+
+        result.extend(left_skyline[left:] or right_skyline[right:])
+
+        return result
