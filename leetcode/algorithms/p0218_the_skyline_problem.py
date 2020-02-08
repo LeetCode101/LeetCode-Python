2a97cb65388ab9f2ca107e277c8c3939a5b856ff
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-08 16:02:30 +0800

    Update 218

--- leetcode/algorithms/p0218_the_skyline_problem.py
+++ leetcode/algorithms/p0218_the_skyline_problem.py
@@ -22,17 +22,17 @@ class Solution:
 
         while left < len(left_skyline) and right < len(right_skyline):
             if left_skyline[left][0] < right_skyline[right][0]:
-                current_point = [left_skyline[left][0],
+                skyline = [left_skyline[left][0],
                                  max(left_skyline[left][1], right_height)]
                 left_height = left_skyline[left][1]
                 left += 1
             elif left_skyline[left][0] > right_skyline[right][0]:
-                current_point = [right_skyline[right][0],
+                skyline = [right_skyline[right][0],
                                  max(right_skyline[right][1], left_height)]
                 right_height = right_skyline[right][1]
                 right += 1
             else:
-                current_point = [
+                skyline = [
                     left_skyline[left][0],
                     max(left_skyline[left][1], right_skyline[right][1])]
                 left_height = left_skyline[left][1]
@@ -40,8 +40,8 @@ class Solution:
                 left += 1
                 right += 1
 
-            if len(result) == 0 or result[-1][1] != current_point[1]:
-                result.append(current_point)
+            if len(result) == 0 or result[-1][1] != skyline[1]:
+                result.append(skyline)
 
         result.extend(left_skyline[left:] or right_skyline[right:])
 
