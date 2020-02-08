59d80dc0b9e85195d642046db8d14df88ce69dd4
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-08 16:10:28 +0800

    Update test

--- tests/algorithms/p0218_the_skyline_problem_test.py
+++ tests/algorithms/p0218_the_skyline_problem_test.py
@@ -6,9 +6,9 @@ from leetcode.algorithms.p0218_the_skyline_problem \
 class TestTheSkylineProblem(unittest.TestCase):
     def test_the_skyline_problem(self):
         solution = Solution()
-        buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 12],
+        buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 15],
                      [15, 20, 10], [19, 24, 8]]
-        skyline = [[2, 10], [3, 15], [7, 12], [12, 0],
+        skyline = [[2, 10], [3, 15], [12, 0],
                    [15, 10], [20, 8], [24, 0]]
 
         self.assertListEqual(skyline, solution.getSkyline(buildings))
