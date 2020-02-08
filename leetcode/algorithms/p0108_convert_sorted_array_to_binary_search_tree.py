cba039696478c45acd01e31898afa797ba4dc607
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-08 22:32:42 +0800

    Add 108

---
+++ leetcode/algorithms/p0108_convert_sorted_array_to_binary_search_tree.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        return self.build_tree(nums, 0, len(nums) - 1)
+
+    def build_tree(self, nums: List[int], low: int, high: int) -> TreeNode:
+        if low > high:
+            return None
+
+        middle = (low + high) // 2
+        root = TreeNode(nums[middle])
+        root.left = self.build_tree(nums, low, middle - 1)
+        root.right = self.build_tree(nums, middle + 1, high)
+
+        return root
---
+++ tests/algorithms/p0108_convert_sorted_array_to_binary_search_tree_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0108_convert_sorted_array_to_binary_search_tree \
+    import Solution
+
+
+class TestConvertSortedArrayToBinarySearchTree(unittest.TestCase):
+    def test_convert_sorted_array_to_binary_search_tree(self):
+        solution = Solution()
+
+        self.assertEqual(0, solution.sortedArrayToBST([-10, -3, 0, 5, 9]))
