6a3fb764125a361b56fbaba1d22e5ed28ebbea20
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-09 12:33:12 +0800

    Update test

--- tests/algorithms/p0028_implement_strstr_test.py
+++ tests/algorithms/p0028_implement_strstr_test.py
@@ -6,5 +6,7 @@ class TestImplementStrStr(unittest.TestCase):
     def test_implement_strstr(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.strStr('hi', ''))
         self.assertEqual(2, solution.strStr('hello', 'll'))
         self.assertEqual(-1, solution.strStr('aaaaa', 'bba'))
+        self.assertEqual(-1, solution.strStr('aaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab'))
