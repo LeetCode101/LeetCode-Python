b51da2dcc77197b0b10c009e51c05f0d58f6d903
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-10 14:32:06 +0800

    Update test

--- tests/algorithms/p0402_remove_k_digits_test.py
+++ tests/algorithms/p0402_remove_k_digits_test.py
@@ -8,3 +8,4 @@ class TestRemoveKDigits(unittest.TestCase):
 
         self.assertEqual('1219', solution.removeKdigits('1432219', 3))
         self.assertEqual('0', solution.removeKdigits('9', 1))
+        self.assertEqual('1', solution.removeKdigits('1001', 1))
