3a2291ef3f47185ced78f18db77e4c10e8d63ba8
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-11 11:05:34 +0800

    Update test

--- tests/algorithms/p0290_word_pattern_test.py
+++ tests/algorithms/p0290_word_pattern_test.py
@@ -6,5 +6,6 @@ class TestWordPattern(unittest.TestCase):
     def test_word_pattern(self):
         solution = Solution()
 
+        self.assertFalse(solution.wordPattern('a', 'b c'))
         self.assertTrue(solution.wordPattern('abba', 'dog cat cat dog'))
-        self.assertFalse('abba', 'dog cat cat fish')
+        self.assertFalse(solution.wordPattern('abba', 'dog cat cat fish'))
