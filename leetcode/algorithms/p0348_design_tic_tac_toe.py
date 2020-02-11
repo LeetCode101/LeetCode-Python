74edd16e61eff9ca62ee10c3db20ee376b67e101
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-11 18:59:40 +0800

    Update 348

--- leetcode/algorithms/p0348_design_tic_tac_toe.py
+++ leetcode/algorithms/p0348_design_tic_tac_toe.py
@@ -1,4 +1,3 @@
-# Time Limit Exceeded!
 class TicTacToe:
     def __init__(self, n: int):
         self.board = [['' for _ in range(n)] for _ in range(n)]
@@ -9,29 +8,29 @@ class TicTacToe:
         result = 0
 
         if player == 1:
-            result = 1 if self.is_player_win(1) else 0
+            result = 1 if self.is_player_win(1, row, col) else 0
         elif player == 2:
-            result = 2 if self.is_player_win(2) else 0
+            result = 2 if self.is_player_win(2, row, col) else 0
 
         return result
 
-    def is_player_win(self, player: int) -> bool:
+    def is_player_win(self, player: int, row: int, column: int) -> bool:
         mark = 'X' if player == 1 else 'O'
         n = len(self.board)
 
-        for i in range(n):
-            if all(x == mark for x in self.board[i]):
-                return True
-
-        for i in range(n):
-            if all(x == mark for x in [self.board[j][i] for j in range(n)]):
-                return True
-
-        if all(x == mark for x in [self.board[i][i] for i in range(n)]):
+        if all(x == mark for x in [self.board[row][i] for i in range(n)]):
             return True
 
-        if all(x == mark for x in
-               [self.board[i][n - 1 - i] for i in range(n)]):
+        if all(x == mark for x in [self.board[i][column] for i in range(n)]):
+            return True
+
+        if row == column and \
+                all(x == mark for x in [self.board[i][i] for i in range(n)]):
+            return True
+
+        if row + column == n - 1 and \
+                all(x == mark for x in
+                    [self.board[i][n - 1 - i] for i in range(n)]):
             return True
 
         return False
--- tests/algorithms/p0348_design_tic_tac_toe_test.py
+++ tests/algorithms/p0348_design_tic_tac_toe_test.py
@@ -5,6 +5,7 @@ from leetcode.algorithms.p0348_design_tic_tac_toe import TicTacToe
 class TestDesignTicTacToe(unittest.TestCase):
     def test_design_tic_tac_toe(self):
         tic_tac_toe = TicTacToe(3)
+        tic_tac_toe2 = TicTacToe(2)
 
         self.assertEqual(0, tic_tac_toe.move(0, 0, 1))
         self.assertEqual(0, tic_tac_toe.move(0, 2, 2))
@@ -13,3 +14,6 @@ class TestDesignTicTacToe(unittest.TestCase):
         self.assertEqual(0, tic_tac_toe.move(2, 0, 1))
         self.assertEqual(0, tic_tac_toe.move(1, 0, 2))
         self.assertEqual(1, tic_tac_toe.move(2, 1, 1))
+        self.assertEqual(0, tic_tac_toe2.move(0, 0, 2))
+        self.assertEqual(0, tic_tac_toe2.move(0, 1, 1))
+        self.assertEqual(2, tic_tac_toe2.move(1, 1, 2))
