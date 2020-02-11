aab6fd13c5dc33b9d8f25ad13c2f08e1a462cc0d
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-11 11:10:32 +0800

    Add 250

---
+++ leetcode/algorithms/p0250_count_univalue_subtrees.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.count = 0
+
+    def countUnivalSubtrees(self, root: TreeNode) -> int:
+        self.is_uni(root)
+
+        return self.count
+
+    def is_uni(self, root):
+        if not root:
+            return True
+
+        is_left_uni = not root.left \
+            or (self.is_uni(root.left) and root.val == root.left.val)
+        is_right_uni = not root.right \
+            or (self.is_uni(root.right) and root.val == root.right.val)
+
+        if is_left_uni and is_right_uni:
+            self.count += 1
+
+            return True
+        else:
+            return False
---
+++ tests/algorithms/p0250_count_univalue_subtrees_test.py
@@ -0,0 +1,18 @@
+import unittest
+from leetcode.algorithms.p0250_count_univalue_subtrees \
+    import TreeNode, Solution
+
+
+class TestCountUnivalueSubtrees(unittest.TestCase):
+    def test_count_univalue_subtrees(self):
+        solution = Solution()
+        a = TreeNode(5)
+        b = TreeNode(1)
+        c = TreeNode(5)
+        a.left = b
+        a.right = c
+        b.left = TreeNode(5)
+        b.right = TreeNode(5)
+        c.right = TreeNode(5)
+
+        self.assertEqual(4, solution.countUnivalSubtrees(a))
