4e157b62d439f3de068b7d462a44cc1b3ed6c02c
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-11 13:46:38 +0800

    Update test

--- tests/algorithms/p0165_compare_version_numbers_test.py
+++ tests/algorithms/p0165_compare_version_numbers_test.py
@@ -6,6 +6,8 @@ class TestCompareVersionNumbers(unittest.TestCase):
     def test_compare_version_numbers(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.compareVersion('1.1', '1.1.000'))
+        self.assertEqual(1, solution.compareVersion('1.2', '1.1'))
         self.assertEqual(1, solution.compareVersion('1.0.1', '1'))
         self.assertEqual(-1, solution.compareVersion('7.5.2.4', '7.5.3'))
         self.assertEqual(0, solution.compareVersion('1.01', '1.001'))
--- tests/algorithms/p0509_fibonacci_number_test.py
+++ tests/algorithms/p0509_fibonacci_number_test.py
@@ -6,5 +6,6 @@ class TestFibonacciNumber(unittest.TestCase):
     def test_fibonacci_number(self):
         solution = Solution()
 
+        self.assertEqual(1, solution.fib(1))
         self.assertEqual(1, solution.fib(2))
         self.assertEqual(2, solution.fib(3))
