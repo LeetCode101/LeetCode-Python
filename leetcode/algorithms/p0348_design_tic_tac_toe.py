e23328bb4bcbbdca8786d68272200ac533f26e27
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-11 18:43:38 +0800

    Update 348

--- leetcode/algorithms/p0348_design_tic_tac_toe.py
+++ leetcode/algorithms/p0348_design_tic_tac_toe.py
@@ -1,3 +1,4 @@
+# Time Limit Exceeded!
 class TicTacToe:
     def __init__(self, n: int):
         self.board = [['' for _ in range(n)] for _ in range(n)]
@@ -5,12 +6,14 @@ class TicTacToe:
     def move(self, row: int, col: int, player: int) -> int:
         self.board[row][col] = 'X' if player == 1 else 'O'
 
-        if self.is_player_win(1):
-            return 1
-        elif self.is_player_win(2):
-            return 2
-        else:
-            return 0
+        result = 0
+
+        if player == 1:
+            result = 1 if self.is_player_win(1) else 0
+        elif player == 2:
+            result = 2 if self.is_player_win(2) else 0
+
+        return result
 
     def is_player_win(self, player: int) -> bool:
         mark = 'X' if player == 1 else 'O'
