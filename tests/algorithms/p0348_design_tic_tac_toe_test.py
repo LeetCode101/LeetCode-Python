6020aeca69256e8f8fca4b345b693b29a536caec
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-11 19:07:44 +0800

    Update test

--- tests/algorithms/p0348_design_tic_tac_toe_test.py
+++ tests/algorithms/p0348_design_tic_tac_toe_test.py
@@ -6,6 +6,8 @@ class TestDesignTicTacToe(unittest.TestCase):
     def test_design_tic_tac_toe(self):
         tic_tac_toe = TicTacToe(3)
         tic_tac_toe2 = TicTacToe(2)
+        tic_tac_toe3 = TicTacToe(2)
+        tic_tac_toe4 = TicTacToe(2)
 
         self.assertEqual(0, tic_tac_toe.move(0, 0, 1))
         self.assertEqual(0, tic_tac_toe.move(0, 2, 2))
@@ -17,3 +19,9 @@ class TestDesignTicTacToe(unittest.TestCase):
         self.assertEqual(0, tic_tac_toe2.move(0, 0, 2))
         self.assertEqual(0, tic_tac_toe2.move(0, 1, 1))
         self.assertEqual(2, tic_tac_toe2.move(1, 1, 2))
+        self.assertEqual(0, tic_tac_toe3.move(0, 0, 2))
+        self.assertEqual(0, tic_tac_toe3.move(0, 1, 1))
+        self.assertEqual(2, tic_tac_toe3.move(1, 0, 2))
+        self.assertEqual(0, tic_tac_toe4.move(0, 1, 2))
+        self.assertEqual(0, tic_tac_toe4.move(0, 0, 1))
+        self.assertEqual(2, tic_tac_toe4.move(1, 0, 2))
