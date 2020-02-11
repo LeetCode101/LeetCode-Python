0875c937e12ec1c11f91ddb11ac3858233765c43
Frederick-S <mao_xiaodan@hotmail.com>
2020-02-11 11:32:53 +0800

    Add 165

---
+++ leetcode/algorithms/p0165_compare_version_numbers.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1 = version1.split('.')
+        v2 = version2.split('.')
+        n1, n2 = len(v1), len(v2)
+        n = min(n1, n2)
+
+        for i in range(n):
+            if int(v1[i]) > int(v2[i]):
+                return 1
+            elif int(v1[i]) < int(v2[i]):
+                return -1
+
+        if n1 < n2:
+            return 0 if self.is_all_zero(v2, n) else -1
+        elif n1 > n2:
+            return 0 if self.is_all_zero(v1, n) else 1
+        else:
+            return 0
+
+    def is_all_zero(self, v: List[str], i: int) -> bool:
+        for j in range(i, len(v)):
+            if int(v[j]) != 0:
+                return False
+
+        return True
---
+++ tests/algorithms/p0165_compare_version_numbers_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p0165_compare_version_numbers import Solution
+
+
+class TestCompareVersionNumbers(unittest.TestCase):
+    def test_compare_version_numbers(self):
+        solution = Solution()
+
+        self.assertEqual(1, solution.compareVersion('1.0.1', '1'))
+        self.assertEqual(-1, solution.compareVersion('7.5.2.4', '7.5.3'))
+        self.assertEqual(0, solution.compareVersion('1.01', '1.001'))
