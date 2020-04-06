c8e211c9b42c17d2869c9fbd6c1932a9edb13d24
Frederick-S <mao_xiaodan@hotmail.com>
2020-04-06 17:32:34 +0800

    refactor: use enumerate

--- leetcode/algorithms/p0003_longest_substring_without_repeating_characters_2.py
+++ leetcode/algorithms/p0003_longest_substring_without_repeating_characters_2.py
@@ -1,13 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_length = 0
-        n = len(s)
         i = 0
+        max_length = 0
         substring = {}
 
-        for j in range(n):
-            if s[j] in substring:
-                index = substring[s[j]]
+        for j, c in enumerate(s):
+            if c in substring:
+                index = substring[c]
 
                 for k in range(i, index + 1):
                     del substring[s[k]]
@@ -15,6 +14,6 @@ class Solution:
                 i = index + 1
 
             max_length = max(max_length, j - i + 1)
-            substring[s[j]] = j
+            substring[c] = j
 
         return max_length
--- leetcode/algorithms/p0003_longest_substring_without_repeating_characters_3.py
+++ leetcode/algorithms/p0003_longest_substring_without_repeating_characters_3.py
@@ -1,15 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_length = 0
-        n = len(s)
         i = 0
+        max_length = 0
         substring = {}
 
-        for j in range(n):
-            if s[j] in substring:
-                i = max(substring[s[j]] + 1, i)
+        for j, c in enumerate(s):
+            if c in substring:
+                i = max(substring[c] + 1, i)
 
             max_length = max(max_length, j - i + 1)
-            substring[s[j]] = j
+            substring[c] = j
 
         return max_length
