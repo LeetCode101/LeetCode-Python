c9d6dccc41216ccd57afadfe7da2d57bc230537a
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-03 08:26:24 +0800

    rename file

--- tests/algorithms/p0977_squares_of_a_sorted_array_1_test.py
+++ tests/algorithms/p0977_squares_of_a_sorted_array_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0977_squares_of_a_sorted_array import Solution
+from leetcode.algorithms.p0977_squares_of_a_sorted_array_1 import Solution
 
 
 class TestSquaresOfASortedArray(unittest.TestCase):
