be2f7dd3b75016693de05554d05a810e56808b05
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-03 11:19:25 +0800

    Update p0977_squares_of_a_sorted_array_2.py

--- leetcode/algorithms/p0977_squares_of_a_sorted_array_2.py
+++ leetcode/algorithms/p0977_squares_of_a_sorted_array_2.py
@@ -5,15 +5,18 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         result = [0] * len(nums)
         low, high = 0, len(nums) - 1
+        index = len(nums) - 1
 
         while low <= high:
             left, right = abs(nums[low]), abs(nums[high])
 
             if left > right:
-                result[high - low] = left * left
+                result[index] = left * left
                 low += 1
             else:
-                result[high - low] = right * right
+                result[index] = right * right
                 high -= 1
+
+            index -= 1
 
         return result
