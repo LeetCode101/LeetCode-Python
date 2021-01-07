0165ac48d172e23f3cdf31bb7cde9b087b55d9f1
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-07 22:23:09 +0800

    p0237

---
+++ tests/algorithms/p0237_delete_node_in_a_linked_list_test.py
@@ -0,0 +1,21 @@
+import unittest
+from leetcode.algorithms.p0237_delete_node_in_a_linked_list import Solution
+from leetcode.algorithms.p0237_delete_node_in_a_linked_list import ListNode
+from .list_helper import convert_linked_list_to_list
+
+
+class DeleteNodeInALinkedList(unittest.TestCase):
+    def test_delete_node_in_a_linked_list(self):
+        solution = Solution()
+        a = ListNode(4)
+        b = ListNode(5)
+        c = ListNode(2)
+        d = ListNode(9)
+        a.next = b
+        b.next = c
+        c.next = d
+
+        solution.deleteNode(b)
+
+        self.assertListEqual(
+            [2, 9], convert_linked_list_to_list(b))
