f3ddb3cba35d8d79ee4d50d43976006e218d10c2
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-10 15:22:59 +0800

    Update p0267_palindrome_permutation_ii_2.py

--- leetcode/algorithms/p0267_palindrome_permutation_ii_2.py
+++ leetcode/algorithms/p0267_palindrome_permutation_ii_2.py
@@ -20,21 +20,21 @@ class Solution:
         if len(chars) > 1:
             return []
 
-        result = []
         middle = next(iter(chars)) if len(chars) == 1 else ''
-        self.generate(counts, middle, result, len(s))
+        return self.generate(counts, middle, len(s))
 
-        return result
-
-    def generate(self, counts, permutation_so_far, result, target_length):
+    def generate(self, counts, permutation_so_far, target_length):
         if len(permutation_so_far) == target_length:
-            result.append(permutation_so_far)
+            return [permutation_so_far]
+
+        permutations = []
 
         for key in counts:
             if counts[key] <= 1:
                 continue
 
             counts[key] -= 2
-            self.generate(counts, key + permutation_so_far + key, result, target_length)
-
+            permutations += self.generate(counts, key + permutation_so_far + key, target_length)
             counts[key] += 2
+
+        return permutations
