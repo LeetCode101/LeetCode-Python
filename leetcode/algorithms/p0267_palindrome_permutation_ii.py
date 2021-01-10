0fb62c323858b9e9e5927831b801ab5496f0c9ed
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-10 12:11:59 +0800

    p267

---
+++ leetcode/algorithms/p0267_palindrome_permutation_ii.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+class Solution:
+    def generatePalindromes(self, s: str) -> List[str]:
+        chars = set()
+        counts = {}
+        used_counts = {}
+
+        for c in s:
+            if c in chars:
+                chars.remove(c)
+            else:
+                chars.add(c)
+
+            if c in counts:
+                counts[c] += 1
+            else:
+                counts[c] = 1
+                used_counts[c] = 0
+
+        if len(chars) > 1:
+            return []
+
+        middle = next(iter(chars)) if len(chars) == 1 else ''
+        permutations = self.generate(counts, used_counts)
+
+        if permutations:
+            return [permutation + middle + permutation[::-1]
+                    for permutation in permutations]
+        else:
+            return [middle] if middle else []
+
+    def generate(self, counts, used_counts):
+        permutations = []
+
+        for key, count in counts.items():
+            if count // 2 == used_counts[key]:
+                continue
+
+            used_counts[key] += 1
+            rest_permutations = self.generate(counts, used_counts)
+
+            if not rest_permutations:
+                permutations += [key]
+            else:
+                permutations += [key + permutation
+                                 for permutation in rest_permutations]
+
+            used_counts[key] -= 1
+
+        return permutations
---
+++ tests/algorithms/p0267_palindrome_permutation_ii_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0267_palindrome_permutation_ii import Solution
+
+
+class TestPalindromePermutationII(unittest.TestCase):
+    def test_palindrome_permutation(self):
+        solution = Solution()
+
+        self.assertListEqual(['abba', 'baab'],
+                             solution.generatePalindromes('aabb'))
