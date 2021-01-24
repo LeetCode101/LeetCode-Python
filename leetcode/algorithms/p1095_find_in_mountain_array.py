58d1503558182bf9746487aab70b98a7c7644eae
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-24 18:06:16 +0800

    p1095

---
+++ leetcode/algorithms/p1095_find_in_mountain_array.py
@@ -0,0 +1,54 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+class MountainArray:
+    def __init__(self, array):
+        self.array = array
+
+    def get(self, index: int) -> int:
+        return self.array[index]
+
+    def length(self) -> int:
+        return len(self.array)
+
+
+class Solution:
+    def findInMountainArray(self, target: int,
+                            mountain_arr: 'MountainArray') -> int:
+        peak = self.find_peak(0, mountain_arr.length() - 1, mountain_arr)
+
+        if mountain_arr.get(peak) == target:
+            return peak
+
+        left = self.binary_search(0, peak - 1, mountain_arr, target, True)
+
+        return left if left != -1 else self.binary_search(
+            peak + 1, mountain_arr.length() - 1, mountain_arr, target, False)
+
+    def find_peak(self, low, high, mountain_arr: 'MountainArray'):
+        while low < high:
+            middle = low + (high - low) // 2
+
+            if mountain_arr.get(middle) < mountain_arr.get(middle + 1):
+                low = middle + 1
+            else:
+                high = middle
+
+        return low
+
+    def binary_search(self, low, high, mountain_arr: 'MountainArray',
+                      target, increasing):
+        while low <= high:
+            middle = low + (high - low) // 2
+            value = mountain_arr.get(middle)
+
+            if value == target:
+                return middle
+            elif (value > target and increasing) \
+                    or (value < target and not increasing):
+                high = middle - 1
+            else:
+                low = middle + 1
+
+        return -1
---
+++ tests/algorithms/p1095_find_in_mountain_array_test.py
@@ -0,0 +1,13 @@
+import unittest
+from leetcode.algorithms.p1095_find_in_mountain_array \
+    import Solution, MountainArray
+
+
+class TestFindInMountainArray(unittest.TestCase):
+    def test_find_in_mountain_array(self):
+        solution = Solution()
+
+        self.assertEqual(2, solution.findInMountainArray(
+            3, MountainArray([1, 2, 3, 4, 5, 3, 1])))
+        self.assertEqual(-1, solution.findInMountainArray(
+            3, MountainArray([0, 1, 2, 4, 2, 1])))
