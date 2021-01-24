3cfc45956e84688a5e4b921bfca46bede10357db
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-24 15:59:47 +0800

    rename file

--- tests/algorithms/p0852_peak_index_in_a_mountain_array_1_test.py
+++ tests/algorithms/p0852_peak_index_in_a_mountain_array_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0852_peak_index_in_a_mountain_array import Solution
+from leetcode.algorithms.p0852_peak_index_in_a_mountain_array_1 import Solution
 
 
 class TestPeakIndexInAMountainArray(unittest.TestCase):
