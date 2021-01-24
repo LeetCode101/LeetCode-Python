98e3ba2648dc779a3e93a019ed250ddde0404865
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-24 15:14:22 +0800

    Update p0845_longest_mountain_in_array_test.py

--- tests/algorithms/p0845_longest_mountain_in_array_test.py
+++ tests/algorithms/p0845_longest_mountain_in_array_test.py
@@ -6,6 +6,7 @@ class TestLongestMountainInArray(unittest.TestCase):
     def test_longest_mountain_in_array(self):
         solution = Solution()
 
-        self.assertEqual(0, solution.longestMountain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+        self.assertEqual(0, solution.longestMountain(
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
         self.assertEqual(0, solution.longestMountain([2, 2, 2]))
         self.assertEqual(5, solution.longestMountain([2, 1, 4, 7, 3, 2, 5]))
