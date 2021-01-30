07a39b8b25b4293a3e352bd40bff6bc8f7c98193
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-30 15:35:44 +0800

    p1299

---
+++ leetcode/algorithms/p1299_replace_elements_with_greatest_element_on_right_side.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        length = len(arr)
+
+        for i in range(length):
+            if i == length - 1:
+                arr[i] = -1
+
+                break
+
+            max_number = arr[i + 1]
+
+            for j in range(i + 2, length):
+                max_number = max(max_number, arr[j])
+
+            arr[i] = max_number
+
+        return arr
---
+++ tests/algorithms/p1299_replace_elements_with_greatest_element_on_right_side_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms\
+    .p1299_replace_elements_with_greatest_element_on_right_side import Solution
+
+
+class TestReplaceElementsWithGreatestElementOnRightSide(unittest.TestCase):
+    def test_replace_elements_with_greatest_element_on_right_side(self):
+        solution = Solution()
+
+        self.assertListEqual([18, 6, 6, 6, 1, -1], solution.replaceElements(
+            [17, 18, 5, 4, 6, 1]))
