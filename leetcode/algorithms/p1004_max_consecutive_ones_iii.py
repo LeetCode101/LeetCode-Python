d14213980abf6b8527b2e7b0d62ca79dae563204
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-12 10:47:19 +0800

    Update p1004_max_consecutive_ones_iii.py

--- leetcode/algorithms/p1004_max_consecutive_ones_iii.py
+++ leetcode/algorithms/p1004_max_consecutive_ones_iii.py
@@ -5,18 +5,18 @@ class Solution:
     def longestOnes(self, A: List[int], K: int) -> int:
         zeros = 0
         max_length = 0
-        i = 0
+        window_start = 0
 
         for j in range(len(A)):
             if A[j] == 0:
                 zeros += 1
 
             while zeros > K:
-                if A[i] == 0:
+                if A[window_start] == 0:
                     zeros -= 1
 
-                i += 1
+                window_start += 1
 
-            max_length = max(max_length, j - i + 1)
+            max_length = max(max_length, j - window_start + 1)
 
         return max_length
