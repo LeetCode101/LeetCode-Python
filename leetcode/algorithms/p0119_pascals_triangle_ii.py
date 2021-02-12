e206a1d7b9b665485e5a40fa3515e44014782c5b
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-12 11:15:46 +0800

    p119

---
+++ leetcode/algorithms/p0119_pascals_triangle_ii.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        triangle = self.generate(rowIndex + 1)
+
+        return triangle[rowIndex]
+    
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for i in range(numRows):
+            row = []
+
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+            triangle.append(row)
+
+        return triangle
