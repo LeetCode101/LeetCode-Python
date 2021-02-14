06a12ca61727980cf6a7e406571cab3e94e7f667
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-14 13:38:55 +0800

    lint

--- tests/algorithms/p0234_palindrome_linked_list_1_test.py
+++ tests/algorithms/p0234_palindrome_linked_list_1_test.py
@@ -1,5 +1,6 @@
 import unittest
-from leetcode.algorithms.p0234_palindrome_linked_list_1 import Solution, ListNode
+from leetcode.algorithms.p0234_palindrome_linked_list_1 \
+    import Solution, ListNode
 
 
 class TestPalindromeLinkedList(unittest.TestCase):
