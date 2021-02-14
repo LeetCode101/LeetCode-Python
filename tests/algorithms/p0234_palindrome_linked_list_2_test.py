25fc0b4b8118c8251caafa061861455f659fd37e
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-14 14:45:40 +0800

    Update p0234_palindrome_linked_list_2_test.py

--- tests/algorithms/p0234_palindrome_linked_list_2_test.py
+++ tests/algorithms/p0234_palindrome_linked_list_2_test.py
@@ -16,4 +16,5 @@ class TestPalindromeLinkedList(unittest.TestCase):
 
         self.assertTrue(solution.isPalindrome(a))
         self.assertFalse(solution.isPalindrome(ListNode(1, ListNode(2))))
-        self.assertTrue(solution.isPalindrome(ListNode(1, ListNode(3, ListNode(1)))))
+        self.assertTrue(solution.isPalindrome(
+            ListNode(1, ListNode(3, ListNode(1)))))
