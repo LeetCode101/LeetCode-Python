f5456b54f84c8a338a691c9433f96073639ab521
Frederick-S <mao_xiaodan@hotmail.com>
2021-02-14 12:14:03 +0800

    p328

---
+++ leetcode/algorithms/p0328_odd_even_linked_list_2.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+
+        is_odd = False
+        odd_tail = head
+        even_tail = head.next
+        current = head.next
+
+        while current:
+            if is_odd:
+                odd_tail.next, current.next, current = current, odd_tail.next, current.next
+                odd_tail = odd_tail.next
+                even_tail.next, even_tail = current, current
+            else:
+                current = current.next
+
+            is_odd = not is_odd
+
+        return head
---
+++ tests/algorithms/p0328_odd_even_linked_list_2_test.py
@@ -0,0 +1,20 @@
+import unittest
+from leetcode.algorithms.p0328_odd_even_linked_list_2 import ListNode, Solution
+from tests.algorithms.list_helper import convert_linked_list_to_list
+
+
+class TestOddEvenLinkedList(unittest.TestCase):
+    def test_odd_even_linked_list(self):
+        a = ListNode(1)
+        b = ListNode(2)
+        c = ListNode(3)
+        d = ListNode(4)
+        e = ListNode(5)
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = e
+        solution = Solution()
+
+        self.assertListEqual([1, 3, 5, 2, 4], convert_linked_list_to_list(
+            solution.oddEvenList(a)))
