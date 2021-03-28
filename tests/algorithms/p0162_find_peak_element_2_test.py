4d1fa2e7ed14dd4511cee5000da9f1d4f1497c20
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-28 15:52:44 +0800

    Update p0162_find_peak_element_2_test.py

--- tests/algorithms/p0162_find_peak_element_2_test.py
+++ tests/algorithms/p0162_find_peak_element_2_test.py
@@ -6,5 +6,6 @@ class TestFindPeakElement(unittest.TestCase):
     def test_find_peak_element(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.findPeakElement([1]))
         self.assertEqual(2, solution.findPeakElement([1, 2, 3, 1]))
         self.assertEqual(1, solution.findPeakElement([1, 2, 1, 3, 5, 6, 4]))
