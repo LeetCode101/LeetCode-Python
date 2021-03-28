bb97c2578427148684c4af2433cbcd2c5b072784
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-28 18:22:17 +0800

    rename test

--- tests/algorithms/p0658_find_k_closest_elements_1_test.py
+++ tests/algorithms/p0658_find_k_closest_elements_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0658_find_k_closest_elements import Solution
+from leetcode.algorithms.p0658_find_k_closest_elements_1 import Solution
 
 
 class TestFindKClosestElements(unittest.TestCase):
