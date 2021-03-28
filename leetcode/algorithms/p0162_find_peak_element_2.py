62ae00343cd9044f240d0b440004fee85fce8628
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-28 15:38:24 +0800

    p162

---
+++ leetcode/algorithms/p0162_find_peak_element_2.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                return i
+
+        return len(nums) - 1
