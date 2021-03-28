e1ae8320ecacd98824d275cd2e25024d549215be
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-28 14:06:54 +0800

    rename file

--- tests/algorithms/p0162_find_peak_element_1_test.py
+++ tests/algorithms/p0162_find_peak_element_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0162_find_peak_element import Solution
+from leetcode.algorithms.p0162_find_peak_element_1 import Solution
 
 
 class TestFindPeakElement(unittest.TestCase):
