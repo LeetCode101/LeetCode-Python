2082f886e4c76d2c6b01201de3a00fca187c86c2
Frederick-S <mao_xiaodan@hotmail.com>
2021-03-28 16:56:58 +0800

    rename test

--- tests/algorithms/p0034_find_first_and_last_position_of_element_in_sorted_array_1_test.py
+++ tests/algorithms/p0034_find_first_and_last_position_of_element_in_sorted_array_1_test.py
@@ -1,6 +1,6 @@
 import unittest
 from leetcode.algorithms\
-    .p0034_find_first_and_last_position_of_element_in_sorted_array \
+    .p0034_find_first_and_last_position_of_element_in_sorted_array_1 \
     import Solution
 
 
