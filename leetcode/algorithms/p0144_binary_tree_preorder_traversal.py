2a4730f058593306843f64af354be9008cd3d8e7
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-03 19:03:15 +0800

    p144

---
+++ leetcode/algorithms/p0144_binary_tree_preorder_traversal.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        values = []
+        self._preorder_traversal(root, values)
+
+        return values
+
+    def _preorder_traversal(self, root: TreeNode, values: List[int]) -> None:
+        if not root:
+            return
+
+        values.append(root.val)
+        self._preorder_traversal(root.left, values)
+        self._preorder_traversal(root.right, values)
---
+++ tests/algorithms/p0144_binary_tree_preorder_traversal_test.py
@@ -0,0 +1,15 @@
+import unittest
+from leetcode.algorithms.p0144_binary_tree_preorder_traversal \
+    import Solution, TreeNode
+
+
+class TestBinaryTreePreorderTraversal(unittest.TestCase):
+    def test_binary_tree_preorder_traversal(self):
+        solution = Solution()
+        a = TreeNode(4)
+        b = TreeNode(2, TreeNode(1), TreeNode(3))
+        c = TreeNode(5)
+        a.left = b
+        a.right = c
+
+        self.assertListEqual([4, 2, 1, 3, 5], solution.preorderTraversal(a))
