a90f52b0e30998bf50e68dd07759e01ca0ae692c
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-04 12:36:56 +0800

    p367

---
+++ tests/algorithms/p0367_valid_perfect_square_2_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0367_valid_perfect_square_2 import Solution
+
+
+class TestValidPerfectSquare(unittest.TestCase):
+    def test_valid_perfect_square(self):
+        solution = Solution()
+
+        self.assertTrue(solution.isPerfectSquare(16))
+        self.assertFalse(solution.isPerfectSquare(14))
