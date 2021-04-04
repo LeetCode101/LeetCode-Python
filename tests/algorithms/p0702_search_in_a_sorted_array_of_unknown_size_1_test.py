7ab2b1011c0454a746789fedcddd6d7485cf2200
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-04 10:26:23 +0800

    rename file

--- tests/algorithms/p0702_search_in_a_sorted_array_of_unknown_size_1_test.py
+++ tests/algorithms/p0702_search_in_a_sorted_array_of_unknown_size_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0702_search_in_a_sorted_array_of_unknown_size \
+from leetcode.algorithms.p0702_search_in_a_sorted_array_of_unknown_size_1 \
     import Solution, ArrayReader
 
 
