0f08d1be847f9fa472d9cade88d08c5e27132959
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-04 15:37:49 +0800

    p349

---
+++ tests/algorithms/p0349_intersection_of_two_arrays_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p0349_intersection_of_two_arrays import Solution
+
+
+class TestIntersectionOfTwoArrays(unittest.TestCase):
+    def test_intersection_of_two_arrays(self):
+        solution = Solution()
+
+        self.assertListEqual([2], solution.intersection([1, 2, 2, 1], [2, 2]))
+        self.assertListEqual([9, 4], solution.intersection(
+            [4, 9, 5], [9, 4, 9, 8, 4]))
