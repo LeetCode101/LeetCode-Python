684999b78c7fc3a1c9c12b9fbe60777aa96cd298
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-04 19:11:05 +0800

    Update p0238_product_of_array_except_self_2_test.py

--- tests/algorithms/p0238_product_of_array_except_self_2_test.py
+++ tests/algorithms/p0238_product_of_array_except_self_2_test.py
@@ -6,6 +6,7 @@ class TestProductOfArrayExceptSelf(unittest.TestCase):
     def test_product_of_array_except_self(self):
         solution = Solution()
 
+        self.assertListEqual([0, 0], solution.productExceptSelf([0, 0]))
         self.assertListEqual([24, 12, 8, 6], solution.productExceptSelf(
             [1, 2, 3, 4]))
         self.assertListEqual([0, 0, 9, 0, 0], solution.productExceptSelf(
