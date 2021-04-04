7392d1de88fce50fccf959625eb38e3a948b9f8f
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-04 18:05:40 +0800

    Update p0953_verifying_an_alien_dictionary.py

--- leetcode/algorithms/p0953_verifying_an_alien_dictionary.py
+++ leetcode/algorithms/p0953_verifying_an_alien_dictionary.py
@@ -11,23 +11,25 @@ class Solution:
         for i in range(1, len(words)):
             word1 = words[i - 1]
             word2 = words[i]
-            j = k = 0
 
-            while j < len(word1) and k < len(word2):
-                order1 = orders[ord(word1[j]) - 97]
-                order2 = orders[ord(word2[k]) - 97]
-
-                if order1 == order2:
-                    j += 1
-                    k += 1
-
-                    continue
-                elif order1 < order2:
-                    break
-                else:
-                    return False
-
-            if j == min(len(word1), len(word2)) and len(word1) > len(word2):
+            if not self._is_smaller(word1, word2, orders):
                 return False
 
         return True
+
+    def _is_smaller(self, word1, word2, orders):
+        j = k = 0
+
+        while j < len(word1) and k < len(word2):
+            order1 = orders[ord(word1[j]) - 97]
+            order2 = orders[ord(word2[k]) - 97]
+
+            if order1 < order2:
+                return True
+            elif order1 > order2:
+                return False
+            else:
+                j += 1
+                k += 1
+
+        return len(word1) <= len(word2)
