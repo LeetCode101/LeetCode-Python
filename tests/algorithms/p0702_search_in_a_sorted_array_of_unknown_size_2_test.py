93ad1bb42edc0c861e17f1a9376727ccd942a442
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-04 10:28:08 +0800

    p702

---
+++ tests/algorithms/p0702_search_in_a_sorted_array_of_unknown_size_2_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms.p0702_search_in_a_sorted_array_of_unknown_size_2 \
+    import Solution, ArrayReader
+
+
+class TestSearchInASortedArrayOfUnknownSize(unittest.TestCase):
+    def test_search_in_a_sorted_array_of_unknown_size(self):
+        solution = Solution()
+        array_reader = ArrayReader()
+
+        self.assertEqual(4, solution.search(array_reader, 9))
+        self.assertEqual(-1, solution.search(array_reader, 2))
