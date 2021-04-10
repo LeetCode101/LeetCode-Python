47e711e8630c51683817e9794cf12cf6dc5684a3
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-10 16:15:27 +0800

    Update p1473_paint_house_iii.py

--- leetcode/algorithms/p1473_paint_house_iii.py
+++ leetcode/algorithms/p1473_paint_house_iii.py
@@ -7,25 +7,25 @@ class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]],
                 m: int, n: int, target: int) -> int:
         @functools.lru_cache(None)
-        def dfs(i, k, p):
-            if i == m and k == target:
+        def dfs(i, neighbor_count, prev_color):
+            if i == m and neighbor_count == target:
                 return 0
 
-            if i >= m or k > target:
+            if i >= m or neighbor_count > target:
                 return sys.maxsize
 
             current_min_cost = sys.maxsize
 
             if houses[i]:
-                current_min_cost = min(
-                    current_min_cost,
-                    dfs(i + 1, k + (houses[i] != p), houses[i]))
+                current_min_cost = dfs(
+                    i + 1, neighbor_count + (houses[i] != prev_color),
+                    houses[i])
             else:
                 for color in range(1, n + 1):
+                    next_house_cost = dfs(
+                        i + 1, neighbor_count + (color != prev_color), color)
                     current_min_cost = min(
-                        current_min_cost,
-                        cost[i][color - 1] +
-                        dfs(i + 1, k + (color != p), color))
+                        current_min_cost, cost[i][color - 1] + next_house_cost)
 
             return current_min_cost
 
