fee5a058b555f584274f9c194c7190fbc237794a
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-10 13:16:35 +0800

    Update p0256_paint_house.py

--- leetcode/algorithms/p0256_paint_house.py
+++ leetcode/algorithms/p0256_paint_house.py
@@ -6,13 +6,12 @@ class Solution:
         if not costs:
             return 0
 
-        last_red, last_green, last_blue = costs[0][0], costs[0][1], costs[0][2]
+        cost_so_far = [costs[0][0], costs[0][1], costs[0][2]]
 
         for i in range(1, len(costs)):
-            current_red = min(last_green, last_blue) + costs[i][0]
-            current_green = min(last_red, last_blue) + costs[i][1]
-            current_blue = min(last_red, last_green) + costs[i][2]
-            last_red, last_green, last_blue = \
-                current_red, current_green, current_blue
+            red_cost = min(cost_so_far[1], cost_so_far[2]) + costs[i][0]
+            green_cost = min(cost_so_far[0], cost_so_far[2]) + costs[i][1]
+            blue_cost = min(cost_so_far[0], cost_so_far[1]) + costs[i][2]
+            cost_so_far = [red_cost, green_cost, blue_cost]
 
-        return min(last_red, last_green, last_blue)
+        return min(cost_so_far)
