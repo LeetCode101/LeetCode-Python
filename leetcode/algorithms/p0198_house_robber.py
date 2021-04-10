229b9ac42a933442e9ada6a98a4cbc2a59e182ad
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-10 16:34:04 +0800

    p198

---
+++ leetcode/algorithms/p0198_house_robber.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return max(nums)
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+
+        return max(dp)
