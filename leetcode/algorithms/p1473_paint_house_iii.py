094342fbb9f62909e7f72f2ca199641c59c1c202
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-10 15:51:59 +0800

    Update p1473_paint_house_iii.py

--- leetcode/algorithms/p1473_paint_house_iii.py
+++ leetcode/algorithms/p1473_paint_house_iii.py
@@ -6,24 +6,24 @@ from typing import List
 class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]],
                 m: int, n: int, target: int) -> int:
-        min_cost = self._dfs(0, 0, None, houses, cost, m, n, target)
+        @functools.lru_cache(None)
+        def dfs(i, k, p):
+            if i == m and k == target:
+                return 0
+
+            if i >= m or k > target:
+                return sys.maxsize
+
+            min_cost = sys.maxsize
+
+            if houses[i]:
+                min_cost = min(min_cost, dfs(i + 1, k + (houses[i] != p), houses[i]))
+            else:
+                for color in range(1, n + 1):
+                    min_cost = min(min_cost, cost[i][color - 1] + dfs(i + 1, k + (color != p), color))
+
+            return min_cost
+
+        min_cost = dfs(0, 0, None)
 
         return min_cost if min_cost != sys.maxsize else -1
-
-    @functools.lru_cache
-    def _dfs(self, i, k, p, houses: List[int], cost: List[List[int]], m: int, n: int, target: int):
-        if i == m and k == target:
-            return 0
-
-        if i >= m or k > target:
-            return sys.maxsize
-
-        min_cost = sys.maxsize
-
-        if houses[i]:
-            min_cost = min(min_cost, self._dfs(i + 1, k + (houses[i] != p), houses[i], houses, cost, m, n, target))
-        else:
-            for color in range(1, n + 1):
-                min_cost = min(min_cost, cost[i][color - 1] + self._dfs(i + 1, k + (color != p), color, houses, cost, m, n, target))
-
-        return min_cost
