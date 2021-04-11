1789c343a764e0fc802c414ac4e4031fec228541
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-11 19:24:59 +0800

    Update p0719_find_kth_smallest_pair_distance_test.py

--- tests/algorithms/p0719_find_kth_smallest_pair_distance_test.py
+++ tests/algorithms/p0719_find_kth_smallest_pair_distance_test.py
@@ -7,4 +7,5 @@ class TestFindKthSmallestPairDistance(unittest.TestCase):
         solution = Solution()
 
         self.assertEqual(0, solution.smallestDistancePair([1, 3, 1], 1))
-        self.assertEqual(6, solution.smallestDistancePair([1, 2, 3, 4, 5, 6, 7], 21))
+        self.assertEqual(6, solution.smallestDistancePair(
+            [1, 2, 3, 4, 5, 6, 7], 21))
