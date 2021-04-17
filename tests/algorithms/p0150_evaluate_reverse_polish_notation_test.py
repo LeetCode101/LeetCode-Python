b7630883ff9712cd0b3f16a894a22d338e7afc87
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-17 13:18:50 +0800

    Update p0150_evaluate_reverse_polish_notation_test.py

--- tests/algorithms/p0150_evaluate_reverse_polish_notation_test.py
+++ tests/algorithms/p0150_evaluate_reverse_polish_notation_test.py
@@ -6,6 +6,7 @@ class TestEvaluateReversePolishNotation(unittest.TestCase):
     def test_evaluate_reverse_polish_notation(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.evalRPN(['1', '1', '-']))
         self.assertEqual(22, solution.evalRPN(
             ['10', '6', '9', '3', '+', '-11', '*',
              '/', '*', '17', '+', '5', '+']))
