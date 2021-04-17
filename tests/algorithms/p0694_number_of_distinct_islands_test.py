cfab457c557a111860c12e5388da807d4be27835
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-17 19:26:59 +0800

    Update p0694_number_of_distinct_islands_test.py

--- tests/algorithms/p0694_number_of_distinct_islands_test.py
+++ tests/algorithms/p0694_number_of_distinct_islands_test.py
@@ -6,6 +6,7 @@ class TestNumberOfDistinctIslands(unittest.TestCase):
     def test_number_of_distinct_islands(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.numDistinctIslands([]))
         self.assertEqual(3, solution.numDistinctIslands(
             [[1, 1, 0, 1, 1], [1, 0, 0, 0, 0],
              [0, 0, 0, 0, 1], [1, 1, 0, 1, 1]]))
