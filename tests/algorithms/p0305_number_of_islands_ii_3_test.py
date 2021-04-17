7b9e52b74c96dd7dce64d877eec993773932d122
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-17 18:21:53 +0800

    test coverage

--- tests/algorithms/p0305_number_of_islands_ii_3_test.py
+++ tests/algorithms/p0305_number_of_islands_ii_3_test.py
@@ -6,5 +6,7 @@ class TestNumberOfIslands(unittest.TestCase):
     def test_number_of_islands(self):
         solution = Solution()
 
+        self.assertListEqual([1, 1, 1, 1], solution.numIslands2(
+            3, 3, [[0, 1], [0, 1], [0, 1], [0, 1]]))
         self.assertListEqual([1, 1, 2, 3], solution.numIslands2(
             3, 3, [[0, 0], [0, 1], [1, 2], [2, 1]]))
