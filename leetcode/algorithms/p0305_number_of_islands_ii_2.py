6c0852fdb58d6496b4eb974ad0c4f9654d2438ca
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-17 18:06:46 +0800

    Update p0305_number_of_islands_ii_2.py

--- leetcode/algorithms/p0305_number_of_islands_ii_2.py
+++ leetcode/algorithms/p0305_number_of_islands_ii_2.py
@@ -1,24 +1,55 @@
 from typing import List
 
 
+class UnionFind:
+    def __init__(self, n: int):
+        self.roots = [-1] * n
+        self.count = 0
+
+    def add(self, index: int):
+        if self.roots[index] == -1:
+            self.roots[index] = index
+            self.count += 1
+
+    def get_parent(self, index: int):
+        return self.roots[index]
+
+    def find_root(self, index: int) -> int:
+        while index != self.roots[index]:
+            index = self.roots[index]
+
+        return index
+
+    def union(self, p: int, q: int):
+        p_root = self.find_root(p)
+        q_root = self.find_root(q)
+
+        if p_root == q_root:
+            return
+
+        self.roots[p_root] = q_root
+        self.count -= 1
+
+    def get_count(self):
+        return self.count
+
+
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) \
             -> List[int]:
         counts = []
-        count = 0
-        roots = [-1] * (m * n)
+        uf = UnionFind(m * n)
         directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 
         for position in positions:
             root = n * position[0] + position[1]
 
-            if roots[root] != -1:
-                counts.append(count)
+            if uf.get_parent(root) != -1:
+                counts.append(uf.get_count())
 
                 continue
 
-            count += 1
-            roots[root] = root
+            uf.add(root)
 
             for direction in directions:
                 x = position[0] + direction[0]
@@ -26,22 +57,11 @@ class Solution:
                 neighbour = n * x + y
 
                 if x < 0 or x >= m or y < 0 or y >= n \
-                        or roots[neighbour] == -1:
+                        or uf.get_parent(neighbour) == -1:
                     continue
 
-                neighbour_root = self._find_root(roots, neighbour)
+                uf.union(root, neighbour)
 
-                if root != neighbour_root:
-                    roots[root] = neighbour_root
-                    root = neighbour_root
-                    count -= 1
-
-            counts.append(count)
+            counts.append(uf.get_count())
 
         return counts
-
-    def _find_root(self, roots: List[int], id: int) -> int:
-        while id != roots[id]:
-            id = roots[id]
-
-        return id
