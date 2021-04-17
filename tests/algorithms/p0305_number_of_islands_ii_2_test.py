9107461ebb4fa164a48acdc2a1972125f5f39da2
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-17 18:46:17 +0800

    Update p0305_number_of_islands_ii_2_test.py

--- tests/algorithms/p0305_number_of_islands_ii_2_test.py
+++ tests/algorithms/p0305_number_of_islands_ii_2_test.py
@@ -6,6 +6,8 @@ class TestNumberOfIslands(unittest.TestCase):
     def test_number_of_islands(self):
         solution = Solution()
 
+        self.assertListEqual([1, 1, 1, 1, 1], solution.numIslands2(
+            5, 5, [[1, 1], [1, 2], [2, 1], [3, 1], [2, 2]]))
         self.assertListEqual([1, 1, 1, 1], solution.numIslands2(
             3, 3, [[0, 1], [0, 1], [0, 1], [0, 1]]))
         self.assertListEqual([1, 1, 2, 3], solution.numIslands2(
