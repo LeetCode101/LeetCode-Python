5ef64c0c0eecf6210d6fd0b54e5b7411dad12033
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-24 08:07:27 +0800

    p1020

---
+++ leetcode/algorithms/p1020_number_of_enclaves_2.py
@@ -0,0 +1,49 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+
+        enclaves = 0
+        m, n = len(grid), len(grid[0])
+        visited = [[False for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and not visited[i][j]:
+                    area = 1
+                    visited[i][j] = True
+                    queue = deque([(i, j)])
+                    has_boundary = self._is_boundary(m, n, i, j)
+
+                    while queue:
+                        cell = queue.popleft()
+                        row, column = cell[0], cell[1]
+
+                        for direction in [[-1, 0], [0, -1], [1, 0], [0, 1]]:
+                            next_row = row + direction[0]
+                            next_column = column + direction[1]
+
+                            if next_row < 0 or next_row >= m \
+                                    or next_column < 0 \
+                                    or next_column >= n \
+                                    or grid[next_row][next_column] != 1 \
+                                    or visited[next_row][next_column]:
+                                continue
+
+                            area += 1
+                            queue.append((next_row, next_column))
+                            visited[next_row][next_column] = True
+                            has_boundary = has_boundary or self._is_boundary(
+                                m, n, next_row, next_column)
+
+                    if not has_boundary:
+                        enclaves += area
+
+        return enclaves
+
+    def _is_boundary(self, m, n, row, column):
+        return row == 0 or row == m - 1 or column == 0 or column == n - 1
---
+++ tests/algorithms/p1020_number_of_enclaves_2_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p1020_number_of_enclaves_2 import Solution
+
+
+class TestNumberOfEnclaves(unittest.TestCase):
+    def test_number_of_enclaves(self):
+        solution = Solution()
+
+        self.assertEqual(0, solution.numEnclaves([]))
+        self.assertEqual(3, solution.numEnclaves(
+            [[0, 0, 0, 0], [1, 0, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]))
