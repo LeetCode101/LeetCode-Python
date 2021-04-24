ce1e7146d20fdc4308b50d1044eaca217d31c23d
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-24 15:43:43 +0800

    p494

---
+++ leetcode/algorithms/p0494_target_sum.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        return self._dfs(nums, 0, 0, target)
+
+    def _dfs(self, nums, i, value, target):
+        if i == len(nums):
+            return 1 if value == target else 0
+
+        return self._dfs(nums, i + 1, value + nums[i], target) + self._dfs(nums, i + 1, value - nums[i], target)
