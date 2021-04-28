d5ee775e1c6aef132e11f1d15bf7be2b8fdc9a05
Frederick-S <mao_xiaodan@hotmail.com>
2021-04-28 22:06:26 +0800

    Update p0227_basic_calculator_ii_test.py

--- tests/algorithms/p0227_basic_calculator_ii_test.py
+++ tests/algorithms/p0227_basic_calculator_ii_test.py
@@ -6,5 +6,6 @@ class TestBasicCalculator(unittest.TestCase):
     def test_basic_calculator(self):
         solution = Solution()
 
+        self.assertEqual(1, solution.calculate('3-2'))
         self.assertEqual(7, solution.calculate('3+2*2'))
         self.assertEqual(5, solution.calculate(' 3+5 / 2 '))
