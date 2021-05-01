ab7625ae38a773578e2e0f3ee7d3d65c7514294e
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-01 19:08:44 +0800

    Update p0241_different_ways_to_add_parentheses_test.py

--- tests/algorithms/p0241_different_ways_to_add_parentheses_test.py
+++ tests/algorithms/p0241_different_ways_to_add_parentheses_test.py
@@ -7,5 +7,6 @@ class TestDifferentWaysToAddParentheses(unittest.TestCase):
     def test_different_ways_to_add_parentheses(self):
         solution = Solution()
 
+        self.assertEqual([2], solution.diffWaysToCompute('1+1'))
         self.assertListEqual(sorted([-34, -14, -10, -10, 10]),
                              sorted(solution.diffWaysToCompute('2*3-4*5')))
