7dbebce3c713b1e7eafe49dd6bdac08360190e89
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-01 20:38:03 +0800

    rename test

--- tests/algorithms/p0394_decode_string_1_test.py
+++ tests/algorithms/p0394_decode_string_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0394_decode_string import Solution
+from leetcode.algorithms.p0394_decode_string_1 import Solution
 
 
 class TestDecodeString(unittest.TestCase):
