5b30f016fb4180f0f38d33a78fd0f939360b0e01
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-01 19:42:54 +0800

    Update p0394_decode_string_test.py

--- tests/algorithms/p0394_decode_string_test.py
+++ tests/algorithms/p0394_decode_string_test.py
@@ -9,3 +9,4 @@ class TestDecodeString(unittest.TestCase):
         self.assertEqual('a' * 10, solution.decodeString('10[a]'))
         self.assertEqual('aaabcbc', solution.decodeString('3[a]2[bc]'))
         self.assertEqual('abccdcdcdxyz', solution.decodeString('abc3[cd]xyz'))
+        self.assertEqual('accaccacc', solution.decodeString('3[a2[c]]'))
