5ee578c2060cbf423937718ecd50d1a502362ff7
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-03 15:22:32 +0800

    p66

---
+++ tests/algorithms/p0066_plus_one_2_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0066_plus_one_2 import Solution
+
+
+class TestPlusOne(unittest.TestCase):
+    def test_plus_one(self):
+        solution = Solution()
+
+        self.assertListEqual([], solution.plusOne([]))
+        self.assertListEqual([1, 0, 0], solution.plusOne([9, 9]))
