363e7df37c21e86c0988f33913238360971d1a21
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-04 11:24:23 +0800

    Update p0067_add_binary.py

--- leetcode/algorithms/p0067_add_binary.py
+++ leetcode/algorithms/p0067_add_binary.py
@@ -5,7 +5,8 @@ class Solution:
         m, n = len(a) - 1, len(b) - 1
 
         while m >= 0 or n >= 0:
-            current_sum = carry + (int(a[m]) if m >= 0 else 0) + (int(b[n]) if n >= 0 else 0)
+            current_sum = carry + (int(a[m]) if m >= 0 else 0) \
+                          + (int(b[n]) if n >= 0 else 0)
             carry = current_sum // 2
             result = str(current_sum % 2) + result
             m -= 1
