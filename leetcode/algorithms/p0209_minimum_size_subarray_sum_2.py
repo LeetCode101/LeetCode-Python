34736e6762b036bd28af29b0411e1a692114916b
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-04 15:06:33 +0800

    p209

---
+++ leetcode/algorithms/p0209_minimum_size_subarray_sum_2.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        min_length = 0
+        sums = [0] * len(nums)
+
+        for i, n in enumerate(nums):
+            sums[i] = sums[i - 1] + n if i != 0 else n
+
+        left = 0
+
+        for right, n in enumerate(sums):
+            if n >= target:
+                left = self._find_left(left, right, sums, target, n)
+                current_length = right - left + 1
+                min_length = min(min_length, current_length) \
+                    if min_length != 0 else current_length
+
+        return min_length
+
+    def _find_left(self, left, right, sums, target, n):
+        while left < right:
+            middle = left + (right - left) // 2
+
+            if n - sums[middle] >= target:
+                left = middle + 1
+            else:
+                right = middle
+
+        return left
---
+++ tests/algorithms/p0209_minimum_size_subarray_sum_2_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms.p0209_minimum_size_subarray_sum_2 import Solution
+
+
+class TestMinimumSizeSubarraySum(unittest.TestCase):
+    def test_minimum_size_subarray_sum(self):
+        solution = Solution()
+
+        self.assertEqual(0, solution.minSubArrayLen(1, []))
+        self.assertEqual(2, solution.minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))
+        self.assertEqual(0, solution.minSubArrayLen(
+            11, [1, 1, 1, 1, 1, 1, 1, 1]))
