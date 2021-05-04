b05887e1a1dbb307f2e271a4748b46bb67bf9df4
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-04 16:41:35 +0800

    Update p0101_symmetric_tree_1_test.py

--- tests/algorithms/p0101_symmetric_tree_1_test.py
+++ tests/algorithms/p0101_symmetric_tree_1_test.py
@@ -12,5 +12,7 @@ class TestSymmetricTree(unittest.TestCase):
         a.right = c
 
         self.assertTrue(solution.isSymmetric(a))
-        self.assertFalse(solution.isSymmetric(TreeNode(1, TreeNode(2), TreeNode(3))))
-        self.assertFalse(solution.isSymmetric(TreeNode(1, TreeNode(2, TreeNode(3)))))
+        self.assertFalse(solution.isSymmetric(
+            TreeNode(1, TreeNode(2), TreeNode(3))))
+        self.assertFalse(solution.isSymmetric(
+            TreeNode(1, TreeNode(2, TreeNode(3)))))
