778e3005e705d9b4bc8f673a0d1326e7276eff23
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-05 15:26:03 +0800

    p124

---
+++ leetcode/algorithms/p0124_binary_tree_maximum_path_sum.py
@@ -0,0 +1,29 @@
+import sys
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.max_sum = -sys.maxsize
+
+    def maxPathSum(self, root: TreeNode) -> int:
+        self._dfs(root)
+
+        return self.max_sum
+
+    def _dfs(self, root):
+        if not root:
+            return 0
+
+        left = max(self._dfs(root.left), 0)
+        right = max(self._dfs(root.right), 0)
+        current_sum = root.val + left + right
+        self.max_sum = max(self.max_sum, current_sum)
+
+        return root.val + max(left, right)
---
+++ tests/algorithms/p0124_binary_tree_maximum_path_sum_test.py
@@ -0,0 +1,15 @@
+import unittest
+from leetcode.algorithms.p0124_binary_tree_maximum_path_sum \
+    import Solution, TreeNode
+
+
+class TestBinaryTreeMaximumPathSum(unittest.TestCase):
+    def test_binary_tree_maximum_path_sum(self):
+        solution = Solution()
+        a = TreeNode(
+            -10,
+            TreeNode(9),
+            TreeNode(20, TreeNode(15), TreeNode(7))
+        )
+
+        self.assertEqual(42, solution.maxPathSum(a))
