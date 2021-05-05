fccf98cfc723ca242080e48abfa7cafb3adc1552
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-05 10:19:53 +0800

    p113

---
+++ leetcode/algorithms/p0113_path_sum_ii.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        paths = []
+
+        self._path_sum(root, targetSum, [], paths)
+
+        return paths
+
+    def _path_sum(self, root, target, path_so_far, paths):
+        if not root:
+            return
+        elif root.left is None and root.right is None:
+            if root.val == target:
+                paths.append(path_so_far + [root.val])
+        else:
+            if root.left:
+                self._path_sum(root.left, target - root.val, path_so_far + [root.val], paths)
+
+            if root.right:
+                self._path_sum(root.right, target - root.val, path_so_far + [root.val], paths)
---
+++ tests/algorithms/p0113_path_sum_ii_test.py
@@ -0,0 +1,15 @@
+import unittest
+from leetcode.algorithms.p0113_path_sum_ii import Solution, TreeNode
+
+
+class TestPathSum(unittest.TestCase):
+    def test_path_sum(self):
+        solution = Solution()
+        a = TreeNode(
+            5,
+            TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))),
+            TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1)))
+        )
+
+        self.assertListEqual([[5, 4, 11, 2], [5, 8, 4, 5]],
+                             solution.pathSum(a, 22))
