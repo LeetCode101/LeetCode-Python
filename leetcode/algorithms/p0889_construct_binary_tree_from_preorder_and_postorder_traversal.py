e67ba852f985dd91ce76064af0cb8f030d354d5c
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-06 22:28:31 +0800

    Update p0889_construct_binary_tree_from_preorder_and_postorder_traversal.py

--- leetcode/algorithms/p0889_construct_binary_tree_from_preorder_and_postorder_traversal.py
+++ leetcode/algorithms/p0889_construct_binary_tree_from_preorder_and_postorder_traversal.py
@@ -11,16 +11,11 @@ class TreeNode:
 class Solution:
     def constructFromPrePost(self, pre: List[int], post: List[int]) \
             -> TreeNode:
-        positions = {}
-
-        for i, n in enumerate(post):
-            positions[n] = i
-
         return self._build_tree(pre, 0, len(pre) - 1,
-                                post, 0, len(post) - 1, positions)
+                                post, 0, len(post) - 1)
 
     def _build_tree(self, pre, pre_start, pre_end,
-                    post, post_start, post_end, positions):
+                    post, post_start, post_end):
         if pre_start > pre_end or post_start > post_end:
             return None
 
@@ -30,14 +25,21 @@ class Solution:
             return root
 
         left_tree_root_value = pre[pre_start + 1]
-        post_left_tree_root_index = positions[left_tree_root_value]
+        post_left_tree_root_index = -1
+
+        for i, n in enumerate(post):
+            if n == left_tree_root_value:
+                post_left_tree_root_index = i
+
+                break
+
         left_tree_size = post_left_tree_root_index - post_start + 1
 
         root.left = self._build_tree(
             pre, pre_start + 1, pre_start + left_tree_size,
-            post, post_start, post_start + left_tree_size - 1, positions)
+            post, post_start, post_start + left_tree_size - 1)
         root.right = self._build_tree(
             pre, pre_start + left_tree_size + 1, pre_end,
-            post, post_start + left_tree_size, post_end - 1, positions)
+            post, post_start + left_tree_size, post_end - 1)
 
         return root
