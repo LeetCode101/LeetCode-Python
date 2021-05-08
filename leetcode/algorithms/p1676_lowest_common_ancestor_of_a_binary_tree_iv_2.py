ae6d1777928cc945f21126714594d3c3dee3b7d7
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-08 16:23:04 +0800

    p1676

---
+++ leetcode/algorithms/p1676_lowest_common_ancestor_of_a_binary_tree_iv_2.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') \
+            -> 'TreeNode':
+        return self._lowest_common_ancestor(root, set(nodes))
+
+    def _lowest_common_ancestor(self, root, nodes) -> TreeNode:
+        if not root or root in nodes:
+            return root
+
+        left = self._lowest_common_ancestor(root.left, nodes)
+        right = self._lowest_common_ancestor(root.right, nodes)
+
+        if left and right:
+            return root
+        else:
+            return left or right
---
+++ tests/algorithms/p1676_lowest_common_ancestor_of_a_binary_tree_iv_2_test.py
@@ -0,0 +1,27 @@
+import unittest
+from leetcode.algorithms.p1676_lowest_common_ancestor_of_a_binary_tree_iv_2 \
+    import Solution, TreeNode
+
+
+class TestLowestCommonAncestorOfABinaryTree(unittest.TestCase):
+    def test_lowest_common_ancestor_of_a_binary_tree(self):
+        solution = Solution()
+        a = TreeNode(3)
+        b = TreeNode(5)
+        c = TreeNode(1)
+        a.left = b
+        a.right = c
+        d = TreeNode(6)
+        e = TreeNode(2)
+        b.left = d
+        b.right = e
+        f = TreeNode(0)
+        g = TreeNode(8)
+        c.left = f
+        c.right = g
+        h = TreeNode(7)
+        i = TreeNode(4)
+        e.left = h
+        e.right = i
+
+        self.assertEqual(2, solution.lowestCommonAncestor(a, [h, i]).val)
