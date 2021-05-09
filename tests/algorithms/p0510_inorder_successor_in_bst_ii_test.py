204ab3679afa9962c05041f825bb748962d5728c
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-09 12:53:22 +0800

    Update p0510_inorder_successor_in_bst_ii_test.py

--- tests/algorithms/p0510_inorder_successor_in_bst_ii_test.py
+++ tests/algorithms/p0510_inorder_successor_in_bst_ii_test.py
@@ -23,6 +23,15 @@ class TestInorderSuccessorInBST(unittest.TestCase):
         d.left = f
         f.parent = d
 
+        o = Node(1)
+        p = Node(2)
+        q = Node(3)
+        o.right = p
+        p.parent = o
+        p.left = q
+        q.parent = p
+
         self.assertEqual(5, solution.inorderSuccessor(e).val)
         self.assertIsNone(solution.inorderSuccessor(c))
         self.assertEqual(4, solution.inorderSuccessor(b).val)
+        self.assertEqual(3, solution.inorderSuccessor(o).val)
