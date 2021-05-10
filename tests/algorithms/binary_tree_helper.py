9f8a9b814fd7c211beb16d396e89f8f9041f2100
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-10 22:11:09 +0800

    p701

---
+++ tests/algorithms/binary_tree_helper.py
@@ -0,0 +1,15 @@
+def inorder(root):
+    values = []
+
+    _inorder(root, values)
+
+    return values
+
+
+def _inorder(root, values):
+    if not root:
+        return values
+
+    _inorder(root.left, values)
+    values.append(root.val)
+    _inorder(root.right, values)
---
+++ tests/algorithms/p0701_insert_into_a_binary_search_tree_test.py
@@ -0,0 +1,17 @@
+import unittest
+from leetcode.algorithms.p0701_insert_into_a_binary_search_tree \
+    import Solution, TreeNode
+from tests.algorithms.binary_tree_helper import inorder
+
+
+class TestInsertIntoABinarySearchTree(unittest.TestCase):
+    def test_insert_into_a_binary_search_tree(self):
+        solution = Solution()
+        root = TreeNode(
+            4,
+            TreeNode(2, TreeNode(1), TreeNode(3)),
+            TreeNode(7)
+        )
+
+        self.assertListEqual([1, 2, 3, 4, 5, 7],
+                             inorder(solution.insertIntoBST(root, 5)))
