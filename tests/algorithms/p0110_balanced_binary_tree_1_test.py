07e0095f950f865473a2ad71d9bfd1fae54aa021
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-15 15:18:24 +0800

    rename test

--- tests/algorithms/p0110_balanced_binary_tree_1_test.py
+++ tests/algorithms/p0110_balanced_binary_tree_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0110_balanced_binary_tree import Solution, TreeNode
+from leetcode.algorithms.p0110_balanced_binary_tree_1 import Solution, TreeNode
 
 
 class TestBalancedBinaryTree(unittest.TestCase):
