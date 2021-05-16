6fec44f9ea5321398e7dcc9c9d3f150f73ed5b51
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-16 18:14:25 +0800

    Update p0378_kth_smallest_element_in_a_sorted_matrix_3_test.py

--- tests/algorithms/p0378_kth_smallest_element_in_a_sorted_matrix_3_test.py
+++ tests/algorithms/p0378_kth_smallest_element_in_a_sorted_matrix_3_test.py
@@ -7,5 +7,6 @@ class TestKthSmallestElementInASortedMatrix(unittest.TestCase):
     def test_kth_smallest_element_in_a_sorted_matrix(self):
         solution = Solution()
 
+        self.assertEqual(1, solution.kthSmallest([[1, 2], [1, 3]], 1))
         self.assertEqual(13, solution.kthSmallest(
             [[1, 5, 9], [10, 11, 13], [12, 13, 15]], 8))
