0bb9ebaa5f89779ba4b9aa1db692545f001deb81
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-23 18:05:57 +0800

    Update p0347_top_k_frequent_elements_1.py

--- leetcode/algorithms/p0347_top_k_frequent_elements_1.py
+++ leetcode/algorithms/p0347_top_k_frequent_elements_1.py
@@ -1,16 +1,10 @@
+import collections
 from typing import List
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        frequencies = {}
-
-        for n in nums:
-            if n in frequencies:
-                frequencies[n] += 1
-            else:
-                frequencies[n] = 1
-
+        frequencies = collections.Counter(nums)
         sort_by_frequency = sorted(list(frequencies.items()),
                                    key=lambda x: x[1], reverse=True)
 
