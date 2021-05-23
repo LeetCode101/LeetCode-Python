807c8558ffcdff91ba84d2ec5c3b6bf6c3621237
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-23 22:05:33 +0800

    p692

---
+++ leetcode/algorithms/p0692_top_k_frequent_words_2.py
@@ -0,0 +1,35 @@
+import collections
+import heapq
+from typing import List
+
+class Word:
+    def __init__(self, word, frequency):
+        self.word = word
+        self.frequency = frequency
+
+    def __lt__(self, other):
+        if self.frequency == other.frequency:
+            return self.word > other.word
+
+        return self.frequency < other.frequency
+
+    def __eq__(self, other):
+        return self.word == other.word and self.frequency == other.cout
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        frequencies = collections.Counter(words)
+        heap = []
+
+        for word, frequency in frequencies.items():
+            heapq.heappush(heap, Word(word, frequency))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        top_k = []
+
+        for _ in range(k):
+            top_k.append(heapq.heappop(heap).word)
+
+        return top_k[::-1]
---
+++ tests/algorithms/p0692_top_k_frequent_words_2_test.py
@@ -0,0 +1,20 @@
+import unittest
+from leetcode.algorithms.p0692_top_k_frequent_words_2 import Solution
+
+
+class TestTopKFrequentWords(unittest.TestCase):
+    def test_top_k_frequent_words(self):
+        solution = Solution()
+
+        self.assertListEqual(
+            ['i'],
+            solution.topKFrequent(['i', 'love', 'leetcode',
+                                   'i', 'love', 'coding'], 1))
+        self.assertListEqual(
+            ['i', 'love'],
+            solution.topKFrequent(['i', 'love', 'leetcode',
+                                   'i', 'love', 'coding'], 2))
+        self.assertListEqual(
+            ['the', 'is', 'sunny', 'day'],
+            solution.topKFrequent(['the', 'day', 'is', 'sunny', 'the', 'the',
+                                   'the', 'sunny', 'is', 'is'], 4))
