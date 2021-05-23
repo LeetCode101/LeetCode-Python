782acd79d981725af2c3384c039da3312f1018c4
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-23 22:29:17 +0800

    Update p0288_unique_word_abbreviation_test.py

--- tests/algorithms/p0288_unique_word_abbreviation_test.py
+++ tests/algorithms/p0288_unique_word_abbreviation_test.py
@@ -4,7 +4,7 @@ from leetcode.algorithms.p0288_unique_word_abbreviation import ValidWordAbbr
 
 class TestUniqueWordAbbreviation(unittest.TestCase):
     def test_unique_word_abbreviation(self):
-        abbr = ValidWordAbbr(['deer', 'door', 'cake', 'card'])
+        abbr = ValidWordAbbr(['deer', 'door', 'cake', 'card', 'it'])
 
         self.assertFalse(abbr.isUnique('dear'))
         self.assertTrue(abbr.isUnique('cart'))
