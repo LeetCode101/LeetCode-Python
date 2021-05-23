042ce8ed52927c9c3d51d4be7f105737f210607e
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-23 14:46:15 +0800

    p387

---
+++ leetcode/algorithms/p0387_first_unique_character_in_a_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        occurrences = {}
+
+        for i, c in enumerate(s):
+            if c in occurrences:
+                occurrences[c] = (occurrences[c][0] + 1, occurrences[c][1])
+            else:
+                occurrences[c] = (1, i)
+
+        min_position = -1
+
+        for occurrence in occurrences.values():
+            if occurrence[0] == 1:
+                if min_position == -1:
+                    min_position = occurrence[1]
+                elif min_position > occurrence[1]:
+                    min_position = occurrence[1]
+
+        return min_position
---
+++ tests/algorithms/p0387_first_unique_character_in_a_string_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms.p0387_first_unique_character_in_a_string \
+    import Solution
+
+
+class TestFirstUniqueCharacterInAString(unittest.TestCase):
+    def test_first_unique_character_in_a_string(self):
+        solution = Solution()
+
+        self.assertEqual(0, solution.firstUniqChar('leetcode'))
+        self.assertEqual(2, solution.firstUniqChar('loveleetcode'))
+        self.assertEqual(-1, solution.firstUniqChar('aabb'))
