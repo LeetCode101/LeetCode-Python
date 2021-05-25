f23263f8162a766d2eeb454476f11ab37bf24842
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-25 21:43:37 +0800

    p381

---
+++ leetcode/algorithms/p0381_insert_delete_getrandom_o1_duplicates_allowed.py
@@ -0,0 +1,50 @@
+import random
+
+
+class RandomizedCollection:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.values = []
+        self.position = {}
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the collection.
+        Returns true if the collection did not already
+        contain the specified element.
+        """
+        self.values.append(val)
+
+        if val in self.position:
+            self.position[val].add(len(self.values) - 1)
+        else:
+            self.position[val] = {len(self.values) - 1}
+
+        return len(self.position[val]) == 1
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the collection.
+        Returns true if the collection contained the specified element.
+        """
+        if val not in self.position or not self.position[val]:
+            return False
+
+        i, last = self.position[val].pop(), self.values[-1]
+        self.values[i] = last
+
+        if self.position[last]:
+            self.position[last].add(i)
+            self.position[last].discard(len(self.values) - 1)
+
+        self.values.pop()
+
+        return True
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the collection.
+        """
+        return self.values[random.randint(0, len(self.values) - 1)]
---
+++ tests/algorithms/p0381_insert_delete_getrandom_o1_duplicates_allowed_test.py
@@ -0,0 +1,14 @@
+import unittest
+from leetcode.algorithms.p0381_insert_delete_getrandom_o1_duplicates_allowed \
+    import RandomizedCollection
+
+
+class TestInsertDeleteGetRandomO1DuplicatesAllowed(unittest.TestCase):
+    def test_insert_delete_getrandom_o1_duplicates_allowed(self):
+        randomized_collection = RandomizedCollection()
+
+        self.assertTrue(randomized_collection.insert(1))
+        self.assertFalse(randomized_collection.insert(1))
+        self.assertTrue(randomized_collection.insert(2))
+        self.assertTrue(randomized_collection.getRandom() in [1, 2])
+        self.assertTrue(randomized_collection.remove(1))
