77fcd36f7f6fcea7b8909a299c8bcecdec6b1300
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-27 21:48:04 +0800

    p559

---
+++ tests/algorithms/p0559_maximum_depth_of_n_ary_tree_1_test.py
@@ -0,0 +1,18 @@
+import unittest
+from leetcode.algorithms.p0559_maximum_depth_of_n_ary_tree_1 import Solution, Node
+
+
+class TestMaximumDepthOfNAryTree(unittest.TestCase):
+    def test_maximum_depth_of_n_ary_tree(self):
+        solution = Solution()
+        root = Node(
+            1,
+            children=[
+                Node(3, children=[Node(5), Node(6)]),
+                Node(2),
+                Node(4)
+            ]
+        )
+
+        self.assertEqual(0, solution.maxDepth(None))
+        self.assertEqual(3, solution.maxDepth(root))
