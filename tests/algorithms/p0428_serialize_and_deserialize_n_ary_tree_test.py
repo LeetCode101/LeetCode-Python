918a7411cfce047ccb6b07c13b0cd4da2891d446
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-28 22:28:00 +0800

    Update p0428_serialize_and_deserialize_n_ary_tree_test.py

--- tests/algorithms/p0428_serialize_and_deserialize_n_ary_tree_test.py
+++ tests/algorithms/p0428_serialize_and_deserialize_n_ary_tree_test.py
@@ -16,5 +16,5 @@ class TestSerializeAndDeserializeNAryTree(unittest.TestCase):
         )
         encoded = codec.serialize(root)
 
-        self.assertEqual('1,3,3,2,5,0,6,0,2,0,4,0', encoded)
+        self.assertEqual('1,3,5,,6,,,2,,4,,', encoded)
         self.assertEqual(1, codec.deserialize(encoded).val)
