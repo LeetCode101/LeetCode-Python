0fa4a2fc02b6eca67d451c2c3facb5df40dd1fc8
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-29 08:23:34 +0800

    p428

---
+++ leetcode/algorithms/p0428_serialize_and_deserialize_n_ary_tree_2.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Codec:
+    def serialize(self, root: 'Node') -> str:
+        """Encodes a tree to a single string.
+
+        :type root: Node
+        :rtype: str
+        """
+        encoded = deque()
+        queue = deque([root]) if root else deque()
+
+        while queue:
+            node = queue.popleft()
+
+            if not node:
+                encoded.append('')
+
+                continue
+
+            encoded.append(str(node.val))
+
+            if node.children:
+                for child in node.children:
+                    queue.append(child)
+
+            queue.append(None)
+
+        return ','.join(encoded)
+
+    def deserialize(self, data: str) -> 'Node':
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: Node
+        """
+        if not data:
+            return None
+
+        encoded = deque(data.split(','))
+        root = Node(encoded.popleft(), [])
+        queue = deque([root])
+
+        while queue and encoded:
+            node = queue.popleft()
+
+            while encoded[0] != '':
+                child = Node(int(encoded.popleft()), [])
+                node.children.append(child)
+                queue.append(child)
+
+            encoded.popleft()
+
+        return root
---
+++ tests/algorithms/p0428_serialize_and_deserialize_n_ary_tree_2_test.py
@@ -0,0 +1,22 @@
+import unittest
+from leetcode.algorithms.p0428_serialize_and_deserialize_n_ary_tree_2 \
+    import Codec, Node
+
+
+class TestSerializeAndDeserializeNAryTree(unittest.TestCase):
+    def test_serialize_and_deserialize_n_ary_tree(self):
+        codec = Codec()
+        root = Node(
+            1,
+            children=[
+                Node(3, children=[Node(5), Node(6)]),
+                Node(2),
+                Node(4)
+            ]
+        )
+        encoded = codec.serialize(root)
+
+        self.assertEqual('', codec.serialize(None))
+        self.assertIsNone(codec.deserialize(''))
+        self.assertEqual('1,3,2,4,,5,6,,,,,', encoded)
+        self.assertEqual(1, codec.deserialize(encoded).val)
