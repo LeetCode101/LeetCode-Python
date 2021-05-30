3413fa923d6799f48757f9fc657bb1be4c87ce77
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-30 16:42:23 +0800

    p336

---
+++ leetcode/algorithms/p0336_palindrome_pairs.py
@@ -0,0 +1,71 @@
+from typing import List
+
+
+class TrieNode:
+    def __init__(self):
+        self.end_of_word = False
+        self.children = {}
+        self.index = -1
+        self.positions = []
+
+
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        if not words:
+            return []
+
+        root = TrieNode()
+        result = []
+
+        for i, word in enumerate(words):
+            self._insert(root, word, i)
+
+        for i, word in enumerate(words):
+            self._search(root, i, word, result)
+
+        return result
+
+    def _insert(self, root, word, index):
+        current = root
+
+        for i in range(len(word) - 1, -1, -1):
+            c = word[i]
+
+            if c not in current.children:
+                current.children[c] = TrieNode()
+
+            if self._is_palindrome(word, 0, i):
+                current.positions.append(index)
+
+            current = current.children[c]
+
+        current.end_of_word = True
+        current.index = index
+        current.positions.append(index)
+
+    def _search(self, root, index, word, result):
+        current = root
+
+        for j, c in enumerate(word):
+            if current.index >= 0 and current.index != index \
+                    and self._is_palindrome(word, j, len(word) - 1):
+                result.append([index, current.index])
+
+            if c not in current.children:
+                return
+
+            current = current.children[c]
+
+        for j in current.positions:
+            if index != j:
+                result.append([index, j])
+
+    def _is_palindrome(self, word, low, high):
+        while low < high:
+            if word[low] != word[high]:
+                return False
+
+            low += 1
+            high -= 1
+
+        return True
---
+++ tests/algorithms/p0336_palindrome_pairs_test.py
@@ -0,0 +1,16 @@
+import unittest
+from leetcode.algorithms.p0336_palindrome_pairs import Solution
+
+
+class TestPalindromePairs(unittest.TestCase):
+    def test_palindrome_pairs(self):
+        solution = Solution()
+
+        self.assertListEqual([], solution.palindromePairs([]))
+        self.assertListEqual(
+            sorted([[0, 1], [1, 0], [3, 2], [2, 4]]),
+            sorted(solution.palindromePairs(
+                ['abcd', 'dcba', 'lls', 's', 'sssll'])))
+        self.assertListEqual(
+            sorted([[0, 1], [1, 0]]),
+            sorted(solution.palindromePairs(['bat', 'tab', 'cat'])))
