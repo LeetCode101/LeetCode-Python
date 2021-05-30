a29a830c54f3fef9f959def8a0073e6cfaf3dbaa
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-30 12:21:00 +0800

    p425

---
+++ leetcode/algorithms/p0425_word_squares.py
@@ -0,0 +1,64 @@
+from typing import List
+
+
+class TrieNode:
+    def __init__(self):
+        self.end_of_word = False
+        self.children = {}
+        self.word = ''
+
+
+class Solution:
+    def wordSquares(self, words: List[str]) -> List[List[str]]:
+        root = TrieNode()
+        result = []
+
+        for word in words:
+            self._insert(root, word)
+
+        self._word_squares(root, len(words[0]), [], result)
+
+        return result
+
+    def _insert(self, root, word):
+        current = root
+
+        for c in word:
+            if c not in current.children:
+                current.children[c] = TrieNode()
+
+            current = current.children[c]
+
+        current.end_of_word = True
+        current.word = word
+
+    def _search(self, root, prefix, candidates):
+        current = root
+
+        if current.end_of_word:
+            candidates.append(current.word)
+
+            return
+
+        for c in prefix:
+            if c not in current.children:
+                return
+
+            current = current.children[c]
+
+        for child in current.children.values():
+            self._search(child, '', candidates)
+
+    def _word_squares(self, root, size, words, result):
+        if len(words) == size:
+            result.append(words)
+
+            return
+
+        candidates = []
+        prefix = ''.join([word[len(words)] for word in words])
+
+        self._search(root, prefix, candidates)
+
+        for candidate in candidates:
+            self._word_squares(root, size, words + [candidate], result)
---
+++ tests/algorithms/p0425_word_squares_test.py
@@ -0,0 +1,17 @@
+import unittest
+from leetcode.algorithms.p0425_word_squares import Solution
+
+
+class TestWordSquares(unittest.TestCase):
+    def test_word_squares(self):
+        solution = Solution()
+
+        self.assertListEqual(
+            sorted([['ball', 'area', 'lead', 'lady'],
+                    ['wall', 'area', 'lead', 'lady']]),
+            sorted(solution.wordSquares(
+                ['area', 'lead', 'wall', 'lady', 'ball'])))
+        self.assertListEqual(
+            sorted([['baba', 'abat', 'baba', 'atal'],
+                    ['baba', 'abat', 'baba', 'atan']]),
+            sorted(solution.wordSquares(['abat', 'baba', 'atan', 'atal'])))
