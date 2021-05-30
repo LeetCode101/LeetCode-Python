8ef7c2042af2b42a61d68603bfc6b7f99df464aa
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-30 09:48:57 +0800

    Update p0422_valid_word_square.py

--- leetcode/algorithms/p0422_valid_word_square.py
+++ leetcode/algorithms/p0422_valid_word_square.py
@@ -8,7 +8,8 @@ class Solution:
 
         for i in range(len(words)):
             for j in range(len(words[i])):
-                if j >= len(words) or i >= len(words[j]) or words[i][j] != words[j][i]:
+                if j >= len(words) or i >= len(words[j]) \
+                        or words[i][j] != words[j][i]:
                     return False
 
         return True
