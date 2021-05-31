8b7940a670ee23df226fdc51c2aac0b47e79b6bf
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-31 21:06:11 +0800

    Update p0779_k_th_symbol_in_grammar_2.py

--- leetcode/algorithms/p0779_k_th_symbol_in_grammar_2.py
+++ leetcode/algorithms/p0779_k_th_symbol_in_grammar_2.py
@@ -6,7 +6,8 @@ class Solution:
             return 1
 
         is_odd = k & 1 == 1
-        prev = self.kthGrammar(n - 1, (k - 1) // 2 + 1) if is_odd else self.kthGrammar(n - 1, k // 2)
+        prev = self.kthGrammar(n - 1, (k - 1) // 2 + 1) if is_odd \
+            else self.kthGrammar(n - 1, k // 2)
 
         if is_odd:
             return 1 if prev == 1 else 0
