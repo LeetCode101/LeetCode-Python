a742019b41914896266ba26aa1abb75dc803b2f9
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-03 21:46:27 +0800

    p912

--- leetcode/algorithms/p0912_sort_an_array.py
+++ leetcode/algorithms/p0912_sort_an_array.py
@@ -19,7 +19,7 @@ class Solution:
 
     def _merge(self, nums, low, middle, high):
         if nums[middle] <= nums[middle + 1]:
-            return 
+            return
 
         left = low
         right = middle + 1
--- tests/algorithms/p0912_sort_an_array_test.py
+++ tests/algorithms/p0912_sort_an_array_test.py
@@ -6,5 +6,7 @@ class TestSortAnArray(unittest.TestCase):
     def test_sort_an_array(self):
         solution = Solution()
 
-        self.assertListEqual(sorted([5, 1, 1, 2, 0, 0]),
-                             solution.sortArray([5, 1, 1, 2, 0, 0]))
+        self.assertListEqual(
+            [0, 0, 1, 1, 2, 5], solution.sortArray([5, 1, 1, 2, 0, 0]))
+        self.assertListEqual(
+            [1, 2, 3, 4, 5, 6], solution.sortArray([3, 2, 1, 6, 5, 4]))
