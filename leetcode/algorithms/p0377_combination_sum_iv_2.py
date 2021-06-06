8d4e56a61a24399f3e7af436ea738e2166e2f1c0
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-06 21:36:22 +0800

    Update p0377_combination_sum_iv_2.py

--- leetcode/algorithms/p0377_combination_sum_iv_2.py
+++ leetcode/algorithms/p0377_combination_sum_iv_2.py
@@ -5,10 +5,13 @@ class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         dp = [0] * (target + 1)
         dp[0] = 1
+        sorted_nums = sorted(nums)
 
         for i in range(1, len(dp)):
-            for n in nums:
+            for n in sorted_nums:
                 if i - n >= 0:
                     dp[i] += dp[i - n]
+                else:
+                    break
 
         return dp[target]
