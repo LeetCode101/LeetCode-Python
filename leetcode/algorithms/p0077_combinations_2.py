5c1f8ec122ad68445801e8a47bc70a6451b9e0f4
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-06 14:19:24 +0800

    p77

---
+++ leetcode/algorithms/p0077_combinations_2.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+
+        self._combine(k, 1, n, [], result)
+
+        return result
+
+    def _combine(self, k, start, end, combination, result):
+        if k == 0:
+            result.append(combination)
+
+            return
+
+        for i in range(start, end + 1):
+
+            self._combine(k - 1, i + 1, end,
+                          combination[:] + [i], result)
---
+++ tests/algorithms/p0077_combinations_2_test.py
@@ -0,0 +1,16 @@
+import unittest
+from leetcode.algorithms.p0077_combinations_2 import Solution
+
+
+class TestCombinations(unittest.TestCase):
+    def test_combinations(self):
+        solution = Solution()
+
+        self.assertListEqual(sorted([
+            [2, 4],
+            [3, 4],
+            [2, 3],
+            [1, 2],
+            [1, 3],
+            [1, 4],
+        ]), sorted(solution.combine(4, 2)))
