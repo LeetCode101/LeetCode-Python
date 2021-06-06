305062fade742ea74899fbc316fde2ea3e976e28
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-06 14:59:48 +0800

    p39

---
+++ tests/algorithms/p0039_combination_sum_2_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p0039_combination_sum_2 import Solution
+
+
+class TestCombinationSum(unittest.TestCase):
+    def test_combination_sum(self):
+        solution = Solution()
+
+        self.assertListEqual(
+            sorted([[2, 2, 2, 2], [2, 3, 3], [3, 5]]),
+            solution.combinationSum([2, 3, 5], 8))
