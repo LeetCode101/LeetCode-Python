07bf675fdf47531931310046cbecbd866dfa80f3
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-08 21:39:50 +0800

    p107

---
+++ leetcode/algorithms/p0107_binary_tree_level_order_traversal_ii.py
@@ -0,0 +1,32 @@
+from collections import deque
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        queue = deque([root]) if root else deque()
+        levels = []
+
+        while queue:
+            level = []
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            levels.append(level)
+
+        return list(reversed(levels))
