c99b0b7b0fe9592f4c62fe33274ae6904e10ae90
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-08 21:14:08 +0800

    p100

---
+++ leetcode/algorithms/p0100_same_tree.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        elif p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) \
+                   and self.isSameTree(p.right, q.right)
+        else:
+            return False
---
+++ tests/algorithms/p0100_same_tree_test.py
@@ -0,0 +1,14 @@
+import unittest
+from leetcode.algorithms.p0100_same_tree import Solution, TreeNode
+
+
+class TestSameTree(unittest.TestCase):
+    def test_same_tree(self):
+        solution = Solution()
+        a = TreeNode(1, TreeNode(2), TreeNode(3))
+        b = TreeNode(1, TreeNode(2), TreeNode(3))
+        c = TreeNode(1, TreeNode(2))
+        d = TreeNode(1, None, TreeNode(2))
+
+        self.assertTrue(solution.isSameTree(a, b))
+        self.assertFalse(solution.isSameTree(c, d))
