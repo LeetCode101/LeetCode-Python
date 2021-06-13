0aafecbfdb2b07b6bb60561746d90108c76b9881
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-13 16:08:25 +0800

    Update p0251_flatten_2d_vector_1_test.py

--- tests/algorithms/p0251_flatten_2d_vector_1_test.py
+++ tests/algorithms/p0251_flatten_2d_vector_1_test.py
@@ -4,11 +4,14 @@ from leetcode.algorithms.p0251_flatten_2d_vector_1 import Vector2D
 
 class TestFlatten2DVector(unittest.TestCase):
     def test_flatten_2_d_vector(self):
-        vector = Vector2D([[1, 2], [3], [4]])
+        vector1 = Vector2D([[1, 2], [3], [4]])
+        vector2 = Vector2D([[3], []])
 
-        self.assertEqual(1, vector.next())
-        self.assertEqual(2, vector.next())
-        self.assertEqual(3, vector.next())
-        self.assertTrue(vector.hasNext())
-        self.assertEqual(4, vector.next())
-        self.assertFalse(vector.hasNext())
+        self.assertEqual(1, vector1.next())
+        self.assertEqual(2, vector1.next())
+        self.assertEqual(3, vector1.next())
+        self.assertTrue(vector1.hasNext())
+        self.assertEqual(4, vector1.next())
+        self.assertFalse(vector1.hasNext())
+        self.assertEqual(3, vector2.next())
+        self.assertFalse(vector2.hasNext())
