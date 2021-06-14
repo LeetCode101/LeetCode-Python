2b936759ebb5f111f721fbb1be625e961294b962
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-14 15:28:33 +0800

    p315

---
+++ leetcode/algorithms/p0315_count_of_smaller_numbers_after_self.py
@@ -0,0 +1,46 @@
+from typing import List
+
+
+# Time Limit Exceeded!
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.count = 1
+
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        counts = [0]
+        root = TreeNode(nums[-1])
+
+        for i in range(len(nums) - 2, -1, -1):
+            count = self._insert(root, nums[i])
+            counts.append(count)
+
+        return list(reversed(counts))
+
+    def _insert(self, root, value):
+        count = 0
+        prev, current = None, root
+
+        while current:
+            prev = current
+
+            if current.value < value:
+                count += current.count
+                current = current.right
+            else:
+                current.count += 1
+                current = current.left
+
+        if prev.value < value:
+            prev.right = TreeNode(value)
+        else:
+            prev.left = TreeNode(value)
+
+        return count
