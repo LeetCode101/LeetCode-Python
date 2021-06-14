feaae82823417465a28a95ceadbd00619caeb793
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-14 12:11:24 +0800

    Update p0528_random_pick_with_weight_test.py

--- tests/algorithms/p0528_random_pick_with_weight_test.py
+++ tests/algorithms/p0528_random_pick_with_weight_test.py
@@ -4,6 +4,8 @@ from leetcode.algorithms.p0528_random_pick_with_weight import Solution
 
 class TestRandomPickWithWeight(unittest.TestCase):
     def test_random_pick_with_weight(self):
-        solution = Solution([1, 3])
+        solution1 = Solution([3, 14, 1, 7])
+        solution2 = Solution([1, 3])
 
-        self.assertTrue(solution.pickIndex() in [0, 1])
+        self.assertTrue(solution1.pickIndex() in [0, 1, 2, 3])
+        self.assertTrue(solution2.pickIndex() in [0, 1])
