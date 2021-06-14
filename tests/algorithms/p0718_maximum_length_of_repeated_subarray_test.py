3706e7f975bbf8b601dccafa825667236513c3c1
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-14 17:36:45 +0800

    Update p0718_maximum_length_of_repeated_subarray_test.py

--- tests/algorithms/p0718_maximum_length_of_repeated_subarray_test.py
+++ tests/algorithms/p0718_maximum_length_of_repeated_subarray_test.py
@@ -7,5 +7,6 @@ class TestMaximumLengthOfRepeatedSubarray(unittest.TestCase):
     def test_maximum_length_of_repeated_subarray(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.findLength([], []))
         self.assertEqual(3, solution.findLength(
             [1, 2, 3, 2, 1], [3, 2, 1, 4, 7]))
