cd5c80032ea3c518f39d23ccdb34cf22d4785439
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-14 11:57:08 +0800

    Update p0528_random_pick_with_weight.py

--- leetcode/algorithms/p0528_random_pick_with_weight.py
+++ leetcode/algorithms/p0528_random_pick_with_weight.py
@@ -1,4 +1,4 @@
-from random import randrange
+from random import randint
 from typing import List
 
 
@@ -12,7 +12,7 @@ class Solution:
 
     def pickIndex(self) -> int:
         low, high = 0, len(self.accumulated_weight_sum) - 1
-        target = randrange(self.accumulated_weight_sum[-1]) + 1
+        target = randint(1, self.accumulated_weight_sum[-1])
 
         while low < high:
             middle = low + (high - low) // 2
