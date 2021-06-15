0aad1eb65cb62499923161d48e33776553192f21
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-15 09:08:26 +0800

    p1041

---
+++ leetcode/algorithms/p1041_robot_bounded_in_circle.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        dx, dy = 0, 1
+        x = y = 0
+
+        for instruction in instructions:
+            if instruction == 'G':
+                x, y = x + dx, y + dy
+            elif instruction == 'L':
+                dx, dy = -dy, dx
+            elif instruction == 'R':
+                dx, dy = dy, -dx
+
+        return (x == 0 and y == 0) or (dx, dy) != (0, 1)
---
+++ tests/algorithms/p1041_robot_bounded_in_circle_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p1041_robot_bounded_in_circle import Solution
+
+
+class TestRobotBoundedInCircle(unittest.TestCase):
+    def test_robot_bounded_in_circle(self):
+        solution = Solution()
+
+        self.assertTrue(solution.isRobotBounded('GGLLGG'))
+        self.assertFalse(solution.isRobotBounded('GG'))
+        self.assertTrue(solution.isRobotBounded('GL'))
