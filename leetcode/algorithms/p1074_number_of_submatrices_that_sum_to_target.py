71e375369db14995f67c3afa1c0a25abe24569f2
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-19 18:23:59 +0800

    Update p1074_number_of_submatrices_that_sum_to_target.py

--- leetcode/algorithms/p1074_number_of_submatrices_that_sum_to_target.py
+++ leetcode/algorithms/p1074_number_of_submatrices_that_sum_to_target.py
@@ -11,17 +11,17 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         count = 0
 
-        for up in range(m):
-            nums = [0] * n
+        for i in range(m):
+            column_sum = [0] * n
 
-            for down in range(up, m):
+            for j in range(i, m):
                 mapping = collections.defaultdict(int)
                 mapping[0] = 1
                 sum_so_far = 0
 
                 for column in range(n):
-                    nums[column] += matrix[down][column]
-                    sum_so_far += nums[column]
+                    column_sum[column] += matrix[j][column]
+                    sum_so_far += column_sum[column]
                     count += mapping[sum_so_far - target]
                     mapping[sum_so_far] += 1
 
