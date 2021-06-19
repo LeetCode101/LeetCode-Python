3df66ffaba2bc71da833bab7c557b941c797c6b2
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-20 07:25:00 +0800

    p295

---
+++ leetcode/algorithms/p0295_find_median_from_data_stream_2.py
@@ -0,0 +1,23 @@
+import bisect
+from heapq import heappush, heappushpop
+
+
+class MedianFinder:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.small = []
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        if len(self.small) == len(self.large):
+            heappush(self.large, -heappushpop(self.small, -num))
+        else:
+            heappush(self.small, -heappushpop(self.large, num))
+
+    def findMedian(self) -> float:
+        if len(self.small) == len(self.large):
+            return float(self.large[0] - self.small[0]) / 2
+        else:
+            return float(self.large[0])
