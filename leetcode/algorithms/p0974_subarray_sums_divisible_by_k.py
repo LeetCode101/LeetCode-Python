5aeccc36028b2736eb65bdf5882c634ea877284e
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-19 16:46:36 +0800

    Update p0974_subarray_sums_divisible_by_k.py

--- leetcode/algorithms/p0974_subarray_sums_divisible_by_k.py
+++ leetcode/algorithms/p0974_subarray_sums_divisible_by_k.py
@@ -3,13 +3,13 @@ from typing import List
 
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        sum = 0
+        sum_so_far = 0
         mapping = {0: 1}
         count = 0
 
         for i, n in enumerate(nums):
-            sum += n
-            key = sum if k == 0 else sum % k
+            sum_so_far += n
+            key = sum_so_far if k == 0 else sum_so_far % k
             count += mapping.get(key, 0)
             mapping[key] = mapping.get(key, 0) + 1
 
