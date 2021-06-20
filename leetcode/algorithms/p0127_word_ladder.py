267bbadf3a2702afbee973d26f9fb021bf0644e3
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-20 16:04:50 +0800

    p127

---
+++ leetcode/algorithms/p0127_word_ladder.py
@@ -0,0 +1,40 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) \
+            -> int:
+        min_length = self._dfs(beginWord, endWord, wordList, 1, set())
+
+        return min_length if min_length < sys.maxsize else 0
+
+    def _dfs(self, begin_word, end_word, word_list, length, visited):
+        if begin_word == end_word:
+            return length
+
+        min_length = sys.maxsize
+
+        for word in word_list:
+            if word in visited:
+                continue
+
+            if self._is_one_letter_diff(begin_word, word):
+                visited.add(word)
+                min_length = min(min_length, self._dfs(word, end_word, word_list, length + 1, visited))
+                visited.remove(word)
+
+        return min_length
+
+    def _is_one_letter_diff(self, a, b):
+        letters = set(list(a))
+        count = 0
+
+        for c in b:
+            if c not in letters:
+                count += 1
+
+            if count > 1:
+                return False
+
+        return count == 1
---
+++ tests/algorithms/p0127_word_ladder_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms.p0127_word_ladder import Solution
+
+
+class TestWordLadder(unittest.TestCase):
+    def test_word_ladder(self):
+        solution = Solution()
+
+        self.assertEqual(4, solution.ladderLength(
+            'lost', 'miss', ['most', 'mist', 'miss', 'lost', 'fist', 'fish']))
+        self.assertEqual(5, solution.ladderLength(
+            'hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog']))
