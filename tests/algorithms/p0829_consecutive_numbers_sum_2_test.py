2735ef678383df489e664ef0a53ec7c6e5387cd7
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-20 17:05:56 +0800

    Create p0829_consecutive_numbers_sum_2_test.py

---
+++ tests/algorithms/p0829_consecutive_numbers_sum_2_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0829_consecutive_numbers_sum_2 import Solution
+
+
+class TestConsecutiveNumbersSum(unittest.TestCase):
+    def test_consecutive_numbers_sum(self):
+        solution = Solution()
+
+        self.assertEqual(2, solution.consecutiveNumbersSum(5))
+        self.assertEqual(4, solution.consecutiveNumbersSum(15))
