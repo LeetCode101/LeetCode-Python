570465d6df20ff0749746847dcfccdea0991e5c8
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-20 10:32:20 +0800

    Update p0460_lfu_cache_test.py

--- tests/algorithms/p0460_lfu_cache_test.py
+++ tests/algorithms/p0460_lfu_cache_test.py
@@ -4,6 +4,7 @@ from leetcode.algorithms.p0460_lfu_cache import LFUCache
 
 class TestLFUCache(unittest.TestCase):
     def test_lfu_cache(self):
+        LFUCache(0).put(1, 1)
         lfu_cache = LFUCache(2)
         lfu_cache.put(1, 1)
         lfu_cache.put(2, 2)
@@ -14,3 +15,7 @@ class TestLFUCache(unittest.TestCase):
 
         self.assertEqual(-1, lfu_cache.get(2))
         self.assertEqual(3, lfu_cache.get(3))
+
+        lfu_cache.put(3, 4)
+
+        self.assertEqual(4, lfu_cache.get(3))
