fe27dc2bf33a7c91902970e83ad5429d1358a7d7
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-20 13:48:02 +0800

    p588

---
+++ leetcode/algorithms/p0588_design_in_memory_file_system.py
@@ -0,0 +1,71 @@
+from typing import List
+
+
+class File:
+    def __init__(self, name, is_file=False):
+        self.name = name
+        self.is_file = is_file
+        self.content = ''
+        self.files = {}
+
+    def search(self, path):
+        paths = path.split('/')
+        file = self
+
+        for i in range(1, len(paths)):
+            file = file.files.get(paths[i], None)
+
+            if not file:
+                return None
+
+        return file
+
+    def ls(self):
+        return sorted(list(map(lambda f: f.name, list(self.files.values()))))
+
+
+class FileSystem:
+    def __init__(self):
+        self.root = File('')
+
+    def ls(self, path: str) -> List[str]:
+        if path == '/':
+            return self.root.ls()
+
+        file = self.root.search(path)
+
+        return file.ls() if file else []
+
+    def mkdir(self, path: str) -> None:
+        paths = path.split('/')
+        file = self.root
+
+        for i in range(1, len(paths)):
+            files = file.files
+            name = paths[i]
+
+            if name not in files:
+                file.files[name] = File(name)
+
+            file = files[name]
+
+    def addContentToFile(self, filePath: str, content: str) -> None:
+        paths = filePath.split('/')
+        file = self.root
+
+        for i in range(1, len(paths)):
+            files = file.files
+            name = paths[i]
+
+            if name not in files:
+                file.files[name] = File(name)
+
+            file = files[name]
+
+        file.is_file = True
+        file.content += content
+
+    def readContentFromFile(self, filePath: str) -> str:
+        file = self.root.search(filePath)
+
+        return file.content if file else ''
---
+++ tests/algorithms/p0588_design_in_memory_file_system_test.py
@@ -0,0 +1,15 @@
+import unittest
+from leetcode.algorithms.p0588_design_in_memory_file_system import FileSystem
+
+
+class TestDesignInMemoryFileSystem(unittest.TestCase):
+    def test_design_in_memory_file_system(self):
+        file_system = FileSystem()
+
+        self.assertListEqual([], file_system.ls('/'))
+
+        file_system.mkdir('/a/b/c')
+        file_system.addContentToFile('/a/b/c/d', 'hello')
+
+        self.assertListEqual(['a'], file_system.ls('/'))
+        self.assertEqual('hello', file_system.readContentFromFile('/a/b/c/d'))
