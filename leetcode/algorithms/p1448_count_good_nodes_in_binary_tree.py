7e776fd1c57d390c3a3148633facda0d70513f4b
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-22 20:04:47 +0800

    p1448

---
+++ leetcode/algorithms/p1448_count_good_nodes_in_binary_tree.py
@@ -0,0 +1,27 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        return self._dfs(root, root.val)
+
+    def _dfs(self, root, max_so_far):
+        if not root:
+            return 0
+
+        count = 1 if root.val >= max_so_far else 0
+
+        if root.left:
+            count += self._dfs(root.left, max(root.val, max_so_far))
+
+        if root.right:
+            count += self._dfs(root.right, max(root.val, max_so_far))
+
+        return count
---
+++ tests/algorithms/p1448_count_good_nodes_in_binary_tree_test.py
@@ -0,0 +1,15 @@
+import unittest
+from leetcode.algorithms.p1448_count_good_nodes_in_binary_tree \
+    import Solution, TreeNode
+
+
+class TestCountGoodNodesInBinaryTree(unittest.TestCase):
+    def test_count_good_nodes_in_binary_tree(self):
+        solution = Solution()
+        root = TreeNode(
+            3,
+            TreeNode(1, TreeNode(3)),
+            TreeNode(4, TreeNode(1), TreeNode(5))
+        )
+
+        self.assertEqual(4, solution.goodNodes(root))
