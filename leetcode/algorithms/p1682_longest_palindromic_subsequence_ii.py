ebb73f3343701888a6d095cf4845fa22857e336e
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-26 16:29:33 +0800

    p1682

---
+++ leetcode/algorithms/p1682_longest_palindromic_subsequence_ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        if not s:
+            return 0
+
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        chars = [[''] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = 0
+
+            for j in range(i + 1, n):
+                if s[i] == s[j] and (s[i] != chars[i][j - 1]
+                                     or s[i] != chars[i + 1][j]):
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                    chars[i][j] = s[i]
+                else:
+                    if dp[i + 1][j] > dp[i][j - 1]:
+                        dp[i][j] = dp[i + 1][j]
+                        chars[i][j] = chars[i + 1][j]
+                    else:
+                        dp[i][j] = dp[i][j - 1]
+                        chars[i][j] = chars[i][j - 1]
+
+        return dp[0][n - 1]
---
+++ tests/algorithms/p1682_longest_palindromic_subsequence_ii_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p1682_longest_palindromic_subsequence_ii \
+    import Solution
+
+
+class TestLongestPalindromicSubsequence(unittest.TestCase):
+    def test_longest_palindromic_subsequence(self):
+        solution = Solution()
+
+        self.assertEqual(4, solution.longestPalindromeSubseq('bbabab'))
+        self.assertEqual(4, solution.longestPalindromeSubseq('dcbccacdb'))
