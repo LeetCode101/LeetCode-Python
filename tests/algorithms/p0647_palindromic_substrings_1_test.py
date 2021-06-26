2849aa7782df09d6bf817b246b2ef04247684e98
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-26 12:16:31 +0800

    Update p0647_palindromic_substrings_1_test.py

--- tests/algorithms/p0647_palindromic_substrings_1_test.py
+++ tests/algorithms/p0647_palindromic_substrings_1_test.py
@@ -6,5 +6,5 @@ class TestPalindromicSubstrings(unittest.TestCase):
     def test_palindromic_substrings(self):
         solution = Solution()
 
-        # self.assertEqual(3, solution.countSubstrings('abc'))
+        self.assertEqual(3, solution.countSubstrings('abc'))
         self.assertEqual(6, solution.countSubstrings('aaa'))
