6d3577ebc1b84a933f354150f0dcdd8792ebe658
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-26 09:31:23 +0800

    rename file

--- tests/algorithms/p1062_longest_repeating_substring_1_test.py
+++ tests/algorithms/p1062_longest_repeating_substring_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p1062_longest_repeating_substring import Solution
+from leetcode.algorithms.p1062_longest_repeating_substring_1 import Solution
 
 
 class TestLongestRepeatingSubstring(unittest.TestCase):
