5f45a012d28604080af99ba7254bf7237d8e63a7
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-28 07:36:12 +0800

    p62

---
+++ tests/algorithms/p0062_unique_paths_2_test.py
@@ -0,0 +1,9 @@
+import unittest
+from leetcode.algorithms.p0062_unique_paths_2 import Solution
+
+
+class TestUniquePaths(unittest.TestCase):
+    def test_unique_paths(self):
+        solution = Solution()
+
+        self.assertEqual(28, solution.uniquePaths(3, 7))
