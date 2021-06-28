e23ef27c9c6359b2f1e57ee8f9c4981887abc4ab
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-28 09:34:03 +0800

    p55

---
+++ leetcode/algorithms/p0055_jump_game.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        return self._dfs(tuple(nums), 0)
+
+    def _dfs(self, nums, start):
+        if start == len(nums):
+            return True
+        elif start > len(nums):
+            return False
+
+        for step in range(nums[start]):
+            if self._dfs(nums, start + step + 1):
+                return True
+
+        return nums[start] == 0 and start == len(nums) - 1
---
+++ tests/algorithms/p0055_jump_game_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p0055_jump_game import Solution
+
+
+class TestJumpGame(unittest.TestCase):
+    def test_jump_game(self):
+        solution = Solution()
+
+        self.assertTrue(solution.canJump([]))
+        self.assertTrue(solution.canJump([2, 3, 1, 1, 4]))
+        self.assertFalse(solution.canJump([3, 2, 1, 0, 4]))
