fa35e31694eb38e2039a03a392af0077d4a3d083
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-28 08:01:55 +0800

    Update p1074_number_of_submatrices_that_sum_to_target_test.py

--- tests/algorithms/p1074_number_of_submatrices_that_sum_to_target_test.py
+++ tests/algorithms/p1074_number_of_submatrices_that_sum_to_target_test.py
@@ -7,5 +7,6 @@ class TestNumberOfSubmatricesThatSumToTarget(unittest.TestCase):
     def test_number_of_submatrices_that_sum_to_target(self):
         solution = Solution()
 
+        self.assertEqual(0, solution.numSubmatrixSumTarget([], 1))
         self.assertEqual(4, solution.numSubmatrixSumTarget(
             [[0, 1, 0], [1, 1, 1], [0, 1, 0]], 0))
