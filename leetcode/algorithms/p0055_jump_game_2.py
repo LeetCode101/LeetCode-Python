1b33b26f94e67bb7abadbdc8b081dd12262475bd
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-28 10:24:28 +0800

    Update p0055_jump_game_2.py

--- leetcode/algorithms/p0055_jump_game_2.py
+++ leetcode/algorithms/p0055_jump_game_2.py
@@ -4,11 +4,14 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         end = 0
+        n = len(nums)
 
-        for i, step in enumerate(nums):
+        for i in range(n - 1):
+            step = nums[i]
+
             if i > end:
                 return False
 
             end = max(end, i + step)
 
-        return True
+        return end >= n - 1
