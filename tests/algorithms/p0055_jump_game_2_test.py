7824e83e99661b73cc70039aeb5269a7aed6c0af
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-28 10:29:47 +0800

    Update p0055_jump_game_2_test.py

--- tests/algorithms/p0055_jump_game_2_test.py
+++ tests/algorithms/p0055_jump_game_2_test.py
@@ -7,6 +7,7 @@ class TestJumpGame(unittest.TestCase):
         solution = Solution()
 
         self.assertTrue(solution.canJump([]))
+        self.assertFalse(solution.canJump([0, 2, 3]))
         self.assertTrue(solution.canJump([3, 0, 8, 2, 0, 0, 1]))
         self.assertTrue(solution.canJump([2, 3, 1, 1, 4]))
         self.assertFalse(solution.canJump([3, 2, 1, 0, 4]))
