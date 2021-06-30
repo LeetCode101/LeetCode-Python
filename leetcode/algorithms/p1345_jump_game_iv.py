683f20760b021f11e0ca50c3bca20e5f3d777dfa
Frederick-S <mao_xiaodan@hotmail.com>
2021-06-30 09:40:57 +0800

    p1345

---
+++ leetcode/algorithms/p1345_jump_game_iv.py
@@ -0,0 +1,36 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        positions = collections.defaultdict(list)
+
+        for i, step in enumerate(arr):
+            positions[step].append(i)
+
+        queue = collections.deque([(0, 0)])
+        visited = set()
+        visited_groups = set()
+
+        while queue:
+            steps, i = queue.popleft()
+
+            if i == n - 1:
+                return steps
+
+            for neighbour in [i - 1, i + 1]:
+                if 0 <= neighbour < n and neighbour not in visited:
+                    visited.add(n)
+                    queue.append((steps + 1, neighbour))
+
+            if arr[i] not in visited_groups:
+                for neighbour in positions[arr[i]]:
+                    if neighbour not in visited:
+                        visited.add(neighbour)
+                        queue.append((steps + 1, neighbour))
+
+                visited_groups.add(arr[i])
+
+        return False
---
+++ tests/algorithms/p1345_jump_game_iv_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p1345_jump_game_iv import Solution
+
+
+class TestJumpGame(unittest.TestCase):
+    def test_jump_game(self):
+        solution = Solution()
+
+        self.assertEqual(3, solution.minJumps(
+            [100, -23, -23, 404, 100, 23, 23, 23, 3, 404]))
+        self.assertEqual(1, solution.minJumps([7, 6, 9, 6, 9, 6, 9, 7]))
