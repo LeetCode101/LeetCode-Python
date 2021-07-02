bc0b94466dcd5125eb26dbd8cbc191c1dffcef82
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-03 07:21:18 +0800

    p1696

---
+++ leetcode/algorithms/p1696_jump_game_vi_2.py
@@ -0,0 +1,22 @@
+import heapq
+import sys
+from typing import List
+
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+        heap = [(-nums[0], 0)]
+        max_score = nums[0]
+
+        for i in range(1, n):
+            while heap[0][1] < i - k:
+                heapq.heappop(heap)
+
+            max_score = -(-nums[i] + heap[0][0])
+            heapq.heappush(heap, max_score)
+
+        return max_score
---
+++ tests/algorithms/p1696_jump_game_vi_2_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p1696_jump_game_vi_2 import Solution
+
+
+class TestJumpGame(unittest.TestCase):
+    def test_jump_game(self):
+        solution = Solution()
+
+        self.assertEqual(0, solution.maxResult([], 1))
+        self.assertEqual(7, solution.maxResult([1, -1, -2, 4, -7, 3], 2))
+        self.assertEqual(17, solution.maxResult([10, -5, -2, 4, 0, 3], 3))
