ad8c631a09ce2e7d9139d85280da793b250ce623
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-03 14:46:19 +0800

    p1871

---
+++ leetcode/algorithms/p1871_jump_game_vii.py
@@ -0,0 +1,24 @@
+# Time Limit Exceeded!
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        if not s:
+            return False
+
+        n = len(s)
+        dp = [False] * n
+        dp[0] = True
+
+        for i in range(1, n):
+            if s[i] != '0':
+                continue
+
+            if i - maxJump < 0 and i - minJump < 0:
+                continue
+
+            for j in range(max(i - maxJump, 0), i - minJump + 1):
+                if dp[j]:
+                    dp[i] = True
+
+                    break
+
+        return dp[-1]
