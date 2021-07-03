0a8f7fa506c9995a1136b1f73d06806fcdf437ab
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-03 12:03:02 +0800

    p1696

---
+++ leetcode/algorithms/p1696_jump_game_vi_3.py
@@ -0,0 +1,27 @@
+import heapq
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        queue = deque([0])
+
+        for i in range(1, n):
+            if queue[0] < i - k:
+                queue.popleft()
+
+            dp[i] = nums[i] + dp[queue[0]]
+
+            while queue and dp[queue[-1]] <= dp[i]:
+                queue.pop()
+
+            queue.append(i)
+
+        return dp[-1]
