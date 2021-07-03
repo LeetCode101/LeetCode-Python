78113f3ca62eee1e406dd893a50376d948af72eb
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-03 16:46:44 +0800

    rename file

--- tests/algorithms/p0207_course_schedule_1_test.py
+++ tests/algorithms/p0207_course_schedule_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0207_course_schedule import Solution
+from leetcode.algorithms.p0207_course_schedule_1 import Solution
 
 
 class TestCourseSchedule(unittest.TestCase):
