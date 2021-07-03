3a76677678ec0d662d630a6180ed58f9d2f1c4b2
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-03 17:36:58 +0800

    p207

---
+++ leetcode/algorithms/p0207_course_schedule_2.py
@@ -0,0 +1,26 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) \
+            -> bool:
+        graph = [[] for _ in range(numCourses)]
+        degrees = [0] * numCourses
+
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+            degrees[course] += 1
+
+        queue = collections.deque([course for course, degree in enumerate(degrees) if degree == 0])
+
+        while queue:
+            course = queue.popleft()
+
+            for next_course in graph[course]:
+                degrees[next_course] -= 1
+
+                if degrees[next_course] == 0:
+                    queue.append(next_course)
+
+        return sum(degrees) == 0
---
+++ tests/algorithms/p0207_course_schedule_2_test.py
@@ -0,0 +1,13 @@
+import unittest
+from leetcode.algorithms.p0207_course_schedule_2 import Solution
+
+
+class TestCourseSchedule(unittest.TestCase):
+    def test_course_schedule(self):
+        solution = Solution()
+
+        self.assertTrue(solution.canFinish(
+            5, [[1, 4], [2, 4], [3, 1], [3, 2]]))
+        self.assertFalse(solution.canFinish(3, [[1, 0], [1, 2], [0, 1]]))
+        self.assertTrue(solution.canFinish(2, [[1, 0]]))
+        self.assertFalse(solution.canFinish(2, [[1, 0], [0, 1]]))
