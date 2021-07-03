ae8545e52ce689eca59de91df7944f60b8883a69
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-03 16:41:39 +0800

    p207

---
+++ leetcode/algorithms/p0207_course_schedule.py
@@ -0,0 +1,31 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        mapping = collections.defaultdict(list)
+
+        for prerequisite in prerequisites:
+            mapping[prerequisite[0]].append(prerequisite[1])
+
+        for i in range(numCourses):
+            if not self._can_finish(i, mapping, set(), set()):
+                return False
+
+        return True
+
+    def _can_finish(self, course, mapping, visited, finished):
+        if course in visited and course not in finished:
+            return False
+
+        visited.add(course)
+
+        if course in mapping:
+            for prerequisite in mapping[course]:
+                if not self._can_finish(prerequisite, mapping, visited, finished):
+                    return False
+
+        finished.add(course)
+
+        return True
