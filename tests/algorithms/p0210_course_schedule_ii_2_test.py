2fe7e1947d3af8454aad1d1fb48467caec245ccd
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-04 10:44:56 +0800

    update test

--- tests/algorithms/p0210_course_schedule_ii_2_test.py
+++ tests/algorithms/p0210_course_schedule_ii_2_test.py
@@ -6,6 +6,7 @@ class TestCourseSchedule(unittest.TestCase):
     def test_course_schedule(self):
         solution = Solution()
 
+        self.assertListEqual([], solution.findOrder(2, [[1, 0], [0, 1]]))
         self.assertListEqual([0, 1], solution.findOrder(2, [[1, 0]]))
         self.assertListEqual([0, 1, 2, 3], solution.findOrder(
             4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
