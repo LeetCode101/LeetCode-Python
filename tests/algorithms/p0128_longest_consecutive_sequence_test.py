a19229b2a1d2d72af0a7598642de6c50cd249187
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-07 21:42:36 +0800

    Update p0128_longest_consecutive_sequence_test.py

--- tests/algorithms/p0128_longest_consecutive_sequence_test.py
+++ tests/algorithms/p0128_longest_consecutive_sequence_test.py
@@ -6,6 +6,7 @@ class TestLongestConsecutiveSequence(unittest.TestCase):
     def test_longest_consecutive_sequence(self):
         solution = Solution()
 
-        self.assertEqual(4, solution.longestConsecutive([100, 4, 200, 1, 3, 2]))
+        self.assertEqual(4, solution.longestConsecutive(
+            [100, 4, 200, 1, 3, 2]))
         self.assertEqual(9, solution.longestConsecutive(
             [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
