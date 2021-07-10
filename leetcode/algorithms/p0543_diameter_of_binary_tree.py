ba7e2213069654ed6f4159b20d917fdb92d60323
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-10 17:41:32 +0800

    p543

---
+++ leetcode/algorithms/p0543_diameter_of_binary_tree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        left_depth = self._get_depth(root.left)
+        right_depth = self._get_depth(root.right)
+
+        return left_depth + right_depth
+
+    def _get_depth(self, root):
+        if not root:
+            return 0
+
+        return 1 + max(self._get_depth(root.left),
+                       self._get_depth(root.right))
