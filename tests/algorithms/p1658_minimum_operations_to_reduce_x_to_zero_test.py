6d77ef16a259ab9fa7410ec4afe3756e78e0de4c
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-12 09:34:56 +0800

    Update p1658_minimum_operations_to_reduce_x_to_zero_test.py

--- tests/algorithms/p1658_minimum_operations_to_reduce_x_to_zero_test.py
+++ tests/algorithms/p1658_minimum_operations_to_reduce_x_to_zero_test.py
@@ -7,5 +7,6 @@ class TestMinimumOperationsToReduceXToZero(unittest.TestCase):
     def test_minimum_operations_to_reduce_x_to_zero(self):
         solution = Solution()
 
+        self.assertEqual(1, solution.minOperations([1], 1))
         self.assertEqual(2, solution.minOperations([1, 1, 4, 2, 3], 5))
         self.assertEqual(-1, solution.minOperations([5, 6, 7, 8, 9], 4))
