146050e19cf181b8df6c138d21b498eb988e5856
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-14 09:32:20 +0800

    p1477

---
+++ leetcode/algorithms/p1477_find_two_non_overlapping_sub_arrays_each_with_target_sum_2.py
@@ -0,0 +1,30 @@
+import collections
+import sys
+from typing import List
+
+
+class Solution:
+    def minSumOfLengths(self, arr: List[int], target: int) -> int:
+        sum_so_far = 0
+        sum_mapping = collections.defaultdict(int)
+        sum_mapping[0] = -1
+        min_left_size = sys.maxsize
+        min_length = sys.maxsize
+
+        for i, n in enumerate(arr):
+            sum_so_far += n
+            sum_mapping[sum_so_far] = i
+
+        sum_so_far = 0
+
+        for i, n in enumerate(arr):
+            sum_so_far += n
+
+            if sum_so_far - target in sum_mapping:
+                min_left_size = min(
+                    min_left_size, i - sum_mapping[sum_so_far - target])
+
+            if sum_so_far + target in sum_mapping and min_left_size != sys.maxsize:
+                min_length = min(min_length, sum_mapping[sum_so_far + target] - i + min_left_size)
+
+        return -1 if min_length == sys.maxsize else min_length
---
+++ tests/algorithms/p1477_find_two_non_overlapping_sub_arrays_each_with_target_sum_2_test.py
@@ -0,0 +1,14 @@
+import unittest
+from leetcode.algorithms\
+    .p1477_find_two_non_overlapping_sub_arrays_each_with_target_sum_2 \
+    import Solution
+
+
+class TestFindTwoNonOverlappingSubArraysEachWithTargetSum(unittest.TestCase):
+    def test_find_two_non_overlapping_sub_arrays_each_with_target_sum(self):
+        solution = Solution()
+
+        self.assertEqual(-1, solution.minSumOfLengths([1, 6, 1], 7))
+        self.assertEqual(2, solution.minSumOfLengths([3, 2, 2, 4, 3], 3))
+        self.assertEqual(-1, solution.minSumOfLengths(
+            [4, 3, 2, 6, 2, 3, 4], 6))
