6030bab49d692dfecc9bf409867d11760e7713b5
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-18 10:18:38 +0800

    Update p0340_longest_substring_with_at_most_k_distinct_characters.py

--- leetcode/algorithms/p0340_longest_substring_with_at_most_k_distinct_characters.py
+++ leetcode/algorithms/p0340_longest_substring_with_at_most_k_distinct_characters.py
@@ -3,22 +3,20 @@ import collections
 
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        chars = set()
         counter = collections.defaultdict(int)
         start = 0
         max_length = 0
 
         for end in range(len(s)):
             c = s[end]
-            chars.add(c)
             counter[c] += 1
 
-            if len(chars) > k:
-                while start < len(s) and len(chars) > k:
+            if len(counter) > k:
+                while start < len(s) and len(counter) > k:
                     counter[s[start]] -= 1
 
                     if counter[s[start]] == 0:
-                        chars.remove(s[start])
+                        counter.pop(s[start])
 
                     start += 1
 
