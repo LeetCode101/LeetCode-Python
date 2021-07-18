54a05a485d3ac53d8c284a06ef018adfa1b10f90
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-18 15:23:52 +0800

    p76

---
+++ leetcode/algorithms/p0076_minimum_window_substring_2.py
@@ -0,0 +1,35 @@
+import collections
+import sys
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        counter = collections.Counter(t)
+        min_window = ''
+        min_window_length = sys.maxsize
+        start = 0
+        count = 0
+
+        for end, c in enumerate(s):
+            if c not in counter:
+                continue
+
+            counter[c] -= 1
+
+            if counter[c] >= 0:
+                count += 1
+
+            while count == len(t):
+                if end - start + 1 < min_window_length:
+                    min_window_length = end - start + 1
+                    min_window = s[start:end + 1]
+
+                if s[start] in counter:
+                    counter[s[start]] += 1
+
+                    if counter[s[start]] > 0:
+                        count -= 1
+
+                start += 1
+
+        return min_window
---
+++ tests/algorithms/p0076_minimum_window_substring_2_test.py
@@ -0,0 +1,9 @@
+import unittest
+from leetcode.algorithms.p0076_minimum_window_substring_2 import Solution
+
+
+class TestMinimumWindowSubstring(unittest.TestCase):
+    def test_minimum_window_substring(self):
+        solution = Solution()
+
+        self.assertEqual('BANC', solution.minWindow('ADOBECODEBANC', 'ABC'))
