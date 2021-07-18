efe2046e4e5ba97e895392159f0a4080734dbb60
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-18 15:10:45 +0800

    p76

---
+++ leetcode/algorithms/p0076_minimum_window_substring.py
@@ -0,0 +1,31 @@
+import collections
+import sys
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        min_window = ''
+        min_window_length = sys.maxsize
+
+        for start, c in enumerate(s):
+            current_counter = collections.Counter(t)
+            end = start
+
+            while end < len(s):
+                if s[end] in current_counter:
+                    current_counter[s[end]] -= 1
+
+                    if current_counter[s[end]] == 0:
+                        current_counter.pop(s[end])
+
+                    if len(current_counter) == 0:
+                        break
+
+                end += 1
+
+            if len(current_counter) == 0 \
+                    and end - start + 1 < min_window_length:
+                min_window_length = end - start + 1
+                min_window = s[start:end + 1]
+
+        return min_window
