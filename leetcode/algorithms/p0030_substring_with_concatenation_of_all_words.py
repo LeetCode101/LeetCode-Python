76e7076346ac22554184ee8cd0e3101bc49be367
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-18 12:44:45 +0800

    p30

---
+++ leetcode/algorithms/p0030_substring_with_concatenation_of_all_words.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        sorted_words = sorted(words)
+        current_words = []
+        word_length = len(words[0])
+        start = 0
+        positions = []
+
+        for end in range(len(words) * word_length - 1, len(s)):
+            for i in range(len(words)):
+                word = s[start + i * word_length: start + (i + 1) * word_length]
+                current_words.append(word)
+
+            if sorted(current_words) == sorted_words:
+                positions.append(start)
+
+            start += 1
+            current_words = []
+
+        return positions
