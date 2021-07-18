ead28da8a82b1b79dbf81604ce707600b7f22e93
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-18 08:20:06 +0800

    Update p0918_maximum_sum_circular_subarray.py

--- leetcode/algorithms/p0918_maximum_sum_circular_subarray.py
+++ leetcode/algorithms/p0918_maximum_sum_circular_subarray.py
@@ -3,13 +3,15 @@ from typing import List
 
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        total, max_sum, max_so_far, min_sum, min_so_far = 0, nums[0], 0, nums[0], 0
+        total, max_sum, max_so_far, min_sum, min_so_far = \
+            nums[0], nums[0], nums[0], nums[0], nums[0]
 
-        for n in nums:
+        for i in range(1, len(nums)):
+            n = nums[i]
             max_so_far = max(max_so_far + n, n)
             max_sum = max(max_sum, max_so_far)
             min_so_far = min(min_so_far + n, n)
             min_sum = min(min_sum, min_so_far)
-            total +=n
+            total += n
 
         return max(max_sum, total - min_sum) if max_sum > 0 else max_sum
