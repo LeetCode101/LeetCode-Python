127bfdc2e39d9f9fda47086a67508b01365b0671
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-18 11:11:45 +0800

    p438

---
+++ leetcode/algorithms/p0438_find_all_anagrams_in_a_string.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+
+        anagrams = set()
+
+        self._get_anagrams(p, len(p), '', anagrams)
+
+        start, n = 0, len(s)
+        chars_so_far = ''
+        positions = []
+
+        for c in s:
+            chars_so_far += c
+
+            if len(chars_so_far) < len(p):
+                continue
+            elif len(chars_so_far) > len(p):
+                chars_so_far = chars_so_far[1:]
+                start += 1
+
+            if chars_so_far in anagrams:
+                positions.append(start)
+
+        return positions
+
+    def _get_anagrams(self, s, n, anagram, anagrams):
+        if len(anagram) == n:
+            anagrams.add(anagram)
+
+            return
+
+        for i, c in enumerate(s):
+            self._get_anagrams(s[0:i] + s[i + 1:], n, anagram + c, anagrams)
