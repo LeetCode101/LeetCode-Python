7a867f44687f042e9ea706759a2de84c426114a8
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-18 14:38:37 +0800

    Update p0030_substring_with_concatenation_of_all_words_test.py

--- tests/algorithms/p0030_substring_with_concatenation_of_all_words_test.py
+++ tests/algorithms/p0030_substring_with_concatenation_of_all_words_test.py
@@ -7,6 +7,9 @@ class TestSubstringWithConcatenationOfAllWords(unittest.TestCase):
     def test_substring_with_concatenation_of_all_words(self):
         solution = Solution()
 
+        self.assertListEqual([], solution.findSubstring('abc', ['foo']))
+        self.assertListEqual([], solution.findSubstring(
+            'foofoo', ['foo', 'bar']))
         self.assertListEqual([], solution.findSubstring(
             'wordgoodgoodgoodbestword', ['word', 'good', 'best', 'word']))
         self.assertListEqual([0, 9], solution.findSubstring(
