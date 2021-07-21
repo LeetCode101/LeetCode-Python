419d4e1d94297c05318c6f52ad702b14519b4016
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-21 09:17:44 +0800

    Update p0340_longest_substring_with_at_most_k_distinct_characters.py

--- leetcode/algorithms/p0340_longest_substring_with_at_most_k_distinct_characters.py
+++ leetcode/algorithms/p0340_longest_substring_with_at_most_k_distinct_characters.py
@@ -11,14 +11,13 @@ class Solution:
             c = s[end]
             counter[c] += 1
 
-            if len(counter) > k:
-                while start < len(s) and len(counter) > k:
-                    counter[s[start]] -= 1
+            while len(counter) > k and start < len(s):
+                counter[s[start]] -= 1
 
-                    if counter[s[start]] == 0:
-                        counter.pop(s[start])
+                if counter[s[start]] == 0:
+                    counter.pop(s[start])
 
-                    start += 1
+                start += 1
 
             max_length = max(max_length, end - start + 1)
 
