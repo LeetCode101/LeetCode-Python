e53269b39a43098762dcd88222de72e4fba2073c
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-24 16:06:52 +0800

    Update p1248_count_number_of_nice_subarrays_1_test.py

--- tests/algorithms/p1248_count_number_of_nice_subarrays_1_test.py
+++ tests/algorithms/p1248_count_number_of_nice_subarrays_1_test.py
@@ -6,6 +6,6 @@ class TestCountNumberOfNiceSubarrays(unittest.TestCase):
     def test_count_number_of_nice_subarrays(self):
         solution = Solution()
 
-        # self.assertEqual(2, solution.numberOfSubarrays([1, 1, 2, 1, 1], 3))
+        self.assertEqual(2, solution.numberOfSubarrays([1, 1, 2, 1, 1], 3))
         self.assertEqual(16, solution.numberOfSubarrays(
             [2, 2, 2, 1, 2, 2, 1, 2, 2, 2], 2))
