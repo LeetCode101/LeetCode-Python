2d26c8ddcae5aba3dd308505841d436b756ff5f1
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-25 16:30:39 +0800

    Update p0424_longest_repeating_character_replacement.py

--- leetcode/algorithms/p0424_longest_repeating_character_replacement.py
+++ leetcode/algorithms/p0424_longest_repeating_character_replacement.py
@@ -4,15 +4,13 @@ import collections
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         start = 0
-        max_count = 0
         max_length = 0
         counter = collections.defaultdict(int)
 
         for end, c in enumerate(s):
             counter[c] += 1
-            max_count = max(max_count, counter[c])
 
-            if end - start + 1 - max_count > k:
+            if end - start + 1 - max(counter.values()) > k:
                 counter[s[start]] -= 1
                 start += 1
 
