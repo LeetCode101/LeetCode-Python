15920d157518804cc3f3367d47db8d8e963050b8
Frederick-S <mao_xiaodan@hotmail.com>
2021-07-30 08:54:46 +0800

    rename file

--- tests/algorithms/p0472_concatenated_words_1_test.py
+++ tests/algorithms/p0472_concatenated_words_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0472_concatenated_words import Solution
+from leetcode.algorithms.p0472_concatenated_words_1 import Solution
 
 
 class TestConcatenatedWords(unittest.TestCase):
