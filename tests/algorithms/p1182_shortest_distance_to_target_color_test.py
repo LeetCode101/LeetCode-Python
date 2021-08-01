eb33ab6b68391007dc518085de384e9ffd7663d9
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-01 15:45:54 +0800

    Update p1182_shortest_distance_to_target_color_test.py

--- tests/algorithms/p1182_shortest_distance_to_target_color_test.py
+++ tests/algorithms/p1182_shortest_distance_to_target_color_test.py
@@ -7,5 +7,9 @@ class TestShortestDistanceToTargetColor(unittest.TestCase):
     def test_shortest_distance_to_target_color(self):
         solution = Solution()
 
+        self.assertListEqual([-1], solution.shortestDistanceColor(
+            [1, 2], [[0, 3]]))
         self.assertListEqual([3, 0, 3], solution.shortestDistanceColor(
             [1, 1, 2, 1, 3, 2, 2, 3, 3], [[1, 3], [2, 2], [6, 1]]))
+        self.assertEqual([0, -1, -1, 1, 1], solution.shortestDistanceColor(
+            [2, 1, 2, 2, 1], [[1, 1], [4, 3], [1, 3], [4, 2], [2, 1]]))
