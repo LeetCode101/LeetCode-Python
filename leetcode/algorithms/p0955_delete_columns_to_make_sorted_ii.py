eb17f38c6edf42594012aa9df09bd7aecac9a0ad
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-07 16:44:50 +0800

    Update p0955_delete_columns_to_make_sorted_ii.py

--- leetcode/algorithms/p0955_delete_columns_to_make_sorted_ii.py
+++ leetcode/algorithms/p0955_delete_columns_to_make_sorted_ii.py
@@ -8,16 +8,19 @@ class Solution:
         in_order = [False] * (m - 1)
 
         for i in range(n):
+            removed = False
             current_in_order = in_order[:]
 
             for j in range(m - 1):
                 if not in_order[j] and strs[j][i] > strs[j + 1][i]:
                     count += 1
+                    removed = True
 
                     break
                 elif strs[j][i] < strs[j + 1][i] and not in_order[j]:
                     current_in_order[j] = True
-            else:
+            
+            if not removed:
                 in_order = current_in_order
 
         return count
