7eccd1b07d248916614c956776adecd8a17fe1db
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-08 14:39:37 +0800

    Update p0532_k_diff_pairs_in_an_array_test.py

--- tests/algorithms/p0532_k_diff_pairs_in_an_array_test.py
+++ tests/algorithms/p0532_k_diff_pairs_in_an_array_test.py
@@ -6,6 +6,7 @@ class TestKDiffPairsInAnArray(unittest.TestCase):
     def test_k_diff_pairs_in_an_array(self):
         solution = Solution()
 
-        # self.assertEqual(2, solution.findPairs([3, 1, 4, 1, 5], 2))
+        self.assertEqual(1, solution.findPairs([1, 1], 0))
+        self.assertEqual(2, solution.findPairs([3, 1, 4, 1, 5], 2))
         self.assertEqual(2, solution.findPairs(
             [1, 2, 4, 4, 3, 3, 0, 9, 2, 3], 3))
