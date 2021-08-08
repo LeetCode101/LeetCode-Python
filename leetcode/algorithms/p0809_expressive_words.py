05e59a2d644f05d37e5f4f0c97d651a7882b44ae
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-08 15:29:28 +0800

    p809

---
+++ leetcode/algorithms/p0809_expressive_words.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def expressiveWords(self, s: str, words: List[str]) -> int:
+        count = 0
+
+        for word in words:
+            i = j = 0
+
+            while i < len(s) and j < len(word):
+                if s[i] != word[j]:
+                    break
+
+                length1 = 1
+                length2 = 1
+
+                while i + 1 < len(s) and s[i] == s[i + 1]:
+                    i += 1
+                    length1 += 1
+
+                while j + 1 < len(word) and word[j] == word[j + 1]:
+                    j += 1
+                    length2 += 1
+
+                if (length1 == length2) or (length1 >= 3 and length2 < length1):
+                    i += 1
+                    j += 1
+                else:
+                    break
+
+            if i == len(s) and j == len(word):
+                count += 1
+
+        return count
---
+++ tests/algorithms/p0809_expressive_words_test.py
@@ -0,0 +1,13 @@
+import unittest
+from leetcode.algorithms.p0809_expressive_words import Solution
+
+
+class TestExpressiveWords(unittest.TestCase):
+    def test_expressive_words(self):
+        solution = Solution()
+
+        self.assertEqual(0, solution.expressiveWords('abcd', ['abc']))
+        self.assertEqual(1, solution.expressiveWords('heeellooo', 
+                                                     ['hello', 'hi', 'helo']))
+        self.assertEqual(3, solution.expressiveWords('zzzzzyyyyy', 
+                                                     ['zzyy', 'zy', 'zyy']))
