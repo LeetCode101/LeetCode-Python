5cf8874355cc54798bea68cb8122c1cc0a380c4a
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-08 11:49:53 +0800

    Update p0723_candy_crush.py

--- leetcode/algorithms/p0723_candy_crush.py
+++ leetcode/algorithms/p0723_candy_crush.py
@@ -3,11 +3,13 @@ from typing import List
 
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        m, n = len(board), len(board[0])
+
         while True:
             crush = set()
 
-            for i in range(len(board)):
-                for j in range(len(board[0])):
+            for i in range(m):
+                for j in range(n):
                     if j > 1 and board[i][j] != 0 and \
                             board[i][j] == board[i][j - 1] == board[i][j - 2]:
                         crush.add((i, j))
@@ -26,10 +28,10 @@ class Solution:
             for i, j in crush:
                 board[i][j] = 0
 
-            for j in range(len(board[0])):
-                k = len(board) - 1
+            for j in range(n):
+                k = m - 1
 
-                for i in range(len(board) - 1, -1, -1):
+                for i in range(m - 1, -1, -1):
                     if board[i][j] != 0:
                         board[k][j] = board[i][j]
                         k -= 1
