776c6e7d7b55369a0d2c9bd3925be7c7ac43be47
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-09 10:19:11 +0800

    p1813

---
+++ tests/algorithms/p1813_sentence_similarity_iii_3_test.py
@@ -0,0 +1,14 @@
+import unittest
+from leetcode.algorithms.p1813_sentence_similarity_iii_3 import Solution
+
+
+class TestSentenceSimilarity(unittest.TestCase):
+    def test_sentence_similarity(self):
+        solution = Solution()
+
+        self.assertTrue(solution.areSentencesSimilar(
+            'My name is Haley', 'My Haley'))
+        self.assertFalse(solution.areSentencesSimilar('of', 'A lot of words'))
+        self.assertTrue(solution.areSentencesSimilar(
+            'Eating right now', 'Eating'))
+        self.assertFalse(solution.areSentencesSimilar('Luky', 'Lucccky'))
