cf4c6530d73a19b55ef8f04f8610ecb1534ab2aa
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-11 10:03:23 +0800

    Update p0876_middle_of_the_linked_list_test.py

--- tests/algorithms/p0876_middle_of_the_linked_list_test.py
+++ tests/algorithms/p0876_middle_of_the_linked_list_test.py
@@ -6,7 +6,8 @@ from leetcode.algorithms.p0876_middle_of_the_linked_list \
 class TestMiddleOfTheLinkedList(unittest.TestCase):
     def test_middle_of_the_linked_list(self):
         solution = Solution()
-        head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
+        head = ListNode(1, ListNode(2, ListNode(3, ListNode(
+            4, ListNode(5, ListNode(6))))))
 
         self.assertIsNone(solution.middleNode(None))
         self.assertEqual(4, solution.middleNode(head).val)
