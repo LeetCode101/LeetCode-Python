4cfd8e94ae2ac33a63508d65c489a22924f44ff6
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-13 10:31:12 +0800

    p556

---
+++ tests/algorithms/p0556_next_greater_element_iii_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0556_next_greater_element_iii import Solution
+
+
+class TestNextGreaterElement(unittest.TestCase):
+    def test_next_greater_element(self):
+        solution = Solution()
+
+        self.assertEqual(21, solution.nextGreaterElement(12))
+        self.assertEqual(-1, solution.nextGreaterElement(21))
