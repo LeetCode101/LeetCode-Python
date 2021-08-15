94389fe26fa30e3e8f88661c7ad52845414ee63b
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-15 15:10:38 +0800

    Update p1745_palindrome_partitioning_iv.py

--- leetcode/algorithms/p1745_palindrome_partitioning_iv.py
+++ leetcode/algorithms/p1745_palindrome_partitioning_iv.py
@@ -4,15 +4,13 @@ class Solution:
         dp = [[False] * n for _ in range(n)]
 
         for i in range(n - 1, -1, -1):
-            for j in range(n):
-                if i >= j:
-                    dp[i][j] = True
-                elif s[i] == s[j]:
-                    dp[i][j] = dp[i + 1][j - 1]
+            for j in range(i, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] if i + 1 <= j - 1 else True
 
-        for i in range(1, n):
-            for j in range(i + 1, n):
-                if dp[0][i - 1] and dp[i][j - 1] and dp[j][n - 1]:
+        for i in range(1, n - 1):
+            for j in range(i, n - 1):
+                if dp[0][i - 1] and dp[i][j] and dp[j + 1][n - 1]:
                     return True
 
         return False
