3501a9b40f345449cf89f5ed30415cce433dff88
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-15 13:22:15 +0800

    Update p1278_palindrome_partitioning_iii_test.py

--- tests/algorithms/p1278_palindrome_partitioning_iii_test.py
+++ tests/algorithms/p1278_palindrome_partitioning_iii_test.py
@@ -9,3 +9,5 @@ class TestPalindromePartitioning(unittest.TestCase):
         self.assertEqual(1, solution.palindromePartition('abc', 2))
         self.assertEqual(0, solution.palindromePartition('aabbc', 3))
         self.assertEqual(0, solution.palindromePartition('leetcode', 8))
+        self.assertEqual(6, solution.palindromePartition(
+            'spsvmwkvwyfnrrfklevvyxsayc', 6))
