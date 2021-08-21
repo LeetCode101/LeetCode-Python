7f4fb7897b3df436feeac496139047eb1544f7d7
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-21 20:07:15 +0800

    Update p1712_ways_to_split_array_into_three_subarrays.py

--- leetcode/algorithms/p1712_ways_to_split_array_into_three_subarrays.py
+++ leetcode/algorithms/p1712_ways_to_split_array_into_three_subarrays.py
@@ -3,26 +3,26 @@ from typing import List
 
 class Solution:
     def waysToSplit(self, nums: List[int]) -> int:
-        prefix_sum = [0]
+        prefix_sum = [n for n in nums]
 
-        for n in nums:
-            prefix_sum.append(prefix_sum[-1] + n)
+        for i in range(1, len(nums)):
+            prefix_sum[i] = nums[i] + prefix_sum[i - 1]
 
         count = 0
         j = k = 0
 
-        for i in range(1, len(nums)):
+        for i in range(len(nums) - 2):
             j = max(j, i + 1)
 
-            while j < len(nums) and prefix_sum[i] > prefix_sum[j] - prefix_sum[i]:
+            while j < len(nums) - 1 and prefix_sum[i] > prefix_sum[j] - prefix_sum[i]:
                 j += 1
 
             k = max(k, j)
 
-            while k < len(nums) \
+            while k < len(nums) - 1 \
                     and prefix_sum[k] - prefix_sum[i] <= prefix_sum[-1] - prefix_sum[k]:
                 k += 1
 
-            count += k - j
+            count = (count + k - j) % 1000000007
 
-        return count % 1_000_000_007
+        return count
