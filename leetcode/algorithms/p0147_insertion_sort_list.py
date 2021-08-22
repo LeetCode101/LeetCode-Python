4a5f31e272bbdadf51b13f4715f27ca774e6502c
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-22 14:21:58 +0800

    Update p0147_insertion_sort_list.py

--- leetcode/algorithms/p0147_insertion_sort_list.py
+++ leetcode/algorithms/p0147_insertion_sort_list.py
@@ -9,7 +9,7 @@ class Solution:
         dummy = ListNode(0)
         dummy.next = head
         tail = head.next
-        prev_tail = head
+        sorted_list_tail = head
 
         while tail:
             rest = tail.next
@@ -19,15 +19,13 @@ class Solution:
                 prev = prev.next
 
             if prev.next is tail:
-                prev_tail = tail
+                sorted_list_tail = tail
                 tail = rest
 
                 continue
 
             prev.next, tail.next = tail, prev.next
-
-            if prev_tail:
-                prev_tail.next = rest
-                tail = rest
+            sorted_list_tail.next = rest
+            tail = rest
 
         return dummy.next
