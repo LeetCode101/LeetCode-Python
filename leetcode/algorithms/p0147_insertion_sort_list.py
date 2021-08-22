9bec95b594abd6ccbb62b66af92dc3a9d7730cf9
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-22 14:19:34 +0800

    Update p0147_insertion_sort_list.py

--- leetcode/algorithms/p0147_insertion_sort_list.py
+++ leetcode/algorithms/p0147_insertion_sort_list.py
@@ -9,6 +9,7 @@ class Solution:
         dummy = ListNode(0)
         dummy.next = head
         tail = head.next
+        prev_tail = head
 
         while tail:
             rest = tail.next
@@ -17,20 +18,16 @@ class Solution:
             while prev.next is not tail and prev.next.val <= tail.val:
                 prev = prev.next
 
-            current_tail = prev.next
-
-            while current_tail and current_tail.next is not tail:
-                current_tail = current_tail.next
-
             if prev.next is tail:
+                prev_tail = tail
                 tail = rest
 
                 continue
 
             prev.next, tail.next = tail, prev.next
 
-            if current_tail:
-                current_tail.next = rest
+            if prev_tail:
+                prev_tail.next = rest
                 tail = rest
 
         return dummy.next
