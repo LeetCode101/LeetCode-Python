132166617f8cdb1f93f0a3b2e678b1dbaa6dc8ab
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-25 09:25:56 +0800

    Update p0877_stone_game_2.py

--- leetcode/algorithms/p0877_stone_game_2.py
+++ leetcode/algorithms/p0877_stone_game_2.py
@@ -9,9 +9,9 @@ class Solution:
         for i in range(n):
             dp[i][i] = piles[i]
 
-        for d in range(1, n):
-            for i in range(n - d):
-                dp[i][i + d] = max(piles[i] - dp[i + 1][i + d],
-                                   piles[i + d] - dp[i][i + d - 1])
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                dp[i][j] = max(piles[i] - dp[i + 1][j],
+                               piles[j] - dp[i][j - 1])
 
         return dp[0][-1] > 0
