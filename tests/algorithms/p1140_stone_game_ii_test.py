a2c307a0c2074604b96b6f78aed4cff9f27a4b21
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-27 09:42:34 +0800

    Update p1140_stone_game_ii_test.py

--- tests/algorithms/p1140_stone_game_ii_test.py
+++ tests/algorithms/p1140_stone_game_ii_test.py
@@ -8,3 +8,5 @@ class TestStoneGame(unittest.TestCase):
 
         self.assertEqual(10, solution.stoneGameII([2, 7, 9, 4, 4]))
         self.assertEqual(104, solution.stoneGameII([1, 2, 3, 4, 5, 100]))
+        self.assertEqual(479, solution.stoneGameII(
+            [94, 65, 70, 79, 91, 80, 21, 67, 84, 64, 34, 22, 14, 10, 72]))
