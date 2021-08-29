1aec8db2a6f415c92fe95c3f922541f2aa6edcfe
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-29 12:14:32 +0800

    Update p1563_stone_game_v.py

--- leetcode/algorithms/p1563_stone_game_v.py
+++ leetcode/algorithms/p1563_stone_game_v.py
@@ -5,7 +5,7 @@ from typing import List
 class Solution:
     def stoneGameV(self, stoneValue: List[int]) -> int:
         n = len(stoneValue)
-        prefix_sum = [i for i in stoneValue] + [0]
+        prefix_sum = [i for i in stoneValue]
 
         for i in range(n - 1, -1, -1):
             prefix_sum[i] += prefix_sum[i + 1] if i + 1 < n else 0
@@ -25,7 +25,7 @@ class Solution:
             left = [start, i]
             right = [i + 1, end]
             left_sum = prefix_sum[start] - prefix_sum[i + 1]
-            right_sum = prefix_sum[i + 1] - prefix_sum[end + 1]
+            right_sum = prefix_sum[i + 1] - (prefix_sum[end + 1] if end + 1 < len(prefix_sum) else 0)
 
             if i > start and 2 * min(left_sum, right_sum) < max_score:
                 continue
