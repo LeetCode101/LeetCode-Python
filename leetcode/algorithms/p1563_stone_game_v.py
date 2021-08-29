4f118cce1b33c5c38f897a5994bfaea95c8d78d7
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-29 12:31:33 +0800

    Update p1563_stone_game_v.py

--- leetcode/algorithms/p1563_stone_game_v.py
+++ leetcode/algorithms/p1563_stone_game_v.py
@@ -23,19 +23,24 @@ class Solution:
         for i in range(start, end):
             left_start, left_end = start, i
             right_start, right_end = i + 1, end
-            left_sum = prefix_sum[left_end] - (prefix_sum[left_start - 1] if left_start - 1 >= 0 else 0)
+            left_sum = prefix_sum[left_end] - (prefix_sum[left_start - 1]
+                                               if left_start - 1 >= 0 else 0)
             right_sum = prefix_sum[right_end] - prefix_sum[right_start - 1]
             score = 0
 
             if 2 * min(left_sum, right_sum) <= max_score:
                 continue
             elif left_sum > right_sum:
-                score = right_sum + self._dfs(right_start, right_end, prefix_sum, memo)
+                score = right_sum + self._dfs(
+                    right_start, right_end, prefix_sum, memo)
             elif right_sum > left_sum:
-                score = left_sum + self._dfs(left_start, left_end, prefix_sum, memo)
+                score = left_sum + self._dfs(
+                    left_start, left_end, prefix_sum, memo)
             else:
-                left_score = left_sum + self._dfs(left_start, left_end, prefix_sum, memo)
-                right_score = right_sum + self._dfs(right_start, right_end, prefix_sum, memo)
+                left_score = left_sum + self._dfs(
+                    left_start, left_end, prefix_sum, memo)
+                right_score = right_sum + self._dfs(
+                    right_start, right_end, prefix_sum, memo)
                 score = max(left_score, right_score)
 
             max_score = max(max_score, score)
