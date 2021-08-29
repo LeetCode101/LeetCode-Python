1d30102f13d839cf5d1e3e7bca681f20fc48e35a
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-29 12:30:41 +0800

    Update p1563_stone_game_v_test.py

--- leetcode/algorithms/p1563_stone_game_v_test.py
+++ leetcode/algorithms/p1563_stone_game_v_test.py
@@ -9,3 +9,5 @@ class TestStoneGame(unittest.TestCase):
         self.assertEqual(18, solution.stoneGameV([6, 2, 3, 4, 5, 5]))
         self.assertEqual(28, solution.stoneGameV([7, 7, 7, 7, 7, 7, 7]))
         self.assertEqual(0, solution.stoneGameV([4]))
+        self.assertEqual(1103936, solution.stoneGameV(
+            [62008, 269055, 379802, 503405, 589774]))
