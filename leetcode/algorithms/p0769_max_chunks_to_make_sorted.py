caf9982c76bba69a76b767202c5f636b826ca3a6
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-03 09:51:06 +0800

    Update p0769_max_chunks_to_make_sorted.py

--- leetcode/algorithms/p0769_max_chunks_to_make_sorted.py
+++ leetcode/algorithms/p0769_max_chunks_to_make_sorted.py
@@ -7,13 +7,13 @@ class Solution:
         count = 0
 
         for i, n in enumerate(arr):
-            while stack and stack[-1][1] < n:
-                _, prev_max = stack.pop()
+            while stack and stack[-1] < n:
+                prev_max = stack.pop()
                 prev_sorted_array_size = prev_max + 1
 
                 if not stack and prev_sorted_array_size == i:
                     count += 1
 
-            stack.append((i, n))
+            stack.append(n)
 
         return count + (1 if len(stack) > 0 else 0)
