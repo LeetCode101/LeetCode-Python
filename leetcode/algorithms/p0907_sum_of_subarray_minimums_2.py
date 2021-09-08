6c7a8280461a828ea2cd49c44e65ff1e59872578
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-08 09:32:52 +0800

    Update p0907_sum_of_subarray_minimums_2.py

--- leetcode/algorithms/p0907_sum_of_subarray_minimums_2.py
+++ leetcode/algorithms/p0907_sum_of_subarray_minimums_2.py
@@ -11,11 +11,11 @@ class Solution:
 
         for i in range(n):
             while stack and arr[stack[-1]] >= arr[i]:
-                j = stack.pop()
+                index = stack.pop()
                 prev_index = stack[-1] if stack else -1
-                prev_count = j - prev_index - 1
-                next_count = i - j - 1
-                result += arr[j] * (prev_count + 1) * (next_count + 1) % mod
+                prev_count = index - prev_index - 1
+                next_count = i - index - 1
+                result += arr[index] * (prev_count + 1) * (next_count + 1) % mod
                 result %= mod
 
             stack.append(i)
