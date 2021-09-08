15d619939c71d1717ab963032fd4715f066cc2b7
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-08 21:14:56 +0800

    Update p0907_sum_of_subarray_minimums_2.py

--- leetcode/algorithms/p0907_sum_of_subarray_minimums_2.py
+++ leetcode/algorithms/p0907_sum_of_subarray_minimums_2.py
@@ -3,21 +3,31 @@ from typing import List
 
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        arr.append(0)
         n = len(arr)
-        stack = []
         result = 0
+        left = [0] * n
+        right = [0] * n
+        stack = []
         mod = pow(10, 9) + 7
 
         for i in range(n):
-            while stack and arr[stack[-1]] >= arr[i]:
-                index = stack.pop()
-                prev_index = stack[-1] if stack else -1
-                prev_count = index - prev_index - 1
-                next_count = i - index - 1
-                result += arr[index] * (prev_count + 1) * (next_count + 1) % mod
-                result %= mod
+            while stack and arr[stack[-1]] > arr[i]:
+                stack.pop()
 
+            left[i] = stack[-1] if stack else -1
             stack.append(i)
+
+        stack = []
+
+        for i in range(n - 1, -1, -1):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+
+            right[i] = stack[-1] if stack else n
+            stack.append(i)
+
+        for i in range(n):
+            result += (i - left[i]) * (right[i] - i) * arr[i]
+            result %= mod
 
         return result
