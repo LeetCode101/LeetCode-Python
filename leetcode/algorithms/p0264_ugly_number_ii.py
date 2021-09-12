335a968dfd0a192e2c783b3b90863252aa1a6ead
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-12 12:04:48 +0800

    p264

---
+++ leetcode/algorithms/p0264_ugly_number_ii.py
@@ -0,0 +1,21 @@
+import heapq
+
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly_numbers = [1]
+        heapq.heapify(ugly_numbers)
+        ugly = 1
+        visited = {1}
+
+        for _ in range(n):
+            ugly = heapq.heappop(ugly_numbers)
+
+            for i in [2, 3, 5]:
+                next_ugly = ugly * i
+
+                if next_ugly not in visited:
+                    visited.add(next_ugly)
+                    heapq.heappush(ugly_numbers, next_ugly)
+
+        return ugly
