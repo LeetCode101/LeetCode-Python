5f8a8c129c70346e232e2a6f8f963b781f0c64d6
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-12 11:02:38 +0800

    Update p0263_ugly_number.py

--- leetcode/algorithms/p0263_ugly_number.py
+++ leetcode/algorithms/p0263_ugly_number.py
@@ -1,16 +1,10 @@
-import math
-
-
 class Solution:
     def isUgly(self, n: int) -> bool:
-        while n % 2 == 0:
-            n //= 2
+        if n <= 0:
+            return False
 
-        for i in range(3, int(math.sqrt(n)) + 1, 2):
+        for i in [2, 3, 5]:
             while n % i == 0:
-                if i not in [3, 5]:
-                    return False
+                n //= i
 
-                n //= 2
-
-        return n == 1 or n in [3, 5]
+        return n <= 1
