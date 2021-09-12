04bcc8c77789a57da75301b1d39ba4e0bcd6847a
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-12 12:08:52 +0800

    Update p0263_ugly_number_test.py

--- tests/algorithms/p0263_ugly_number_test.py
+++ tests/algorithms/p0263_ugly_number_test.py
@@ -6,6 +6,7 @@ class TestUglyNumber(unittest.TestCase):
     def test_ugly_number(self):
         solution = Solution()
 
+        self.assertFalse(solution.isUgly(0))
         self.assertTrue(solution.isUgly(6))
         self.assertTrue(solution.isUgly(8))
         self.assertFalse(solution.isUgly(14))
