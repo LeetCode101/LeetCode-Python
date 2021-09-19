e69021704fc87be750b43416c6c7a9f1d6665d38
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-19 08:26:07 +0800

    p1901

---
+++ leetcode/algorithms/p1901_find_a_peak_element_ii.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        row, column = len(mat), len(mat[0])
+        i, j = 0, column - 1
+
+        while 0 <= i < row and 0 <= j < column:
+            up = -1 if i - 1 < 0 else mat[i - 1][j]
+            down = -1 if i + 1 >= row else mat[i + 1][j]
+            left = -1 if j - 1 < 0 else mat[i][j - 1]
+            right = -1 if j + 1 >= column else mat[i][j + 1]
+            current = mat[i][j]
+
+            if current > up and current > down and current > left and current > right:
+                return [i, j]
+
+            max_value = max(up, down, left, right)
+
+            if up == max_value:
+                i -= 1
+            elif down == max_value:
+                i += 1
+            elif left == max_value:
+                j -= 1
+            elif right == max_value:
+                j += 1
+
+        return [-1, -1]
---
+++ tests/algorithms/p1901_find_a_peak_element_ii_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p1901_find_a_peak_element_ii import Solution
+
+
+class TestFindAPeakElement(unittest.TestCase):
+    def test_find_a_peak_element(self):
+        solution = Solution()
+
+        self.assertListEqual([1, 0], solution.findPeakGrid([[1, 4], [3, 2]]))
+        self.assertListEqual([1, 1], solution.findPeakGrid(
+            [[10, 20, 15], [21, 30, 14], [7, 16, 32]]))
