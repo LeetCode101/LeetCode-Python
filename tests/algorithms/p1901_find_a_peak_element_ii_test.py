11fa37241a17d5f28d0a8e62f28cc3f1bf91bcb5
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-19 08:32:16 +0800

    Update p1901_find_a_peak_element_ii_test.py

--- tests/algorithms/p1901_find_a_peak_element_ii_test.py
+++ tests/algorithms/p1901_find_a_peak_element_ii_test.py
@@ -6,6 +6,6 @@ class TestFindAPeakElement(unittest.TestCase):
     def test_find_a_peak_element(self):
         solution = Solution()
 
-        self.assertListEqual([1, 0], solution.findPeakGrid([[1, 4], [3, 2]]))
+        self.assertListEqual([0, 1], solution.findPeakGrid([[1, 4], [3, 2]]))
         self.assertListEqual([1, 1], solution.findPeakGrid(
             [[10, 20, 15], [21, 30, 14], [7, 16, 32]]))
