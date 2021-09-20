15d0c323b84e948bdf9c5e713696572482fb3501
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-20 09:32:59 +0800

    p1046

--- leetcode/algorithms/p1049_last_stone_weight_ii.py
+++ leetcode/algorithms/p1049_last_stone_weight_ii.py
@@ -8,7 +8,7 @@ class Solution:
         dp = [[0] * (total // 2 + 1) for _ in range(n + 1)]
 
         for i in range(1, n + 1):
-            for j in range(total // 2 + 1):
+            for j in range(1, total // 2 + 1):
                 if j >= stones[i - 1]:
                     current = dp[i - 1][j - stones[i - 1]] + stones[i - 1]
                     dp[i][j] = max(dp[i - 1][j], current)
