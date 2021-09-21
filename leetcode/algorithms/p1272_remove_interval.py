dae1585ab082a0daa1e97517edb26fd1c22d7d69
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-21 18:13:11 +0800

    p1272

--- leetcode/algorithms/p1272_remove_interval.py
+++ leetcode/algorithms/p1272_remove_interval.py
@@ -12,7 +12,15 @@ class Solution:
             i += 1
 
         if i < n and intervals[i][0] <= toBeRemoved[0] <= intervals[i][1]:
-            result.append([intervals[i][0], toBeRemoved[0]])
+            if intervals[i][0] <= toBeRemoved[0] and toBeRemoved[1] <= intervals[i][1]:
+                if intervals[i][0] != toBeRemoved[0]:
+                    result.append([intervals[i][0], toBeRemoved[0]])
+
+                if intervals[i][1] != toBeRemoved[1]:
+                    result.append([toBeRemoved[1], intervals[i][1]])
+            else:
+                result.append([intervals[i][0], toBeRemoved[0]])
+
             i += 1
 
         while i < n and toBeRemoved[0] <= intervals[i][0] and intervals[i][1] <= toBeRemoved[1]:
--- tests/algorithms/p1272_remove_interval_test.py
+++ tests/algorithms/p1272_remove_interval_test.py
@@ -6,6 +6,8 @@ class TestRemoveInterval(unittest.TestCase):
     def test_remove_interval(self):
         solution = Solution()
 
+        self.assertListEqual([[50, 100]], solution.removeInterval(
+            [[0, 100]], [0, 50]))
         self.assertListEqual([[0, 1], [6, 7]], solution.removeInterval(
             [[0, 2], [3, 4], [5, 7]], [1, 6]))
         self.assertListEqual([[0, 2], [3, 5]], solution.removeInterval(
