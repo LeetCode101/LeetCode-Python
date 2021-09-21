ea3e33ad9117c1106d00bdeb8db9480e8edc54a4
Frederick-S <mao_xiaodan@hotmail.com>
2021-09-21 18:15:18 +0800

    Update p1272_remove_interval.py

--- leetcode/algorithms/p1272_remove_interval.py
+++ leetcode/algorithms/p1272_remove_interval.py
@@ -11,16 +11,17 @@ class Solution:
             result.append(intervals[i])
             i += 1
 
-        if i < n and intervals[i][0] <= toBeRemoved[0] <= intervals[i][1]:
-            if intervals[i][0] <= toBeRemoved[0] \
-                    and toBeRemoved[1] <= intervals[i][1]:
-                if intervals[i][0] != toBeRemoved[0]:
-                    result.append([intervals[i][0], toBeRemoved[0]])
-
-                if intervals[i][1] != toBeRemoved[1]:
-                    result.append([toBeRemoved[1], intervals[i][1]])
-            else:
+        if i < n and intervals[i][0] <= toBeRemoved[0] \
+                and toBeRemoved[1] <= intervals[i][1]:
+            if intervals[i][0] != toBeRemoved[0]:
                 result.append([intervals[i][0], toBeRemoved[0]])
+
+            if intervals[i][1] != toBeRemoved[1]:
+                result.append([toBeRemoved[1], intervals[i][1]])
+
+            i += 1
+        elif i < n and intervals[i][0] <= toBeRemoved[0] <= intervals[i][1]:
+            result.append([intervals[i][0], toBeRemoved[0]])
 
             i += 1
 
