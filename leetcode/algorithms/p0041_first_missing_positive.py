d62931f4a94aed2fa56e8cdc32a7f8601e8a9e22
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-09 11:38:27 +0800

    Update p0041_first_missing_positive.py

--- leetcode/algorithms/p0041_first_missing_positive.py
+++ leetcode/algorithms/p0041_first_missing_positive.py
@@ -7,7 +7,9 @@ class Solution:
 
         for i in range(n):
             while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
-                nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
+                temp = nums[nums[i] - 1]
+                nums[nums[i] - 1] = nums[i]
+                nums[i] = temp
 
         for i in range(n):
             if nums[i] != i + 1:
