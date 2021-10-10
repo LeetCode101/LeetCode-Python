c2823cd6fff2bb0d58827691acdd71e85d7a8da0
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-10 19:00:41 +0800

    Update p1066_campus_bikes_ii.py

--- leetcode/algorithms/p1066_campus_bikes_ii.py
+++ leetcode/algorithms/p1066_campus_bikes_ii.py
@@ -1,33 +1,36 @@
+import collections
 import sys
-from functools import lru_cache
 from typing import List
 
 
 class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) \
             -> int:
-        @lru_cache(None)
-        def dfs(seen_workers, bike_index):
-            if all(seen_workers):
-                return 0
+        memo = collections.defaultdict(int)
 
-            min_distance = sys.maxsize
-            seen_workers = list(seen_workers)
+        return self._dfs(workers, bikes, 0, 0, memo)
 
-            for i in range(len(workers)):
-                for j in range(bike_index, len(bikes)):
-                    if seen_workers[i]:
-                        continue
+    def _dfs(self, workers, bikes, worker_index, seen_bikes, memo):
+        if worker_index == len(workers):
+            return 0
 
-                    seen_workers[i] = True
-                    min_distance = min(min_distance, self._get_distance(workers[i], bikes[j]) + dfs(tuple(seen_workers), j + 1))
-                    seen_workers[i] = False
+        if seen_bikes in memo:
+            return memo[seen_bikes]
 
-            return min_distance
+        min_distance_sum = sys.maxsize
 
-        seen_workers = [False] * len(workers)
+        for i in range(len(bikes)):
+            if (seen_bikes >> i) & 1 == 1:
+                continue
 
-        return dfs(tuple(seen_workers), 0)
+            distance = self._get_distance(workers[worker_index], bikes[i])
+            current_distance_sum = distance + self._dfs(
+                workers, bikes, worker_index + 1, seen_bikes | (1 << i), memo)
+            min_distance_sum = min(min_distance_sum, current_distance_sum)
+
+        memo[seen_bikes] = min_distance_sum
+
+        return min_distance_sum
 
     def _get_distance(self, p1, p2):
         return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
