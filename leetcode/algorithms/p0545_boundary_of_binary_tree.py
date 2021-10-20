d75c44d510e13ee6c07699a6b844ecbcedf4dcff
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-20 09:35:48 +0800

    p545

---
+++ leetcode/algorithms/p0545_boundary_of_binary_tree.py
@@ -0,0 +1,73 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        left_boundary = self._get_left_boundary(root.left)
+        leaves = self._get_leaves(root)
+        right_boundary = self._get_right_boundary(root.right)
+
+        return [root.val] + left_boundary + leaves + right_boundary
+
+    def _is_leaf(self, node):
+        return not node.left and not node.right
+
+    def _get_left_boundary(self, node):
+        if not node:
+            return []
+
+        order = []
+
+        while not self._is_leaf(node):
+            order.append(node.val)
+
+            if node.left:
+                node = node.left
+            else:
+                node = node.right
+
+        return order
+
+    def _get_right_boundary(self, node):
+        if not node:
+            return []
+
+        order = []
+
+        while not self._is_leaf(node):
+            order.append(node.val)
+
+            if node.right:
+                node = node.right
+            else:
+                node = node.left
+
+        return order[::-1]
+
+    def _get_leaves(self, root):
+        if self._is_leaf(root):
+            return []
+
+        stack = [root]
+        order = []
+
+        while stack:
+            node = stack.pop()
+
+            if self._is_leaf(node):
+                order.append(node.val)
+            else:
+                if node.right:
+                    stack.append(node.right)
+
+                if node.left:
+                    stack.append(node.left)
+
+        return order
---
+++ tests/algorithms/p0545_boundary_of_binary_tree_test.py
@@ -0,0 +1,23 @@
+import unittest
+from leetcode.algorithms.p0545_boundary_of_binary_tree \
+    import Solution, TreeNode
+
+
+class TestBoundaryOfBinaryTree(unittest.TestCase):
+    def test_boundary_of_binary_tree(self):
+        solution = Solution()
+        root = TreeNode(
+            1,
+            TreeNode(
+                2,
+                TreeNode(4),
+                TreeNode(5, TreeNode(7), TreeNode(8))
+            ),
+            TreeNode(
+                3,
+                TreeNode(6, TreeNode(9), TreeNode(10))
+            )
+        )
+
+        self.assertListEqual([1, 2, 4, 7, 8, 9, 10, 6, 3],
+                             solution.boundaryOfBinaryTree(root))
