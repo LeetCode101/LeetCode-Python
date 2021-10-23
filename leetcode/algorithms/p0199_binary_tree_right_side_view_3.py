f2cf890c612a3101b35e8253cbb064d5fe2606a4
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-23 14:50:18 +0800

    p199

---
+++ leetcode/algorithms/p0199_binary_tree_right_side_view_3.py
@@ -0,0 +1,31 @@
+import collections
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        order = []
+
+        self._dfs(root, 1, order)
+
+        return order
+
+    def _dfs(self, root, level, order):
+        if not root:
+            return
+
+        if len(order) < level:
+            order.append(root.val)
+
+        if root.right:
+            self._dfs(root.right, level + 1, order)
+
+        if root.left:
+            self._dfs(root.left, level + 1, order)
---
+++ tests/algorithms/p0199_binary_tree_right_side_view_3_test.py
@@ -0,0 +1,16 @@
+import unittest
+from leetcode.algorithms.p0199_binary_tree_right_side_view_3 \
+    import Solution, TreeNode
+
+
+class TestBinaryTreeRightSideView(unittest.TestCase):
+    def test_binary_tree_right_side_view(self):
+        solution = Solution()
+        root1 = TreeNode(
+            1,
+            TreeNode(2, None, TreeNode(5)),
+            TreeNode(3, None, TreeNode(4))
+        )
+
+        self.assertListEqual([], solution.rightSideView(None))
+        self.assertListEqual([1, 3, 4], solution.rightSideView(root1))
