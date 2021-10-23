7baaf834acd504c45d822866d525bb87bb7041ae
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-23 15:32:59 +0800

    p1469

---
+++ leetcode/algorithms/p1469_find_all_the_lonely_nodes.py
@@ -0,0 +1,32 @@
+import collections
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        nodes = []
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+
+            if (node.left and not node.right) or (node.right and not node.left):
+                nodes.append(node.left.val if node.left else node.right.val)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        return nodes
