b3038e9a320faa0ccfb685f8982e38e5e6c93391
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-23 16:37:18 +0800

    Update p0582_kill_process_test.py

--- tests/algorithms/p0582_kill_process_test.py
+++ tests/algorithms/p0582_kill_process_test.py
@@ -6,5 +6,8 @@ class TestKillProcess(unittest.TestCase):
     def test_kill_process(self):
         solution = Solution()
 
+
+        self.assertListEqual([1, 2, 3], solution.killProcess(
+            [1, 2, 3], [0, 1, 1], 1))
         self.assertListEqual([5, 10], solution.killProcess(
             [1, 3, 10, 5], [3, 0, 5, 3], 5))
