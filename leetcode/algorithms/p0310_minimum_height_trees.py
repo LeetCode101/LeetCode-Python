93b5bb104b2063631e0f5ec3d1bbec02ce3a0503
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-24 13:34:19 +0800

    p310

---
+++ leetcode/algorithms/p0310_minimum_height_trees.py
@@ -0,0 +1,39 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        parent_to_children = collections.defaultdict(list)
+
+        for edge in edges:
+            parent_to_children[edge[0]].append(edge[1])
+            parent_to_children[edge[1]].append(edge[0])
+
+        min_height = n
+        result = []
+
+        for i in range(n):
+            visited = set()
+            height = 0
+            queue = collections.deque([i])
+
+            while queue:
+                height += 1
+                size = len(queue)
+
+                for _ in range(size):
+                    node = queue.popleft()
+                    visited.add(node)
+
+                    for child in parent_to_children[node]:
+                        if child not in visited:
+                            queue.append(child)
+
+            if height == min_height:
+                result.append(i)
+            elif height < min_height:
+                result = [i]
+                min_height = height
+
+        return result
---
+++ tests/algorithms/p0310_minimum_height_trees_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms.p0310_minimum_height_trees import Solution
+
+
+class TestMinimumHeightTrees(unittest.TestCase):
+    def test_minimum_height_trees(self):
+        solution = Solution()
+
+        self.assertListEqual([1], solution.findMinHeightTrees(
+            4, [[1, 0], [1, 2], [1, 3]]))
+        self.assertListEqual([3, 4], solution.findMinHeightTrees(
+            6, [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]))
