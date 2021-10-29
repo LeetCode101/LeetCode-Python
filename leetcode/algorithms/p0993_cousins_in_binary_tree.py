818deeb0330c2391f8a93d363e1fbc7a18255fe4
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-29 08:51:09 +0800

    p993

---
+++ leetcode/algorithms/p0993_cousins_in_binary_tree.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        stack = [(root, 0, None)]
+        count = 2
+        prev_parent = None
+        prev_depth = -1
+
+        while stack:
+            node, depth, parent = stack.pop()
+
+            if node.val == x or node.val == y:
+                if prev_parent:
+                    return prev_parent != parent and depth == prev_depth
+                else:
+                    prev_parent = parent
+                    prev_depth = depth
+
+            if node.left:
+                stack.append((node.left, depth + 1, node))
+
+            if node.right:
+                stack.append((node.right, depth + 1, node))
---
+++ tests/algorithms/p0993_cousins_in_binary_tree_test.py
@@ -0,0 +1,20 @@
+import unittest
+from leetcode.algorithms.p0993_cousins_in_binary_tree import Solution, TreeNode
+
+
+class TestCousinsInBinaryTree(unittest.TestCase):
+    def test_cousins_in_binary_tree(self):
+        solution = Solution()
+        root1 = TreeNode(
+            1,
+            TreeNode(2, None, TreeNode(4)),
+            TreeNode(3, None, TreeNode(5))
+        )
+        root2 = TreeNode(
+            1,
+            TreeNode(2, TreeNode(4)),
+            TreeNode(3)
+        )
+
+        self.assertTrue(solution.isCousins(root1, 5, 4))
+        self.assertFalse(solution.isCousins(root2, 4, 3))
