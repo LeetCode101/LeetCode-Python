6d20ed5b06c0aaf018e7ca579bc52efff4f995a4
Frederick-S <mao_xiaodan@hotmail.com>
2021-10-31 15:28:22 +0800

    p865

---
+++ leetcode/algorithms/p0865_smallest_subtree_with_all_the_deepest_nodes.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        stack = [(root, 1, [])]
+        max_depth = 0
+        candidates = []
+
+        while stack:
+            node, depth, paths = stack.pop()
+
+            if not node.left and not node.right:
+                if depth > max_depth:
+                    max_depth = depth
+                    candidates = [paths + [node]]
+                elif depth == max_depth:
+                    candidates.append(paths + [node])
+
+            if node.left:
+                stack.append((node.left, depth + 1, paths + [node]))
+
+            if node.right:
+                stack.append((node.right, depth + 1, paths + [node]))
+
+        for i in range(len(candidates[0])):
+            for j in range(1, len(candidates)):
+                if candidates[j][i] != candidates[j - 1][i]:
+                    return candidates[0][i - 1]
---
+++ tests/algorithms/p0865_smallest_subtree_with_all_the_deepest_nodes_test.py
@@ -0,0 +1,16 @@
+import unittest
+from leetcode.algorithms.p0865_smallest_subtree_with_all_the_deepest_nodes \
+    import Solution, TreeNode
+
+
+class TestSmallestSubtreeWithAllTheDeepestNodes(unittest.TestCase):
+    def test_smallest_subtree_with_all_the_deepest_nodes(self):
+        solution = Solution()
+        a = TreeNode(2, TreeNode(7), TreeNode(4))
+        root1 = TreeNode(
+            3,
+            TreeNode(5, TreeNode(6), a),
+            TreeNode(1, TreeNode(0), TreeNode(8))
+        )
+
+        self.assertEqual(a, solution.subtreeWithAllDeepest(root1))
