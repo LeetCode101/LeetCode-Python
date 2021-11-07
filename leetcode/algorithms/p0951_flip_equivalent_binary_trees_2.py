8d896264da476f3eca62d31229a722e00e9ac4f3
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-07 11:10:37 +0800

    p951

---
+++ leetcode/algorithms/p0951_flip_equivalent_binary_trees_2.py
@@ -0,0 +1,57 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) \
+            -> bool:
+        if not root1 and not root2:
+            return True
+
+        stack1 = [root1]
+        stack2 = [root2]
+
+        while stack1 and stack2:
+            node1 = stack1.pop()
+            node2 = stack2.pop()
+
+            if not self._equal(node1, node2):
+                return False
+
+            left1 = node1.left
+            right1 = node1.right
+            left2 = node2.left
+            right2 = node2.right
+
+            if left1:
+                stack1.append(left1)
+
+            if right1:
+                stack1.append(right1)
+
+            if self._equal(left1, left2):
+                if left2:
+                    stack2.append(left2)
+
+                if right2:
+                    stack2.append(right2)
+            else:
+                if right2:
+                    stack2.append(right2)
+
+                if left2:
+                    stack2.append(left2)
+
+        return not stack1 and not stack2
+
+    def _equal(self, root1, root2):
+        if root1 and root2:
+            return root1.val == root2.val
+        else:
+            return not root1 and not root2
---
+++ tests/algorithms/p0951_flip_equivalent_binary_trees_2_test.py
@@ -0,0 +1,56 @@
+import unittest
+from leetcode.algorithms.p0951_flip_equivalent_binary_trees_2 \
+    import Solution, TreeNode
+
+
+class TestFlipEquivalentBinaryTrees(unittest.TestCase):
+    def test_flip_equivalent_binary_trees(self):
+        solution = Solution()
+        root1 = TreeNode(
+            1,
+            TreeNode(
+                2,
+                TreeNode(4),
+                TreeNode(5, TreeNode(7), TreeNode(8))
+            ),
+            TreeNode(3, TreeNode(6))
+        )
+        root2 = TreeNode(
+            1,
+            TreeNode(3, None, TreeNode(6)),
+            TreeNode(
+                2,
+                TreeNode(4),
+                TreeNode(5, TreeNode(8), TreeNode(7))
+            )
+        )
+        root3 = TreeNode(
+            0,
+            TreeNode(4),
+            TreeNode(
+                1,
+                None,
+                TreeNode(
+                    2,
+                    TreeNode(3, TreeNode(5, None, TreeNode(8)), TreeNode(7)),
+                    TreeNode(6)
+                )
+            )
+        )
+        root4 = TreeNode(
+            0,
+            TreeNode(
+                1,
+                None,
+                TreeNode(
+                    2,
+                    TreeNode(6, TreeNode(8)),
+                    TreeNode(3, TreeNode(5), TreeNode(7))
+                )
+            ),
+            TreeNode(4)
+        )
+
+        self.assertTrue(solution.flipEquiv(None, None))
+        self.assertTrue(solution.flipEquiv(root1, root2))
+        self.assertFalse(solution.flipEquiv(root3, root4))
