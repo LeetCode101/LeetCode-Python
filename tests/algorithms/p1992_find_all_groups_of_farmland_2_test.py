1aeac909ecc46eec03ddd8b6de56342c66e67322
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-13 16:33:08 +0800

    p1992

---
+++ tests/algorithms/p1992_find_all_groups_of_farmland_2_test.py
@@ -0,0 +1,13 @@
+import unittest
+from leetcode.algorithms.p1992_find_all_groups_of_farmland_2 import Solution
+
+
+class TestFindAllGroupsOfFarmland(unittest.TestCase):
+    def test_find_all_groups_of_farmland(self):
+        solution = Solution()
+
+        self.assertListEqual([[0, 0, 0, 0], [1, 1, 2, 2]],
+                             solution.findFarmland(
+                                 [[1, 0, 0], [0, 1, 1], [0, 1, 1]]))
+        self.assertListEqual([[0, 0, 1, 1]],
+                             solution.findFarmland([[1, 1], [1, 1]]))
