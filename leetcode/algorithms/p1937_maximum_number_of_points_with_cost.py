481921e3424c29c0b1f09b0eb29b19c219511390
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-19 09:48:35 +0800

    p1937

---
+++ leetcode/algorithms/p1937_maximum_number_of_points_with_cost.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+# Time Limit Exceeded!
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        m, n = len(points), len(points[0])
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range(n):
+            dp[0][i] = points[0][i]
+
+        for i in range(1, m):
+            for j in range(n):
+                dp[i][j] = max([dp[i - 1][k] + points[i][j] - abs(j - k)
+                                for k in range(n)])
+
+        return max(dp[m - 1])
