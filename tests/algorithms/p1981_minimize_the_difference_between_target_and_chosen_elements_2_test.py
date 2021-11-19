a0b2a8b88c6a69da679efa6093584059cf2f27c0
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-19 09:10:33 +0800

    Update p1981_minimize_the_difference_between_target_and_chosen_elements_2_test.py

--- tests/algorithms/p1981_minimize_the_difference_between_target_and_chosen_elements_2_test.py
+++ tests/algorithms/p1981_minimize_the_difference_between_target_and_chosen_elements_2_test.py
@@ -9,6 +9,12 @@ class TestMinimizeTheDifferenceBetweenTargetAndChosenElements(
     def test_minimize_the_difference_between_target_and_chosen_elements(self):
         solution = Solution()
 
+        self.assertEqual(3, solution.minimizeTheDifference(
+            [[10, 3, 7, 7, 9, 6, 9, 8, 9, 5], [1, 1, 6, 8, 6, 7, 7, 9, 3, 9],
+             [3, 4, 4, 1, 3, 6, 3, 3, 9, 9],
+             [6, 9, 9, 3, 8, 7, 9, 6, 10, 6]], 5))
+        self.assertEqual(0, solution.minimizeTheDifference(
+            [[1, 1, 3], [4, 5, 6], [7, 8, 9]], 13))
         self.assertEqual(0, solution.minimizeTheDifference(
             [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 13))
         self.assertEqual(94, solution.minimizeTheDifference(
