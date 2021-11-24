3a7c8809d780476373be3486cffeb6ee54160fee
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-24 09:39:00 +0800

    rename file

--- tests/algorithms/p0376_wiggle_subsequence_1_test.py
+++ tests/algorithms/p0376_wiggle_subsequence_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0376_wiggle_subsequence import Solution
+from leetcode.algorithms.p0376_wiggle_subsequence_1 import Solution
 
 
 class TestWiggleSubsequence(unittest.TestCase):
