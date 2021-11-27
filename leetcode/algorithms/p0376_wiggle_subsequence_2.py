e49b2efe1e9a0ffd16c82ccc22ca3debf030ce74
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-27 15:49:56 +0800

    p376

---
+++ leetcode/algorithms/p0376_wiggle_subsequence_2.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        postive = [0] * n
+        negative = [0] * n
+        postive[0] = 1
+        negative[0] = 1
+
+        for i in range(1, n):
+            postive[i] = postive[i - 1]
+            negative[i] = negative[i - 1]
+            diff = nums[i] - nums[i - 1]
+
+            if diff < 0:
+                negative[i] = max(negative[i], 1 + postive[i])
+            elif diff > 0:
+                postive[i] = max(postive[i], 1 + negative[i])
+
+        return max(max(postive), max(negative))
---
+++ tests/algorithms/p0376_wiggle_subsequence_2_test.py
@@ -0,0 +1,13 @@
+import unittest
+from leetcode.algorithms.p0376_wiggle_subsequence_2 import Solution
+
+
+class TestWiggleSubsequence(unittest.TestCase):
+    def test_wiggle_subsequence(self):
+        solution = Solution()
+
+        self.assertEqual(6, solution.wiggleMaxLength([1, 7, 4, 9, 2, 5]))
+        self.assertEqual(7, solution.wiggleMaxLength(
+            [1, 17, 5, 10, 13, 15, 10, 5, 16, 8]))
+        self.assertEqual(2, solution.wiggleMaxLength(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9]))
