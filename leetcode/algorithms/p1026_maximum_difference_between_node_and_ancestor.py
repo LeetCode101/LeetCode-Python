c6498de9669a4041d207421470c9dc3837d968dc
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-27 16:44:31 +0800

    Update p1026_maximum_difference_between_node_and_ancestor.py

--- leetcode/algorithms/p1026_maximum_difference_between_node_and_ancestor.py
+++ leetcode/algorithms/p1026_maximum_difference_between_node_and_ancestor.py
@@ -22,8 +22,9 @@ class Solution:
         min_value = min(root.val, min_value)
         max_value = max(root.val, max_value)
 
-        max_left = self._find_max(root.left, min_value, max_value) if root.left else abs(min_value - max_value)
-        max_right = self._find_max(root.right, min_value, max_value) if root.right else abs(min_value - max_value)
+        max_left = self._find_max(root.left, min_value, max_value) \
+            if root.left else abs(min_value - max_value)
+        max_right = self._find_max(root.right, min_value, max_value) \
+            if root.right else abs(min_value - max_value)
 
         return max(max_left, max_right)
-
