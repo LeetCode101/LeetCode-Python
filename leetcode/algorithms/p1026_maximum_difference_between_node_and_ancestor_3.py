2ab7b7e71bfc9b18aba82cf3def86af3b04ddd19
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-27 16:59:08 +0800

    p1026

---
+++ leetcode/algorithms/p1026_maximum_difference_between_node_and_ancestor_3.py
@@ -0,0 +1,29 @@
+import collections
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        queue = collections.deque([(root, root.val, root.val)])
+        max_diff = 0
+
+        while queue:
+            node, min_value, max_value = queue.popleft()
+            max_diff = max(max_diff, abs(min_value - max_value))
+
+            if node.left:
+                queue.append((node.left, min(min_value, node.left.val),
+                              max(max_value, node.left.val)))
+
+            if node.right:
+                queue.append((node.right, min(min_value, node.right.val),
+                              max(max_value, node.right.val)))
+
+        return max_diff
---
+++ tests/algorithms/p1026_maximum_difference_between_node_and_ancestor_3_test.py
@@ -0,0 +1,19 @@
+import unittest
+from leetcode.algorithms.p1026_maximum_difference_between_node_and_ancestor_3 \
+    import Solution, TreeNode
+
+
+class TestMaximumDifferenceBetweenNodeAndAncestor(unittest.TestCase):
+    def test_maximum_difference_between_node_and_ancestor(self):
+        solution = Solution()
+        root = TreeNode(
+            8,
+            TreeNode(
+                3,
+                TreeNode(1),
+                TreeNode(6, TreeNode(4), TreeNode(7))
+            ),
+            TreeNode(10, None, TreeNode(14, TreeNode(13)))
+        )
+
+        self.assertEqual(7, solution.maxAncestorDiff(root))
