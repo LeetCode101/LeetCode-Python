a047335d33cfe3b7d46b2b9b738328d283183f0a
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-28 13:30:17 +0800

    Update p0333_largest_bst_subtree_2.py

--- leetcode/algorithms/p0333_largest_bst_subtree_2.py
+++ leetcode/algorithms/p0333_largest_bst_subtree_2.py
@@ -23,6 +23,7 @@ class Solution:
         right_min, right_max, right_size = self._dfs(root.right)
 
         if left_max < root.val < right_min:
-            return min(left_min, root.val), max(right_max, root.val), left_size + right_size + 1
+            return min(left_min, root.val), max(right_max, root.val), \
+                   left_size + right_size + 1
         else:
             return -sys.maxsize, sys.maxsize, max(left_size, right_size)
