0198bfe27a58620550c12d74a454af72dc2a2f52
Frederick-S <mao_xiaodan@hotmail.com>
2021-11-28 13:28:07 +0800

    Update p0333_largest_bst_subtree_2.py

--- leetcode/algorithms/p0333_largest_bst_subtree_2.py
+++ leetcode/algorithms/p0333_largest_bst_subtree_2.py
@@ -11,27 +11,18 @@ class TreeNode:
 
 class Solution:
     def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
-        _, _, size, _ = self._dfs(root)
+        _, _, size = self._dfs(root)
 
         return size
 
     def _dfs(self, root):
         if not root:
-            return -sys.maxsize, sys.maxsize, 0, True
+            return sys.maxsize, -sys.maxsize, 0
 
-        left_min, left_max, left_size, left_valid = self._dfs(root.left)
-        right_min, right_max, right_size, right_valid = self._dfs(root.right)
+        left_min, left_max, left_size = self._dfs(root.left)
+        right_min, right_max, right_size = self._dfs(root.right)
 
-        if left_valid and right_valid and (left_min < root.val < right_max):
-            if not root.left:
-                left_max = root.val
-
-            if not root.right:
-                right_min = root.val
-
-            return right_min, left_max, left_size + right_size + 1, True
+        if left_max >= root.val or right_min <= root.val:
+            return -sys.maxsize, sys.maxsize, max(left_size, right_size)
         else:
-            if left_size > right_size:
-                return left_min, left_max, left_size, False
-            else:
-                return right_min, right_max, right_size, False
+            return min(left_min, root.val), max(right_max, root.val), left_size + right_size + 1
