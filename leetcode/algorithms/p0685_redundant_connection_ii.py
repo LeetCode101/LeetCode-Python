bebc8720161a08187f1c510945a4f3d304b66976
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-01 09:21:43 +0800

    p685

---
+++ leetcode/algorithms/p0685_redundant_connection_ii.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class Solution:
+    def findRedundantDirectedConnection(self, edges: List[List[int]]) \
+            -> List[int]:
+        candidate1, candidate2, parents = None, None, {}
+
+        for a, b in edges:
+            if b in parents:
+                candidate1, candidate2 = [parents[b], b], [a, b]
+
+                break
+
+            parents[b] = a
+
+        roots = list(range(len(edges) + 1))
+
+        for a, b in edges:
+            if [a, b] == candidate2:
+                continue
+
+            root_a, root_b = self._get_root(roots, a), self._get_root(roots, b)
+
+            if root_a == root_b:
+                if not candidate1:
+                    return [a, b]
+                else:
+                    return candidate1
+
+            roots[root_a] = roots[root_b]
+
+        return candidate2
+
+    def _get_root(self, roots, x):
+        while roots[x] != x:
+            roots[x] = roots[roots[x]]
+            x = roots[x]
+
+        return roots[x]
---
+++ tests/algorithms/p0685_redundant_connection_ii_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms.p0685_redundant_connection_ii import Solution
+
+
+class TestRedundantConnection(unittest.TestCase):
+    def test_redundant_connection(self):
+        solution = Solution()
+
+        self.assertListEqual([2, 3], solution.findRedundantDirectedConnection(
+            [[1, 2], [1, 3], [2, 3]]))
+        self.assertListEqual([4, 1], solution.findRedundantDirectedConnection(
+            [[1, 2], [2, 3], [3, 4], [4, 1], [1, 5]]))
