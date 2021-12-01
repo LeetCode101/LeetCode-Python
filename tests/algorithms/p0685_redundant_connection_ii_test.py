ad5dfa5285964b88b111a43f83b861425342f8dc
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-01 09:43:36 +0800

    Update p0685_redundant_connection_ii_test.py

--- tests/algorithms/p0685_redundant_connection_ii_test.py
+++ tests/algorithms/p0685_redundant_connection_ii_test.py
@@ -6,6 +6,8 @@ class TestRedundantConnection(unittest.TestCase):
     def test_redundant_connection(self):
         solution = Solution()
 
+        self.assertListEqual([2, 1], solution.findRedundantDirectedConnection(
+            [[2, 1], [3, 1], [4, 2], [1, 4]]))
         self.assertListEqual([2, 3], solution.findRedundantDirectedConnection(
             [[1, 2], [1, 3], [2, 3]]))
         self.assertListEqual([4, 1], solution.findRedundantDirectedConnection(
