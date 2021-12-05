c21781f28553cb84796062cb656c235f99f63853
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-05 12:00:27 +0800

    Update p1202_smallest_string_with_swaps_test.py

--- tests/algorithms/p1202_smallest_string_with_swaps_test.py
+++ tests/algorithms/p1202_smallest_string_with_swaps_test.py
@@ -6,6 +6,8 @@ class TestSmallestStringWithSwaps(unittest.TestCase):
     def test_smallest_string_with_swaps(self):
         solution = Solution()
 
+        self.assertEqual('dqwyt', solution.smallestStringWithSwaps(
+            'qdwyt', [[2, 3], [3, 2], [0, 1], [4, 0], [3, 2]]))
         self.assertEqual('bacd', solution.smallestStringWithSwaps(
             'dcab', [[0, 3], [1, 2]]))
         self.assertEqual('abcd', solution.smallestStringWithSwaps(
