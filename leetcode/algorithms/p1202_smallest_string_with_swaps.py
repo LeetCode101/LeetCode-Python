5e238690c5faf217ebe3af0e1b9b852bd96f350e
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-05 11:47:09 +0800

    p1202

---
+++ leetcode/algorithms/p1202_smallest_string_with_swaps.py
@@ -0,0 +1,40 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        n = len(s)
+        roots = list(range(n))
+        mapping = collections.defaultdict(list)
+
+        for a, b in pairs:
+            self._union(roots, a, b)
+
+        for i in range(n):
+            root = self._get_root(roots, i)
+            mapping[root].append(s[i])
+
+        for key in mapping.keys():
+            mapping[key].sort(reverse=True)
+
+        result = []
+
+        for i in range(n):
+            root = self._get_root(roots, i)
+            result.append(mapping[root].pop())
+
+        return ''.join(result)
+
+    def _get_root(self, roots, x):
+        while roots[x] != x:
+            roots[x] = roots[roots[x]]
+            x = roots[x]
+
+        return x
+
+    def _union(self, roots, a, b):
+        root_a = self._get_root(roots, a)
+        root_b = self._get_root(roots, b)
+
+        roots[root_a] = root_b
---
+++ tests/algorithms/p1202_smallest_string_with_swaps_test.py
@@ -0,0 +1,14 @@
+import unittest
+from leetcode.algorithms.p1202_smallest_string_with_swaps import Solution
+
+
+class TestSmallestStringWithSwaps(unittest.TestCase):
+    def test_smallest_string_with_swaps(self):
+        solution = Solution()
+
+        self.assertEqual('bacd', solution.smallestStringWithSwaps(
+            'dcab', [[0, 3], [1, 2]]))
+        self.assertEqual('abcd', solution.smallestStringWithSwaps(
+            'dcab', [[0, 3], [1, 2], [0, 2]]))
+        self.assertEqual('abc', solution.smallestStringWithSwaps(
+            'cba', [[0, 1], [1, 2]]))
