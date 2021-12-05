67e0ffce026a0818efec8e1217ae3072bdb9f826
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-05 10:38:24 +0800

    Update p0938_range_sum_of_bst.py

--- leetcode/algorithms/p0938_range_sum_of_bst.py
+++ leetcode/algorithms/p0938_range_sum_of_bst.py
@@ -9,7 +9,8 @@ class TreeNode:
 
 
 class Solution:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) \
+            -> int:
         if not root:
             return 0
 
