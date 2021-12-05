a94049ca4337c2d8394d16be4f45fbb11061b9b6
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-05 11:11:02 +0800

    Update p0938_range_sum_of_bst_2_test.py

--- tests/algorithms/p0938_range_sum_of_bst_2_test.py
+++ tests/algorithms/p0938_range_sum_of_bst_2_test.py
@@ -5,10 +5,24 @@ from leetcode.algorithms.p0938_range_sum_of_bst_2 import Solution, TreeNode
 class TestRangeSumOfBST(unittest.TestCase):
     def test_range_sum_of_bst(self):
         solution = Solution()
-        root = TreeNode(
+        root1 = TreeNode(
             10,
             TreeNode(5, TreeNode(3), TreeNode(7)),
             TreeNode(15, None, TreeNode(18))
         )
+        root2 = TreeNode(
+            18,
+            TreeNode(
+                9,
+                TreeNode(6, TreeNode(3)),
+                TreeNode(15, TreeNode(12))
+            ),
+            TreeNode(
+                27,
+                TreeNode(24, TreeNode(21)),
+                TreeNode(30)
+            )
+        )
 
-        self.assertEqual(32, solution.rangeSumBST(root, 7, 15))
+        self.assertEqual(32, solution.rangeSumBST(root1, 7, 15))
+        self.assertEqual(63, solution.rangeSumBST(root2, 18, 24))
