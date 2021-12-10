d27b35bfcb13800344fda29782d9de7ea13e5935
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-10 09:33:30 +0800

    rename file

--- tests/algorithms/p0721_accounts_merge_1_test.py
+++ tests/algorithms/p0721_accounts_merge_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0721_accounts_merge import Solution
+from leetcode.algorithms.p0721_accounts_merge_1 import Solution
 
 
 class TestAccountsMerge(unittest.TestCase):
