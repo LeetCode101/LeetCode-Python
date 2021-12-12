e737e7d208e28ff8bb02740f3e9308a9ef3c08b4
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-12 13:40:25 +0800

    p1457

---
+++ leetcode/algorithms/p1457_pseudo_palindromic_paths_in_a_binary_tree.py
@@ -0,0 +1,51 @@
+import collections
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        stack = [(root, [])]
+        count = 0
+
+        while stack:
+            node, prev_values = stack.pop()
+            values = prev_values + [node.val]
+
+            if self._is_leaf(node) and self._is_palindromic(values):
+                count += 1
+
+            if node.left:
+                stack.append((node.left, values))
+
+            if node.right:
+                stack.append((node.right, values))
+
+        return count
+
+    def _is_leaf(self, node):
+        return not node.left and not node.right
+
+    def _is_palindromic(self, values):
+        has_odd = False
+        counter = collections.Counter(values)
+
+        for count in counter.values():
+            if count == 1:
+                if has_odd:
+                    return False
+
+                has_odd = True
+            elif count != 2:
+                return False
+
+        return True
