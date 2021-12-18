a9c9af0028d86890ca459aa7ba18dd1d4315ecaf
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-18 09:47:48 +0800

    [323

---
+++ leetcode/algorithms/p0323_number_of_connected_components_in_an_undirected_graph_5.py
@@ -0,0 +1,35 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        neighbours = collections.defaultdict(list)
+
+        for a, b in edges:
+            neighbours[a].append(b)
+            neighbours[b].append(a)
+
+        count = 0
+        visited = set()
+
+        for i in range(n):
+            if i not in visited:
+                self._dfs(neighbours, visited, i)
+                count += 1
+
+        return count
+
+    def _dfs(self, neighbours, visited, i):
+        if i not in neighbours:
+            return
+
+        queue = collections.deque([i])
+
+        while queue:
+            for neighbour in neighbours.pop(queue.popleft(), []):
+                if neighbour in visited:
+                    continue
+
+                visited.add(neighbour)
+                queue.append(neighbour)
