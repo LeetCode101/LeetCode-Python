4c3707991088b131fd9ff2373c6cfa51aac76f32
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-01 09:49:52 +0800

    Update p0291_word_pattern_ii_test.py

--- tests/algorithms/p0291_word_pattern_ii_test.py
+++ tests/algorithms/p0291_word_pattern_ii_test.py
@@ -6,6 +6,7 @@ class TestWordPattern(unittest.TestCase):
     def test_word_pattern(self):
         solution = Solution()
 
+        self.assertFalse(solution.wordPatternMatch('ab', 'aa'))
         self.assertTrue(solution.wordPatternMatch('abab', 'redblueredblue'))
         self.assertTrue(solution.wordPatternMatch('aaaa', 'asdasdasdasd'))
         self.assertFalse(solution.wordPatternMatch('aabb', 'xyzabcxzyabc'))
