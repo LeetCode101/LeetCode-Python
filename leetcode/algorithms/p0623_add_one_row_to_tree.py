a54453d4e1aab148430315a38744e4b21ddf5fda
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-02 14:42:47 +0800

    p623

---
+++ leetcode/algorithms/p0623_add_one_row_to_tree.py
@@ -0,0 +1,42 @@
+import collections
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) \
+            -> Optional[TreeNode]:
+        if depth == 1:
+            return TreeNode(val, root)
+
+        queue = collections.deque([(root, None)])
+        current_depth = 0
+
+        while queue:
+            current_depth += 1
+            size = len(queue)
+
+            if current_depth + 1 == depth:
+                for _ in range(size):
+                    node, parent = queue.popleft()
+                    node.left = TreeNode(val, node.left)
+                    node.right = TreeNode(val, None, node.right)
+
+                return root
+
+            for _ in range(size):
+                node, parent = queue.popleft()
+
+                if node.left:
+                    queue.append((node.left, node))
+
+                if node.right:
+                    queue.append((node.right, node))
+
+        return root
