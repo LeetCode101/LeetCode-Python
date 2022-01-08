3198cf8abc637e1990aaa5c99d5fe1e5c9664724
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-08 15:36:06 +0800

    p126

---
+++ leetcode/algorithms/p0126_word_ladder_ii.py
@@ -0,0 +1,36 @@
+import collections
+import string
+from typing import List
+
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) \
+            -> List[List[str]]:
+        words = set(wordList)
+        visited = set()
+        queue = collections.deque([(beginWord, 1, [beginWord])])
+        letters = string.ascii_lowercase
+        result = []
+        min_length = len(wordList)
+
+        while queue:
+            word, length, words_so_far = queue.popleft()
+
+            if word == endWord:
+                if length < min_length:
+                    min_length = length
+                    result = [words_so_far]
+                elif length == min_length:
+                    result.append(words_so_far)
+
+                continue
+
+            for i in range(len(word)):
+                for c in letters:
+                    new_word = word[:i] + c + word[i + 1:]
+
+                    if new_word in words and new_word not in visited:
+                        queue.append((new_word, length + 1, words_so_far + [new_word]))
+                        visited.add(new_word)
+
+        return result
