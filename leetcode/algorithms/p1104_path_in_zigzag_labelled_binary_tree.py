e8591d7f294e33fb5d091b93fda9f02815889e39
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-08 18:53:05 +0800

    p1104

---
+++ leetcode/algorithms/p1104_path_in_zigzag_labelled_binary_tree.py
@@ -0,0 +1,27 @@
+import collections
+import math
+from typing import List
+
+
+class Solution:
+    def pathInZigZagTree(self, label: int) -> List[int]:
+        queue = collections.deque([(1, [1])])
+        level = 0
+
+        while queue:
+            level += 1
+            size = len(queue)
+            start = int(math.pow(2, level))
+            new_queue = collections.deque([])
+
+            for i in range(size):
+                node, nodes = queue.pop()
+
+                if node == label:
+                    return nodes
+
+                a, b = start + 2 * i, start + 1 + 2 * i
+                new_queue.append((a, nodes + [a]))
+                new_queue.append((b, nodes + [b]))
+
+            queue = new_queue
---
+++ tests/algorithms/p1104_path_in_zigzag_labelled_binary_tree_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p1104_path_in_zigzag_labelled_binary_tree import Solution
+
+
+class TestPathInZigzagLabelledBinaryTree(unittest.TestCase):
+    def test_path_in_zigzag_labelled_binary_tree(self):
+        solution = Solution()
+
+        self.assertListEqual([1, 3, 4, 14], solution.pathInZigZagTree(14))
+        self.assertListEqual([1, 2, 6, 10, 26], solution.pathInZigZagTree(26))
