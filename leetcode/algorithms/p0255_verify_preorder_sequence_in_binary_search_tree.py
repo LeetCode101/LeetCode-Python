214bd6f686cd9a3663ae10503c65598e4b51d7a8
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-09 15:58:18 +0800

    p255

---
+++ leetcode/algorithms/p0255_verify_preorder_sequence_in_binary_search_tree.py
@@ -0,0 +1,25 @@
+import sys
+from typing import List
+
+
+# Time Limit Exceeded!
+class Solution:
+    def verifyPreorder(self, preorder: List[int]) -> bool:
+        return self._verify(preorder, 0, len(preorder) - 1, -sys.maxsize, sys.maxsize)
+
+    def _verify(self, preorder, start, end, min_value, max_value):
+        if start > end:
+            return True
+
+        root = preorder[start]
+
+        if root < min_value or root > max_value:
+            return False
+
+        i = start + 1
+
+        while i <= end and preorder[i] <= root:
+            i += 1
+
+        return self._verify(preorder, start + 1, i - 1, min_value, root) \
+            and self._verify(preorder, i, end, root, max_value)
