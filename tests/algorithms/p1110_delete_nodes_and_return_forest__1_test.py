8a65998d3d09036c4e36b20a7e7c7f067f7b080a
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-22 12:17:48 +0800

    Update p1110_delete_nodes_and_return_forest__1_test.py

--- tests/algorithms/p1110_delete_nodes_and_return_forest__1_test.py
+++ tests/algorithms/p1110_delete_nodes_and_return_forest__1_test.py
@@ -6,11 +6,17 @@ from leetcode.algorithms.p1110_delete_nodes_and_return_forest_1 \
 class TestDeleteNodesAndReturnForest(unittest.TestCase):
     def test_delete_nodes_and_return_forest(self):
         solution = Solution()
-        root = TreeNode(
+        root1 = TreeNode(
             1,
             TreeNode(2, TreeNode(4), TreeNode(5)),
             TreeNode(3, TreeNode(6), TreeNode(7))
         )
+        root2 = TreeNode(
+            1,
+            TreeNode(2, TreeNode(4), TreeNode(3))
+        )
 
         self.assertListEqual([1, 6, 7], list(map(
-            lambda x: x.val, solution.delNodes(root, [3, 5]))))
+            lambda x: x.val, solution.delNodes(root1, [3, 5]))))
+        self.assertListEqual([1, 4], list(map(
+            lambda x: x.val, solution.delNodes(root2, [2, 3]))))
