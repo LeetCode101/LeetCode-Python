72b4eebf6376246bdee0ab386a61e727d84e5434
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-26 09:26:14 +0800

    p1102

---
+++ leetcode/algorithms/p1102_path_with_maximum_minimum_value.py
@@ -0,0 +1,30 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def maximumMinimumPath(self, grid: List[List[int]]) -> int:
+        heap = [(-grid[0][0], 0, 0)]
+        heapq.heapify(heap)
+        m, n = len(grid), len(grid[0])
+        max_score = grid[0][0]
+        visited = [[False] * n for _ in range(m)]
+
+        while heap:
+            score, row, column = heapq.heappop(heap)
+            max_score = min(max_score, -score)
+
+            if row == m - 1 and column == n - 1:
+                break
+
+            for dx, dy in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
+                next_row = row + dx
+                next_column = column + dy
+
+                if 0 <= next_row < m and 0 <= next_column < n \
+                        and not visited[next_row][next_column]:
+                    heapq.heappush(heap, (-grid[next_row][next_column],
+                                          next_row, next_column))
+                    visited[next_row][next_column] = True
+
+        return max_score
---
+++ tests/algorithms/p1102_path_with_maximum_minimum_value_test.py
@@ -0,0 +1,15 @@
+import unittest
+from leetcode.algorithms.p1102_path_with_maximum_minimum_value import Solution
+
+
+class TestPathWithMaximumMinimumValue(unittest.TestCase):
+    def test_path_with_maximum_minimum_value(self):
+        solution = Solution()
+
+        self.assertEqual(4, solution.maximumMinimumPath(
+            [[5, 4, 5], [1, 2, 6], [7, 4, 6]]))
+        self.assertEqual(2, solution.maximumMinimumPath(
+            [[2, 2, 1, 2, 2, 2], [1, 2, 2, 2, 1, 2]]))
+        self.assertEqual(3, solution.maximumMinimumPath(
+            [[3, 4, 6, 3, 4], [0, 2, 1, 1, 7], [8, 8, 3, 2, 7],
+             [3, 2, 4, 9, 8], [4, 1, 2, 0, 0], [4, 6, 5, 4, 3]]))
