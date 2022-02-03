d799b20a1df838f3e40ba979b7508e73d3996e11
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-03 11:27:12 +0800

    p226

---
+++ tests/algorithms/p0226_invert_binary_tree_test.py
@@ -0,0 +1,16 @@
+import unittest
+from leetcode.algorithms.p0226_invert_binary_tree import Solution, TreeNode
+from tests.algorithms.binary_tree_helper import preorder
+
+
+class TestInvertBinaryTree(unittest.TestCase):
+    def test_invert_binary_tree(self):
+        solution = Solution()
+        root = TreeNode(
+            4,
+            TreeNode(2, TreeNode(1), TreeNode(3)),
+            TreeNode(7, TreeNode(6), TreeNode(9))
+        )
+
+        self.assertListEqual([4, 7, 9, 6, 2, 3, 1], preorder(
+            solution.invertTree(root)))
