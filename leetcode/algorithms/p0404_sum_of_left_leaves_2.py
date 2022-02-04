671b33b1a56953720033ce874a1122368302ab85
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-04 15:14:55 +0800

    p404

---
+++ leetcode/algorithms/p0404_sum_of_left_leaves_2.py
@@ -0,0 +1,31 @@
+import collections
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        sum = 0
+        queue = collections.deque([(root, None)])
+
+        while queue:
+            node, parent = queue.popleft()
+
+            if parent and node is parent.left \
+                    and not node.left and not node.right:
+                sum += node.val
+
+            if node.left:
+                queue.append((node.left, node))
+
+            if node.right:
+                queue.append((node.right, node))
+
+        return sum
---
+++ tests/algorithms/p0404_sum_of_left_leaves_2_test.py
@@ -0,0 +1,15 @@
+import unittest
+from leetcode.algorithms.p0404_sum_of_left_leaves_2 import Solution, TreeNode
+
+
+class TestSumOfLeftLeaves(unittest.TestCase):
+    def test_sum_of_left_leaves(self):
+        solution = Solution()
+        root = TreeNode(
+            3,
+            TreeNode(9),
+            TreeNode(20, TreeNode(15), TreeNode(7))
+        )
+
+        self.assertEqual(24, solution.sumOfLeftLeaves(root))
+        self.assertEqual(0, solution.sumOfLeftLeaves(TreeNode(1)))
