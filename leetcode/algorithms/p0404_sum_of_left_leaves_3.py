df88551d1a71576790f28c76240b845fef1e985d
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-04 15:15:50 +0800

    p404

---
+++ leetcode/algorithms/p0404_sum_of_left_leaves_3.py
@@ -0,0 +1,31 @@
+import collections
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        sum = 0
+        stack = [(root, None)]
+
+        while stack:
+            node, parent = stack.pop()
+
+            if parent and node is parent.left \
+                    and not node.left and not node.right:
+                sum += node.val
+
+            if node.left:
+                stack.append((node.left, node))
+
+            if node.right:
+                stack.append((node.right, node))
+
+        return sum
