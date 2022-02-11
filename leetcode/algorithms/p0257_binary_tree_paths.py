996d8c889d96c2cf6ad1539a2e20935ab1976add
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-11 14:33:51 +0800

    p257

---
+++ leetcode/algorithms/p0257_binary_tree_paths.py
@@ -0,0 +1,34 @@
+import collections
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root:
+            return []
+
+        result = []
+        queue = collections.deque([(root, [])])
+
+        while queue:
+            node, path = queue.popleft()
+            new_path = path + [str(node.val)]
+
+            if not node.left and not node.right:
+                result.append('->'.join(new_path))
+
+            if node.left:
+                queue.append((node.left, new_path))
+
+            if node.right:
+                queue.append((node.right, new_path))
+
+        return result
