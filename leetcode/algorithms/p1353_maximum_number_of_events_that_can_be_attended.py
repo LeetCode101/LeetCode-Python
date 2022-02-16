bec3072c8fa39946150b2041b1faa696102b4306
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-16 21:23:20 +0800

    Update p1353_maximum_number_of_events_that_can_be_attended.py

--- leetcode/algorithms/p1353_maximum_number_of_events_that_can_be_attended.py
+++ leetcode/algorithms/p1353_maximum_number_of_events_that_can_be_attended.py
@@ -7,30 +7,25 @@ class Solution:
         if not events:
             return 0
 
-        sorted_events = sorted(events)
-        total_days = max(event[1] for event in events)
         heap = []
-        i = 0
-        day = 1
         count = 0
+        i, n = 0, len(events)
+        sorted_events = sorted(events, key=lambda x: x[0])
+        day = 0
 
-        while day <= total_days:
-            if i < len(sorted_events) and not heap:
+        while i < n or heap:
+            if not heap:
                 day = sorted_events[i][0]
 
-            while i < len(sorted_events) and sorted_events[i][0] <= day:
+            while i < n and sorted_events[i][0] <= day:
                 heapq.heappush(heap, sorted_events[i][1])
                 i += 1
 
+            heapq.heappop(heap)
+            count += 1
+            day += 1
+
             while heap and heap[0] < day:
                 heapq.heappop(heap)
-
-            if heap:
-                heapq.heappop(heap)
-                count += 1
-            elif i >= len(sorted_events):
-                break
-
-            day += 1
 
         return count
