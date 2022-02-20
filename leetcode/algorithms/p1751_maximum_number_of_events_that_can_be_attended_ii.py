9c57a3b3c8442511a40e94c65b76206d59302dcd
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-20 16:17:25 +0800

    Update p1751_maximum_number_of_events_that_can_be_attended_ii.py

--- leetcode/algorithms/p1751_maximum_number_of_events_that_can_be_attended_ii.py
+++ leetcode/algorithms/p1751_maximum_number_of_events_that_can_be_attended_ii.py
@@ -7,11 +7,11 @@ class Solution:
         n = len(events)
         dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
         sorted_events = sorted(events)
-        starts = [x for x, _, _ in events]
+        starts = [x for x, _, _ in sorted_events]
 
         for i in range(n - 1, -1, -1):
             for j in range(1, k + 1):
                 next_event = bisect.bisect_right(starts, sorted_events[i][1])
-                dp[i][j] = max(dp[i + 1][j], events[i][2] + dp[next_event][j - 1])
+                dp[i][j] = max(dp[i + 1][j], sorted_events[i][2] + dp[next_event][j - 1])
 
         return dp[0][k]
