8f69ef7543a6cb8f0c1a9324a407d89acca5b6d2
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-27 11:09:03 +0800

    Update p1850_minimum_adjacent_swaps_to_reach_the_kth_smallest_number_test.py

--- tests/algorithms/p1850_minimum_adjacent_swaps_to_reach_the_kth_smallest_number_test.py
+++ tests/algorithms/p1850_minimum_adjacent_swaps_to_reach_the_kth_smallest_number_test.py
@@ -10,3 +10,4 @@ class TestMinimumAdjacentSwapsToReachTheKthSmallestNumber(unittest.TestCase):
 
         self.assertEqual(2, solution.getMinSwaps('5489355142', 4))
         self.assertEqual(4, solution.getMinSwaps('11112', 4))
+        self.assertEqual(3, solution.getMinSwaps('321', 1))
