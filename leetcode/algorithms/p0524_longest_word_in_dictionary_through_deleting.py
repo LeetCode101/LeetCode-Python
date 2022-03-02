403928d313e05d1bc7377235c80538ec4bfeed53
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-02 09:22:14 +0800

    Update p0524_longest_word_in_dictionary_through_deleting.py

--- leetcode/algorithms/p0524_longest_word_in_dictionary_through_deleting.py
+++ leetcode/algorithms/p0524_longest_word_in_dictionary_through_deleting.py
@@ -3,26 +3,22 @@ from typing import List
 
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        m = len(s)
         longest = ''
 
         for word in dictionary:
-            i, j = 0, 0
+            j = 0
             n = len(word)
 
-            while i < m and j < n:
-                if s[i] == word[j]:
+            for i, c in enumerate(s):
+                if c == word[j]:
                     j += 1
 
-                i += 1
-
                 if j == n:
-                    break
+                    if len(word) > len(longest):
+                        longest = word
+                    elif len(word) == len(longest) and word < longest:
+                        longest = word
 
-            if j == n:
-                if len(word) > len(longest):
-                    longest = word
-                elif len(word) == len(longest) and word < longest:
-                    longest = word
+                    break
 
         return longest
