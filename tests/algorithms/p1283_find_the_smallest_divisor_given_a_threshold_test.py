60593da73f1fda4f6f05471c9d0fc347fcb15664
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-06 11:48:01 +0800

    Update p1283_find_the_smallest_divisor_given_a_threshold_test.py

--- tests/algorithms/p1283_find_the_smallest_divisor_given_a_threshold_test.py
+++ tests/algorithms/p1283_find_the_smallest_divisor_given_a_threshold_test.py
@@ -7,5 +7,5 @@ class TestFindTheSmallestDivisorGivenAThreshold(unittest.TestCase):
     def test_find_the_smallest_divisor_given_a_threshold(self):
         solution = Solution()
 
-        # self.assertEqual(5, solution.smallestDivisor([1, 2, 5, 9], 6))
+        self.assertEqual(5, solution.smallestDivisor([1, 2, 5, 9], 6))
         self.assertEqual(44, solution.smallestDivisor([44, 22, 33, 11, 1], 5))
