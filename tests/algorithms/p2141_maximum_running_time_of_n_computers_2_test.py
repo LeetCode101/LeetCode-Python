8d3ce05f8eb8c965bec392d92f535701dcb33fba
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-12 12:21:19 +0800

    Update p2141_maximum_running_time_of_n_computers_2_test.py

--- tests/algorithms/p2141_maximum_running_time_of_n_computers_2_test.py
+++ tests/algorithms/p2141_maximum_running_time_of_n_computers_2_test.py
@@ -7,5 +7,6 @@ class TestMaximumRunningTimeOfNComputers(unittest.TestCase):
     def test_maximum_running_time_of_n_computers(self):
         solution = Solution()
 
+        self.assertEqual(8, solution.maxRunTime(3, [10, 10, 3, 5]))
         self.assertEqual(4, solution.maxRunTime(2, [3, 3, 3]))
         self.assertEqual(2, solution.maxRunTime(2, [1, 1, 1, 1]))
