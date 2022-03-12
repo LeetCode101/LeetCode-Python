49cfd5770ac09bbc7fadbb8397f0add7a4f76459
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-12 12:49:56 +0800

    Update p2141_maximum_running_time_of_n_computers_2.py

--- leetcode/algorithms/p2141_maximum_running_time_of_n_computers_2.py
+++ leetcode/algorithms/p2141_maximum_running_time_of_n_computers_2.py
@@ -1,15 +1,32 @@
-import heapq
 from typing import List
 
 
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
         total_energy = sum(batteries)
-        heap = [-x for x in batteries]
-        heapq.heapify(heap)
+        left, right = 0, total_energy // n
 
-        while -heap[0] > total_energy // n:
-            total_energy -= -heapq.heappop(heap)
-            n -= 1
+        while left < right:
+            middle = left + (right - left) // 2 + 1
 
-        return total_energy // n
+            if self._can_run_all_computers(n, middle, batteries):
+                left = middle
+            else:
+                right = middle - 1
+
+        return left
+
+    def _can_run_all_computers(self, n, time, batteries):
+        required_batteries = n * time
+        current_batteries = 0
+
+        for battery in batteries:
+            if battery < time:
+                current_batteries += battery
+            else:
+                current_batteries += time
+
+            if current_batteries >= required_batteries:
+                return True
+
+        return current_batteries >= required_batteries
