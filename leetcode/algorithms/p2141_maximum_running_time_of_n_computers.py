91a45928420736cbe20cae75a97eee7c5c27fe23
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-12 11:54:51 +0800

    p2141

---
+++ leetcode/algorithms/p2141_maximum_running_time_of_n_computers.py
@@ -0,0 +1,28 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        heap = [-x for x in batteries]
+        heapq.heapify(heap)
+        time = 0
+
+        while heap:
+            current_batteries = []
+
+            for _ in range(n):
+                if not heap:
+                    return time
+
+                remaining_battery_time = -heapq.heappop(heap) - 1
+
+                if remaining_battery_time > 0:
+                    current_batteries.append(remaining_battery_time)
+
+            time += 1
+
+            for battery in current_batteries:
+                heapq.heappush(heap, -battery)
+
+        return time
---
+++ tests/algorithms/p2141_maximum_running_time_of_n_computers_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p2141_maximum_running_time_of_n_computers \
+    import Solution
+
+
+class TestMaximumRunningTimeOfNComputers(unittest.TestCase):
+    def test_maximum_running_time_of_n_computers(self):
+        solution = Solution()
+
+        self.assertEqual(4, solution.maxRunTime(2, [3, 3, 3]))
+        self.assertEqual(2, solution.maxRunTime(2, [1, 1, 1, 1]))
