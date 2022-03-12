54f26ff1c86bb0ea9383844c98b2bd79ad560cf4
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-12 17:28:48 +0800

    p1648

---
+++ leetcode/algorithms/p1648_sell_diminishing_valued_colored_balls_2.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, inventory: List[int], orders: int) -> int:
+        left, right = 0, max(inventory)
+        bound = -1
+        mod = 1000000007
+
+        while left <= right:
+            middle = left + (right - left) // 2
+            total = sum(x - middle for x in inventory if x >= middle)
+
+            if total <= orders:
+                bound = middle
+                right = middle - 1
+            else:
+                left = middle + 1
+
+        range_sum = lambda x, y: (x + y) * (y - x + 1) // 2
+        rest = orders - sum(x - bound for x in inventory if x >= bound)
+        profit = 0
+
+        for x in inventory:
+            if x < bound:
+                continue
+
+            if rest > 0:
+                profit = (profit + range_sum(bound, x)) % mod
+                rest -= 1
+            else:
+                profit = (profit + range_sum(bound + 1, x)) % mod
+
+        return profit
---
+++ tests/algorithms/p1648_sell_diminishing_valued_colored_balls_2_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p1648_sell_diminishing_valued_colored_balls_2 \
+    import Solution
+
+
+class TestSellDiminishingValuedColoredBalls(unittest.TestCase):
+    def test_sell_diminishing_valued_colored_balls(self):
+        solution = Solution()
+
+        self.assertEqual(14, solution.maxProfit([2, 5], 4))
+        self.assertEqual(19, solution.maxProfit([3, 5], 6))
