60dffa8bab0f1b972209b8bf6ab01e0a3926ed9a
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-13 17:41:54 +0800

    Update p1648_sell_diminishing_valued_colored_balls_1_test.py

--- tests/algorithms/p1648_sell_diminishing_valued_colored_balls_1_test.py
+++ tests/algorithms/p1648_sell_diminishing_valued_colored_balls_1_test.py
@@ -7,5 +7,6 @@ class TestSellDiminishingValuedColoredBalls(unittest.TestCase):
     def test_sell_diminishing_valued_colored_balls(self):
         solution = Solution()
 
+        self.assertEqual(1, solution.maxProfit([1], 2))
         self.assertEqual(14, solution.maxProfit([2, 5], 4))
         self.assertEqual(19, solution.maxProfit([3, 5], 6))
