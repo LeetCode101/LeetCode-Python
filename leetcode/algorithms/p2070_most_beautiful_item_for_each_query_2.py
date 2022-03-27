fb67b184cca0f7d2164227a1fd555b560d7331e1
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-27 13:24:38 +0800

    p2070

---
+++ leetcode/algorithms/p2070_most_beautiful_item_for_each_query_2.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) \
+            -> List[int]:
+        result = [0] * len(queries)
+        sorted_items = sorted(items, key=lambda x: x[0])
+        sorted_queries = sorted([(x, i) for i, x in enumerate(queries)])
+        prev = -1
+        max_beauty = 0
+
+        for price, i in sorted_queries:
+            j = self._select_right(sorted_items, price, prev)
+
+            for k in range(max(0, prev), j):
+                max_beauty = max(max_beauty, sorted_items[k][1])
+
+            result[i] = max_beauty
+            prev = j
+
+        return result
+
+    def _select_right(self, items, target, start):
+        low, high = 0, len(items) - 1
+
+        if start != -1:
+            low = start
+
+        while low <= high:
+            middle = low + (high - low) // 2
+
+            if items[middle][0] <= target:
+                low = middle + 1
+            else:
+                high = middle - 1
+
+        return low
---
+++ tests/algorithms/p2070_most_beautiful_item_for_each_query_2_test.py
@@ -0,0 +1,14 @@
+import unittest
+from leetcode.algorithms.p2070_most_beautiful_item_for_each_query_2 \
+    import Solution
+
+
+class TestMostBeautifulItemForEachQuery(unittest.TestCase):
+    def test_most_beautiful_item_for_each_query(self):
+        solution = Solution()
+
+        self.assertListEqual([2, 4, 5, 5, 6, 6], solution.maximumBeauty(
+            [[1, 2], [3, 2], [2, 4], [5, 6], [3, 5]], [1, 2, 3, 4, 5, 6]))
+        self.assertListEqual([4], solution.maximumBeauty(
+            [[1, 2], [1, 2], [1, 3], [1, 4]], [1]))
+        self.assertListEqual([0], solution.maximumBeauty([[10, 1000]], [5]))
