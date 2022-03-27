f087f7d20385f1a261d113136d3fab5b74342cb8
Frederick-S <mao_xiaodan@hotmail.com>
2022-03-27 11:08:10 +0800

    p1231

---
+++ leetcode/algorithms/p1231_divide_chocolate.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        low, high = min(sweetness), sum(sweetness)
+
+        while low < high:
+            middle = low + (high - low) // 2 + 1
+            count = self._count(sweetness, middle)
+
+            if count >= k + 1:
+                low = middle
+            else:
+                high = middle - 1
+
+        return low
+
+    def _count(self, sweetness, sweet):
+        sum_so_far = 0
+        count = 0
+
+        for x in sweetness:
+            sum_so_far += x
+
+            if sum_so_far >= sweet:
+                count += 1
+                sum_so_far = 0
+
+        return count
---
+++ tests/algorithms/p1231_divide_chocolate_test.py
@@ -0,0 +1,17 @@
+import unittest
+from leetcode.algorithms.p1231_divide_chocolate import Solution
+
+
+class TestDivideChocolate(unittest.TestCase):
+    def test_divide_chocolate(self):
+        solution = Solution()
+
+        self.assertEqual(55382, solution.maximizeSweetness(
+            [90670, 55382, 95298, 95795, 73204, 41464,
+             18675, 30104, 47442, 55307], 6))
+        self.assertEqual(6, solution.maximizeSweetness(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9], 5))
+        self.assertEqual(1, solution.maximizeSweetness(
+            [5, 6, 7, 8, 9, 1, 2, 3, 4], 8))
+        self.assertEqual(5, solution.maximizeSweetness(
+            [1, 2, 2, 1, 2, 2, 1, 2, 2], 2))
