30acdcf9623a7230cef1d27d69c1f30867228e36
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-02 12:26:32 +0800

    Update p0826_most_profit_assigning_work.py

--- leetcode/algorithms/p0826_most_profit_assigning_work.py
+++ leetcode/algorithms/p0826_most_profit_assigning_work.py
@@ -5,7 +5,8 @@ from typing import List
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int],
                             worker: List[int]) -> int:
-        sorted_profit = sorted([(x, i) for i, x in enumerate(profit)], reverse=True)
+        sorted_profit = sorted([(x, i) for i, x in enumerate(profit)],
+                               reverse=True)
         sorted_worker = sorted(worker)
         worker_end = len(sorted_worker) - 1
         max_profit = 0
@@ -14,7 +15,8 @@ class Solution:
             if difficulty[i] > sorted_worker[worker_end]:
                 continue
 
-            j = bisect.bisect_left(sorted_worker, difficulty[i], lo=0, hi=worker_end + 1)
+            j = bisect.bisect_left(sorted_worker, difficulty[i],
+                                   lo=0, hi=worker_end + 1)
             max_profit += p * (worker_end - j + 1)
             worker_end = j - 1
 
