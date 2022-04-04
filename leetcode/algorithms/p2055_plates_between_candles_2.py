7a6056c1ea3836a48c8a9cae4caac6e963066867
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-04 11:23:23 +0800

    Update p2055_plates_between_candles_2.py

--- leetcode/algorithms/p2055_plates_between_candles_2.py
+++ leetcode/algorithms/p2055_plates_between_candles_2.py
@@ -11,7 +11,7 @@ class Solution:
         for start, end in queries:
             i = bisect.bisect_left(candles, start)
             j = bisect.bisect_right(candles, end) - 1
-            
+
             if i < j:
                 window_size = candles[j] - candles[i] + 1
                 candle_size = j - i + 1
