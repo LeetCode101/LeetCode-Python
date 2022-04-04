8291812fa6afacc764800df26da775ef466682fa
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-04 13:56:00 +0800

    Update p0033_search_in_rotated_sorted_array_test.py

--- tests/algorithms/p0033_search_in_rotated_sorted_array_test.py
+++ tests/algorithms/p0033_search_in_rotated_sorted_array_test.py
@@ -6,6 +6,8 @@ class TestSearchInRotatedSortedArray(unittest.TestCase):
     def test_search_in_rotated_sorted_array(self):
         solution = Solution()
 
+        self.assertEqual(-1, solution.search([5, 1, 3], 0))
+        self.assertEqual(2, solution.search([5, 1, 3], 3))
         self.assertEqual(4, solution.search([4, 5, 6, 7, 0, 1, 2], 0))
         self.assertEqual(-1, solution.search([4, 5, 6, 7, 0, 1, 2], 3))
         self.assertEqual(-1, solution.search([4, 5, 6, 7, 0, 1, 2], 10))
