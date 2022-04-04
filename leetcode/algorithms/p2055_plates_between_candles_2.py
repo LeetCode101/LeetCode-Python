f767281ffde835c4b22ab8aa9d36028ad7e95614
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-04 11:20:47 +0800

    p2055

---
+++ leetcode/algorithms/p2055_plates_between_candles_2.py
@@ -0,0 +1,22 @@
+import bisect
+from typing import List
+
+
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) \
+            -> List[int]:
+        candles = [i for i, c in enumerate(s) if c == '|']
+        result = []
+
+        for start, end in queries:
+            i = bisect.bisect_left(candles, start)
+            j = bisect.bisect_right(candles, end) - 1
+            
+            if i < j:
+                window_size = candles[j] - candles[i] + 1
+                candle_size = j - i + 1
+                result.append(window_size - candle_size)
+            else:
+                result.append(0)
+
+        return result
