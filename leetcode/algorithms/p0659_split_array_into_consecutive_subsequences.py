9b47ae79e889ca975d79a54b1e22513dd5e8d894
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-05 16:01:41 +0800

    Update p0659_split_array_into_consecutive_subsequences.py

--- leetcode/algorithms/p0659_split_array_into_consecutive_subsequences.py
+++ leetcode/algorithms/p0659_split_array_into_consecutive_subsequences.py
@@ -4,40 +4,23 @@ from typing import List
 
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        length = nums[-1] - nums[0] + 1
-        buckets = [0] * length
         counter = collections.Counter(nums)
-        shift = nums[0]
+        next_numbers = collections.defaultdict(int)
 
-        for n, count in counter.items():
-            buckets[n - shift] = count
+        for n in nums:
+            if counter[n] == 0:
+                continue
 
-        while True:
-            i = 0
-
-            while i < length and buckets[i] == 0:
-                i += 1
-
-            if i == length:
-                break
-
-            count = 1
-            base = buckets[i]
-            buckets[i] = 0
-            i += 1
-            prev_count = 0
-
-            while i < length and buckets[i] != 0:
-                if buckets[i] >= base and buckets[i] - base >= prev_count:
-                    buckets[i] -= base
-                    count += 1
-                else:
-                    break
-
-                prev_count = buckets[i]
-                i += 1
-
-            if count < 3:
+            if next_numbers[n] > 0:
+                next_numbers[n] -= 1
+                next_numbers[n + 1] += 1
+            elif counter[n + 1] > 0 and counter[n + 2] > 0:
+                counter[n + 1] -= 1
+                counter[n + 2] -= 1
+                next_numbers[n + 3] += 1
+            else:
                 return False
+
+            counter[n] -= 1
 
         return True
