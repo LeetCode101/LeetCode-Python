0079840095513720f5ab6ca665c6b52c5866ec01
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-05 10:55:37 +0800

    Update p1985_find_the_kth_largest_integer_in_the_array.py

--- leetcode/algorithms/p1985_find_the_kth_largest_integer_in_the_array.py
+++ leetcode/algorithms/p1985_find_the_kth_largest_integer_in_the_array.py
@@ -1,31 +1,35 @@
+from random import shuffle
 from typing import List
 
 
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        low, high = 0, len(nums) - 1
+        shuffle(nums)
+        lo, hi = 0, len(nums) - 1
 
-        while low <= high:
-            p = self._partition(nums, low, high)
-
-            if p == k - 1:
-                return nums[p]
-            elif p > k - 1:
-                high = p - 1
+        while lo <= hi:
+            mid = self._partition(nums, lo, hi)
+            if mid == k - 1:
+                return str(nums[mid])
+            elif mid < k - 1:
+                lo = mid + 1
             else:
-                low = p + 1
+                hi = mid - 1
 
-    def _partition(self, nums: List[str], low: int, high: int):
-        pivot = (low + high) // 2
-        pivot_value = int(nums[pivot])
-        nums[pivot], nums[high] = nums[high], nums[pivot]
-        p = low
+    def _partition(self, nums, low, high):
+        i, j = low + 1, high
+        pivot_value = int(nums[low])
 
-        for i in range(low, high):
-            if int(nums[i]) >= pivot_value:
-                nums[p], nums[i] = nums[i], nums[p]
-                p += 1
+        while i <= j:
+            if int(nums[j]) < pivot_value:
+                j -= 1
+            elif pivot_value < int(nums[i]):
+                i += 1
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
 
-        nums[high], nums[p] = nums[p], nums[high]
+        nums[low], nums[j] = nums[j], nums[low]
 
-        return p
+        return j
