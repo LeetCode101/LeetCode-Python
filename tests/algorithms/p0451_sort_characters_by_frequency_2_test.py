994beddaba22e62180b669b0be8d8989ade04c9b
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-06 14:45:36 +0800

    Update p0451_sort_characters_by_frequency_2_test.py

--- tests/algorithms/p0451_sort_characters_by_frequency_2_test.py
+++ tests/algorithms/p0451_sort_characters_by_frequency_2_test.py
@@ -6,7 +6,8 @@ class TestSortCharactersByFrequency(unittest.TestCase):
     def test_sort_characters_by_frequency(self):
         solution = Solution()
 
-        self.assertEqual('eetr', solution.frequencySort('tree'))
+        self.assertTrue(
+            solution.frequencySort('tree') in ['eetr', 'eert'])
         self.assertTrue(
             solution.frequencySort('cccaaa') in ['aaaccc', 'cccaaa'])
         self.assertEqual('bbAa', solution.frequencySort('Aabb'))
