2c45d7de02e8fdf59dd768e891f5799db62d0fa2
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-07 10:43:51 +0800

    Update p0079_word_search.py

--- leetcode/algorithms/p0079_word_search.py
+++ leetcode/algorithms/p0079_word_search.py
@@ -9,18 +9,18 @@ class Solution:
             for j in range(n):
                 visited = [[False] * n for _ in range(m)]
 
-                if self._dfs(board, i, j, word, 0, len(word) - 1, visited):
+                if self._dfs(board, i, j, word, 0, visited):
                     return True
 
         return False
 
-    def _dfs(self, board, row, column, word, start, end, visited):
+    def _dfs(self, board, row, column, word, start, visited):
         m, n = len(board), len(board[0])
 
         if row < 0 or row == m or column < 0 or column == n or visited[row][column] or board[row][column] != word[start]:
             return False
 
-        if start == end:
+        if start == len(word) - 1:
             return True
 
         visited[row][column] = True
@@ -29,8 +29,10 @@ class Solution:
             next_row = row + dx
             next_column = column + dy
 
-            if 0 <= next_row < m and 0 <= next_column < n and not visited[next_row][next_column] and board[next_row][next_column] == word[start + 1]:
-                found = self._dfs(board, next_row, next_column, word, start + 1, end, visited)
+            if 0 <= next_row < m and 0 <= next_column < n \
+                    and not visited[next_row][next_column] \
+                    and board[next_row][next_column] == word[start + 1]:
+                found = self._dfs(board, next_row, next_column, word, start + 1, visited)
 
                 if found:
                     return True
