41e7daab09d6e050c2658aa8d1df7368f8b6b3d9
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-08 15:53:21 +0800

    Update p1146_snapshot_array.py

--- leetcode/algorithms/p1146_snapshot_array.py
+++ leetcode/algorithms/p1146_snapshot_array.py
@@ -6,14 +6,17 @@ class SnapshotArray:
         self.snap_id = -1
         self.array = [0] * length
         self.snaps = [collections.defaultdict(int) for _ in range(length)]
+        self.set_index = set()
 
     def set(self, index: int, val: int) -> None:
         self.array[index] = val
+        self.set_index.add(index)
 
     def snap(self) -> int:
         self.snap_id += 1
 
-        for i, snap in enumerate(self.snaps):
+        for i in self.set_index:
+            snap = self.snaps[i]
             snap[self.snap_id] = self.array[i]
 
         return self.snap_id
