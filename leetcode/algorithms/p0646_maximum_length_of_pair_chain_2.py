0c57eb005b7d996230d8494d79ed0e9113da670a
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-08 11:42:32 +0800

    Update p0646_maximum_length_of_pair_chain_2.py

--- leetcode/algorithms/p0646_maximum_length_of_pair_chain_2.py
+++ leetcode/algorithms/p0646_maximum_length_of_pair_chain_2.py
@@ -4,7 +4,6 @@ from typing import List
 
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
-        n = len(pairs)
         sorted_pairs = sorted(pairs, key=lambda x: x[1])
         tail = -sys.maxsize
         max_length = 0
