bedfa6e387a91780482584bfc9f98caa1104d7e5
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-08 15:51:27 +0800

    Update p1150_check_if_a_number_is_majority_element_in_a_sorted_array_test.py

--- tests/algorithms/p1150_check_if_a_number_is_majority_element_in_a_sorted_array_test.py
+++ tests/algorithms/p1150_check_if_a_number_is_majority_element_in_a_sorted_array_test.py
@@ -8,6 +8,7 @@ class TestCheckIfANumberIsMajorityElementInASortedArray(unittest.TestCase):
     def test_check_if_a_number_is_majority_element_in_a_sorted_array(self):
         solution = Solution()
 
+        self.assertFalse(solution.isMajorityElement([1], 2))
         self.assertFalse(solution.isMajorityElement([1, 2, 2], 1))
         self.assertTrue(solution.isMajorityElement(
             [2, 4, 5, 5, 5, 5, 5, 6, 6], 5))
