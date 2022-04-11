fbc93075cfac5a5d4b755c10b837b7bd6ebab3e5
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-11 12:17:40 +0800

    Update p0857_minimum_cost_to_hire_k_workers.py

--- leetcode/algorithms/p0857_minimum_cost_to_hire_k_workers.py
+++ leetcode/algorithms/p0857_minimum_cost_to_hire_k_workers.py
@@ -9,13 +9,14 @@ class Solution:
         total_quality = 0
         min_cost = sys.maxsize
         heap = []
+        workers = sorted(zip(wage, quality), key=lambda x: x[0] / x[1])
 
-        for cost, q in sorted(zip(wage, quality), key=lambda x: x[0] / x[1]):
+        for cost, q in workers:
             total_quality += q
             heapq.heappush(heap, -q)
 
             if len(heap) > k:
-                total_quality += heapq.heappop(heap)
+                total_quality -= -heapq.heappop(heap)
 
             if len(heap) == k:
                 min_cost = min(min_cost, total_quality * cost / q)
