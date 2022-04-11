94106d1145736e16d91a68a27af9fc6401956082
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-11 12:06:20 +0800

    p857

---
+++ leetcode/algorithms/p0857_minimum_cost_to_hire_k_workers.py
@@ -0,0 +1,23 @@
+import heapq
+import sys
+from typing import List
+
+
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int],
+                             wage: List[int], k: int) -> float:
+        total_quality = 0
+        min_cost = sys.maxsize
+        heap = []
+
+        for cost, q in sorted(zip(wage, quality), key=lambda x: x[0] / x[1]):
+            total_quality += q
+            heapq.heappush(heap, -q)
+
+            if len(heap) > k:
+                total_quality += heapq.heappop(heap)
+
+            if len(heap) == k:
+                min_cost = min(min_cost, total_quality * cost / q)
+
+        return min_cost
---
+++ tests/algorithms/p0857_minimum_cost_to_hire_k_workers_test.py
@@ -0,0 +1,12 @@
+import unittest
+from leetcode.algorithms.p0857_minimum_cost_to_hire_k_workers import Solution
+
+
+class TestMinimumCostToHireKWorkers(unittest.TestCase):
+    def test_minimum_cost_to_hire_k_workers(self):
+        solution = Solution()
+
+        self.assertTrue(abs(solution.mincostToHireWorkers(
+            [10, 20, 5], [70, 50, 30], 2)) < 0.01)
+        self.assertTrue(abs(solution.mincostToHireWorkers(
+            [3, 1, 10, 10, 1], [4, 8, 2, 2, 7], 3)) < 0.01)
