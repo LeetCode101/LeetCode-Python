8dcd67aef60e7a897400321ba4eca13c8ba84866
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-13 12:32:23 +0800

    p1750

---
+++ leetcode/algorithms/p1750_minimum_length_of_string_after_deleting_similar_ends.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        low, high = 0, len(s) - 1
+
+        while low < high:
+            if s[low] != s[high]:
+                return high - low + 1
+
+            i, j = low, high
+
+            while i < high and s[i] == s[low]:
+                i += 1
+
+            while j > low and s[j] == s[high]:
+                j -= 1
+
+            if i <= j and s[i] != s[low]:
+                low = i
+                high = j
+            else:
+                return 0
+
+        return 1 if low == high else 0
---
+++ tests/algorithms/p1750_minimum_length_of_string_after_deleting_similar_ends_test.py
@@ -0,0 +1,14 @@
+import unittest
+from leetcode.algorithms\
+    .p1750_minimum_length_of_string_after_deleting_similar_ends import Solution
+
+
+class TestMinimumLengthOfStringAfterDeletingSimilarEnds(unittest.TestCase):
+    def test_minimum_length_of_string_after_deleting_similar_ends(self):
+        solution = Solution()
+
+        self.assertEqual(1, solution.minimumLength(
+            'bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb'))
+        self.assertEqual(2, solution.minimumLength('ca'))
+        self.assertEqual(0, solution.minimumLength('cabaabac'))
+        self.assertEqual(3, solution.minimumLength('aabccabba'))
