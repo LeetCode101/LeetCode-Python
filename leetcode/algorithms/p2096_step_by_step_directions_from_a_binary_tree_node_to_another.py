0134f0f740c19e6ba4395575eef18a300cd4c263
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-15 16:03:32 +0800

    p2096

---
+++ leetcode/algorithms/p2096_step_by_step_directions_from_a_binary_tree_node_to_another.py
@@ -0,0 +1,46 @@
+import collections
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int,
+                      destValue: int) -> str:
+        graph = collections.defaultdict(list)
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+
+            if node.left:
+                graph[node.val].append((node.left.val, 'L'))
+                graph[node.left.val].append((node.val, 'U'))
+                queue.append(node.left)
+
+            if node.right:
+                graph[node.val].append((node.right.val, 'R'))
+                graph[node.right.val].append((node.val, 'U'))
+                queue.append(node.right)
+
+        queue = collections.deque([(startValue, '')])
+        visited = {startValue}
+
+        while queue:
+            node, path = queue.popleft()
+
+            if node == destValue:
+                return path
+
+            for neighbour, direction in graph[node]:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    queue.append((neighbour, path + direction))
+
+        return ''
---
+++ tests/algorithms/p2096_step_by_step_directions_from_a_binary_tree_node_to_another_test.py
@@ -0,0 +1,17 @@
+import unittest
+from leetcode.algorithms\
+    .p2096_step_by_step_directions_from_a_binary_tree_node_to_another \
+    import Solution, TreeNode
+
+
+class TestStepByStepDirectionsFromABinaryTreeNodeToAnother(unittest.TestCase):
+    def test_step_by_step_directions_from_a_binary_tree_node_to_another(self):
+        solution = Solution()
+        root = TreeNode(
+            5,
+            TreeNode(1, TreeNode(3)),
+            TreeNode(2, TreeNode(6), TreeNode(4))
+        )
+
+        self.assertEqual('UURL', solution.getDirections(root, 3, 6))
+        self.assertEqual('', solution.getDirections(root, 9, 9))
