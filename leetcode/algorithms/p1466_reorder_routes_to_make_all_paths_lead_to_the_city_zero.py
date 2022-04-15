bd5474f612d1ceb98e3eb4c622155ead48a7ee17
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-15 14:17:17 +0800

    Update p1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.py

--- leetcode/algorithms/p1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.py
+++ leetcode/algorithms/p1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.py
@@ -5,12 +5,11 @@ from typing import List
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         graph = collections.defaultdict(list)
-        direction = [[False] * n for _ in range(n)]
+        connection_set = set([(a, b) for a, b in connections])
 
         for a, b in connections:
             graph[a].append(b)
             graph[b].append(a)
-            direction[a][b] = True
 
         queue = collections.deque([0])
         visited = {0}
@@ -21,7 +20,7 @@ class Solution:
 
             for neighbour in graph[node]:
                 if neighbour not in visited:
-                    if direction[node][neighbour]:
+                    if (node, neighbour) in connection_set:
                         count += 1
 
                     visited.add(neighbour)
