7be845776b543943afada2caea0d46b298de80f9
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-15 15:43:35 +0800

    Update p1315_sum_of_nodes_with_even_valued_grandparent.py

--- leetcode/algorithms/p1315_sum_of_nodes_with_even_valued_grandparent.py
+++ leetcode/algorithms/p1315_sum_of_nodes_with_even_valued_grandparent.py
@@ -1,7 +1,7 @@
-# Definition for a binary tree node.
 import collections
 
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
