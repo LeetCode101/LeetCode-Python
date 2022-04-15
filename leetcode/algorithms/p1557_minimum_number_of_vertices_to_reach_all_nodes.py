0bb2cd31a1ed6bb400341781f4cb6ef2118fb58f
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-15 14:44:22 +0800

    Update p1557_minimum_number_of_vertices_to_reach_all_nodes.py

--- leetcode/algorithms/p1557_minimum_number_of_vertices_to_reach_all_nodes.py
+++ leetcode/algorithms/p1557_minimum_number_of_vertices_to_reach_all_nodes.py
@@ -1,4 +1,3 @@
-import collections
 from typing import List
 
 
@@ -6,16 +5,8 @@ class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) \
             -> List[int]:
         visited = set()
-        graph = collections.defaultdict(list)
 
         for a, b in edges:
-            graph[a].append(b)
             visited.add(b)
 
-        result = []
-
-        for i in range(n):
-            if i not in visited:
-                result.append(i)
-
-        return result
+        return [i for i in range(n) if i not in visited]
