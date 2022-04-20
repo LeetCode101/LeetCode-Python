3cb3d736c6368ba789e808a212be57a8acf67169
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-20 16:42:01 +0800

    Update p1105_filling_bookcase_shelves.py

--- leetcode/algorithms/p1105_filling_bookcase_shelves.py
+++ leetcode/algorithms/p1105_filling_bookcase_shelves.py
@@ -6,18 +6,18 @@ class Solution:
         n = len(books)
         dp = [0] * (n + 1)
 
-        for i in range(n):
-            dp[i + 1] = dp[i] + books[i][1]
+        for i in range(1, n + 1):
+            dp[i] = dp[i - 1] + books[i - 1][1]
             width_sum = 0
             height = 0
 
-            for j in range(i, -1, -1):
+            for j in range(i - 1, -1, -1):
                 width_sum += books[j][0]
 
                 if width_sum > shelfWidth:
                     break
 
                 height = max(height, books[j][1])
-                dp[i + 1] = min(dp[i + 1], dp[j] + height)
+                dp[i] = min(dp[i], dp[j] + height)
 
         return dp[-1]
