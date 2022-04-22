788d0553f44a08fe251ca3b8edbd63831c3a9424
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-22 11:22:31 +0800

    Update p0413_arithmetic_slices_test.py

--- tests/algorithms/p0413_arithmetic_slices_test.py
+++ tests/algorithms/p0413_arithmetic_slices_test.py
@@ -6,4 +6,4 @@ class TestArithmeticSlices(unittest.TestCase):
     def test_arithmetic_slices(self):
         solution = Solution()
 
-        self.assertEqual(3, solution.numberOfArithmeticSlices([1,2,3,4]))
+        self.assertEqual(3, solution.numberOfArithmeticSlices([1, 2, 3, 4]))
