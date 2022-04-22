d3fd057a2a9d842bbd6e84240571d911c12d99a5
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-22 14:33:57 +0800

    p64

---
+++ leetcode/algorithms/p0064_minimum_path_sum_2.py
@@ -0,0 +1,23 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [0] * n
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[j] = grid[i][j]
+                elif i == 0:
+                    dp[j] = dp[j - 1] + grid[i][j]
+                elif j == 0:
+                    dp[j] = dp[j] + grid[i][j]
+                else:
+                    left = dp[j - 1]
+                    up = dp[j]
+                    dp[j] = grid[i][j] + min(left, up)
+
+        return dp[-1]
---
+++ tests/algorithms/p0064_minimum_path_sum_2_test.py
@@ -0,0 +1,11 @@
+import unittest
+from leetcode.algorithms.p0064_minimum_path_sum_2 import Solution
+
+
+class TestMinimumPathSum(unittest.TestCase):
+    def test_minimum_path_sum(self):
+        solution = Solution()
+
+        self.assertEqual(7, solution.minPathSum(
+            [[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
+        self.assertEqual(12, solution.minPathSum([[1, 2, 3], [4, 5, 6]]))
