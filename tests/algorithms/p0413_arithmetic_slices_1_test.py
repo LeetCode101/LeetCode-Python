a364fbcc4c9885a03e7b703ac41589b47d00c7c7
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-22 11:27:56 +0800

    rename file

--- tests/algorithms/p0413_arithmetic_slices_1_test.py
+++ tests/algorithms/p0413_arithmetic_slices_1_test.py
@@ -1,5 +1,5 @@
 import unittest
-from leetcode.algorithms.p0413_arithmetic_slices import Solution
+from leetcode.algorithms.p0413_arithmetic_slices_1 import Solution
 
 
 class TestArithmeticSlices(unittest.TestCase):
