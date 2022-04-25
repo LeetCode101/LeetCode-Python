7dfae54acb50eb9ed705b7fa3e588016df9a1fae
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-25 12:35:49 +0800

    Update p0416_partition_equal_subset_sum.py

--- leetcode/algorithms/p0416_partition_equal_subset_sum.py
+++ leetcode/algorithms/p0416_partition_equal_subset_sum.py
@@ -16,7 +16,7 @@ class Solution:
             dp[i][0] = True
 
         for i in range(1, n + 1):
-            for j in range(target + 1):
+            for j in range(1, target + 1):
                 if j < nums[i - 1]:
                     dp[i][j] = dp[i - 1][j]
                 else:
