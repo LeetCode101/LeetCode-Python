d8e62da5b931c64dee13659dcad0252e79cbc9f7
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-26 15:46:30 +0800

    Update p1824_minimum_sideway_jumps.py

--- leetcode/algorithms/p1824_minimum_sideway_jumps.py
+++ leetcode/algorithms/p1824_minimum_sideway_jumps.py
@@ -4,17 +4,19 @@ from typing import List
 
 class Solution:
     def minSideJumps(self, obstacles: List[int]) -> int:
-        n = len(obstacles) - 1
-        dp = [[0] * 3 for _ in range(n)]
-        dp[0][0] = dp[0][2] = 1
+        n = len(obstacles)
+        dp = [[sys.maxsize] * 3 for _ in range(n)]
+        dp[0] = [1, 0, 1]
 
         for i in range(1, n):
             for j in range(3):
-                if obstacles[i] == j + 1 or obstacles[i + 1] == j + 1:
-                    dp[i][j] = sys.maxsize
-                else:
-                    dp[i][j] = min(dp[i - 1][j],
-                                   dp[i - 1][(j + 1) % 3] + 1,
-                                   dp[i - 1][(j + 2) % 3] + 1)
+                if obstacles[i] != j + 1:
+                    dp[i][j] = dp[i - 1][j]
+
+            min_jumps = min(dp[i])
+
+            for j in range(3):
+                if obstacles[i] != j + 1:
+                    dp[i][j] = min(dp[i][j], min_jumps + 1)
 
         return min(dp[-1])
