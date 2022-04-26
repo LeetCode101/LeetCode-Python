1bab4021cdfe554661c3ce4a136b096bf297b4c1
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-26 16:32:31 +0800

    p650

---
+++ tests/algorithms/p0650_2_keys_keyboard_test.py
@@ -0,0 +1,10 @@
+import unittest
+from leetcode.algorithms.p0650_2_keys_keyboard import Solution
+
+
+class Test2KeysKeyboard(unittest.TestCase):
+    def test_2_keys_keyboard(self):
+        solution = Solution()
+
+        self.assertEqual(3, solution.minSteps(3))
+        self.assertEqual(0, solution.minSteps(1))
