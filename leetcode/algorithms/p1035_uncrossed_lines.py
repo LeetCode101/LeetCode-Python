7334aa9692c63ccb9d08f4440d1610e4d3f428bd
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-26 16:05:31 +0800

    Update p1035_uncrossed_lines.py

--- leetcode/algorithms/p1035_uncrossed_lines.py
+++ leetcode/algorithms/p1035_uncrossed_lines.py
@@ -1,14 +1,16 @@
-import collections
 from typing import List
 
 
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        dp = collections.defaultdict(int)
         m, n = len(nums1), len(nums2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        for i in range(m):
-            for j in range(n):
-                dp[i, j] = max(dp[i - 1, j - 1] + (nums1[i] == nums2[j]), dp[i - 1, j], dp[i, j - 1])
-        
-        return dp[m - 1, n - 1]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+        return dp[-1][-1]
