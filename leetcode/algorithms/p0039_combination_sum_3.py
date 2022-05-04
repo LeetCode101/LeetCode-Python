b7283dd139028c60de7a7e6a51d0b026a5f6b68c
Frederick-S <mao_xiaodan@hotmail.com>
2022-05-04 15:45:20 +0800

    p39

---
+++ leetcode/algorithms/p0039_combination_sum_3.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) \
+            -> List[List[int]]:
+        result = []
+
+        self._dfs(sorted(candidates), 0, target, [], result)
+
+        return result
+
+    def _dfs(self, candidates, start, target, combination, result):
+        if target == 0:
+            result.append(combination[:])
+
+            return
+
+        for i in range(start, len(candidates)):
+            n = candidates[i]
+
+            if n > target:
+                break
+
+            combination.append(n)
+
+            self._dfs(candidates, i, target - n, combination, result)
+
+            combination.pop()
