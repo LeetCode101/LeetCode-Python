0253a7466f63eb86a86a8c9a60a2d203a4ef48d6
Frederick-S <mao_xiaodan@hotmail.com>
2022-05-04 14:39:55 +0800

    Update p0031_next_permutation.py

--- leetcode/algorithms/p0031_next_permutation.py
+++ leetcode/algorithms/p0031_next_permutation.py
@@ -6,7 +6,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = len(nums) - 1
+        n = len(nums)
+        i = n - 1
 
         while i - 1 >= 0 and nums[i] <= nums[i - 1]:
             i -= 1
@@ -18,8 +19,13 @@ class Solution:
 
         j = i
 
-        while j + 1 < len(nums) and nums[j + 1] > nums[i - 1]:
+        while j + 1 < n and nums[j + 1] > nums[i - 1]:
             j += 1
 
         nums[i - 1], nums[j] = nums[j], nums[i - 1]
-        nums[i:] = nums[i:][::-1]
+        low, high = i, n - 1
+
+        while low < high:
+            nums[low], nums[high] = nums[high], nums[low]
+            low += 1
+            high -= 1
